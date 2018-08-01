835e30604356ddfecd10f4726f1cd321788a5c1b
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-01 08:37:14 +0200

    refactor: remove unnecessary usings

--- Common/BLL.Interfaces/IEventService.cs
+++ Common/BLL.Interfaces/IEventService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ferretto.Common.BLL.Interfaces
+﻿namespace Ferretto.Common.BLL.Interfaces
 {
   public interface IEventService
   {
