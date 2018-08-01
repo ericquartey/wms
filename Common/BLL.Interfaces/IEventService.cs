c04addf0d14bec23ee2e44bbe9da837039b0f2ff
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-01 08:35:16 +0200

    feature(event-service): add service and interfaces

---
+++ Common/BLL.Interfaces/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferretto.Common.BLL.Interfaces
+{
+  public interface IEventService
+  {
+    void Invoke<T>(T eventArgs) where T : IEventArgs;
+  }
+}
---
+++ Common/BLL/EventService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Ferretto.Common.BLL.Interfaces;
+using Prism.Events;
+
+namespace Ferretto.Common.BLL
+{
+  public class EventService : IEventService
+  {
+    private readonly IEventAggregator eventAggregator;
+
+    public EventService(IEventAggregator eventAggregator)
+    {
+      this.eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
+    }
+
+    public void Invoke<TEventArgs>(TEventArgs eventArgs) where TEventArgs : IEventArgs
+    {
+      GetEventBus<TEventArgs>()?.Publish(eventArgs);
+    }
+
+    public void Subscribe<TEventArgs>(Action<TEventArgs> action, bool keepSubscriberReferenceAlive = false) where TEventArgs : IEventArgs
+    {
+      GetEventBus<TEventArgs>()?.Subscribe(action, keepSubscriberReferenceAlive);
+    }
+
+    /// <summary>
+    /// Subscribes a delegate to an event.
+    /// </summary>
+    /// <typeparam name="TEventArgs">The type of the event arguments.</typeparam>
+    /// <param name="action">The delegate that gets executed when the event is raised.</param>
+    /// <param name="token">Specifies the subscriber token. Events will be routed only if the event's token matches the subscriber's token.</param>
+    /// <param name="keepSubscriberReferenceAlive">When true, the EventService keeps a refernce to the subscriber so it does not get garbage collected.</param>
+    public void Subscribe<TEventArgs>(Action<TEventArgs> action, string token, bool keepSubscriberReferenceAlive = false) where TEventArgs : IEventArgs
+    {
+      GetEventBus<TEventArgs>().Subscribe(action, ThreadOption.BackgroundThread, keepSubscriberReferenceAlive, x => string.IsNullOrEmpty(x.Token) || x.Token == token);
+    }
+
+    public void Unusbscribe<T>(Action<T> action) where T : IEventArgs
+    {
+      GetEventBus<T>().Unsubscribe(action);
+    }
+
+    private PubSubEvent<TEventArgs> GetEventBus<TEventArgs>() where TEventArgs : IEventArgs
+    {
+      return eventAggregator.GetEvent<PubSubEvent<TEventArgs>>();
+    }
+  }
+}
