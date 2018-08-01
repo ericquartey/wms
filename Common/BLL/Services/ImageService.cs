f7d74bbf24c2f9f9c8dc8c24eab62d5214ade8c7
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-01 10:25:50 +0200

    refactor(image-service): add argument validation for ImageService.GetImage method

--- Common/BLL/Services/ImageService.cs
+++ Common/BLL/Services/ImageService.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Media;
+﻿using System;
+using System.Windows.Media;
 using Ferretto.Common.DAL.Interfaces;
 
 namespace Ferretto.Common.BLL.Services
@@ -14,6 +15,11 @@ namespace Ferretto.Common.BLL.Services
 
     public ImageSource GetImage(string pathName)
     {
+      if (String.IsNullOrWhiteSpace(pathName))
+      {
+        throw new ArgumentException("The parameter cannot be null or whitespace.", nameof(pathName));
+      }
+
       return imageRepository.GetById(pathName);
     }
   }
