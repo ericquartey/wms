ef7e1cf1280f26b4acc0ba800c8e0b08e5883ce6
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-02 12:19:44 +0200

    refactor: rename Prism module classes

--- WMS/Modules/Layout/LayoutModule.cs
+++ WMS/Modules/Layout/LayoutModule.cs
@@ -7,14 +7,14 @@ namespace Ferretto.WMS.Modules.Layout
 {
   [Module(ModuleName = nameof(Common.Configuration.Modules.Layout), OnDemand = true)]
   [ModuleDependency(nameof(Common.Configuration.Modules.BusinessLogic))]
-  public class Module : IModule
+  public class LayoutModule : IModule
   {
     #region IModule Members
 
     public IUnityContainer Container { get; private set; }
     public IRegionManager RegionManager { get; private set; }
 
-    public Module(IUnityContainer container, IRegionManager regionManager)
+    public LayoutModule(IUnityContainer container, IRegionManager regionManager)
     {
       this.Container = container;
       this.RegionManager = regionManager;
