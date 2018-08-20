65dfd72fcb9556bb64ecac6ffa7cbe9d6a06cb78
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-20 12:06:45 +0200

    feat(dialog-service): add direct call to DevExpress MessageBox

--- WMS/Modules/Catalog/Views/ItemsAndDetailsView.xaml.cs
+++ WMS/Modules/Catalog/Views/ItemsAndDetailsView.xaml.cs
@@ -5,13 +5,9 @@ namespace Ferretto.WMS.Modules.Catalog
 {
   public partial class ItemsAndDetailsView : WMSView
   {
-    IDialogService dialogService;
     public ItemsAndDetailsView(IDialogService dialogService)
     {
       InitializeComponent();
-
-      this.dialogService = dialogService;
-    {
     }
   }
 }
