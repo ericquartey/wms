afaf4358e5ce7ef46d1bae47c0a6120e1d87a3ca
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-21 10:35:55 +0200

    feat(bll-objects): add basic business object for Item entity

---
+++ Common/BLL.Interfaces/Models/IItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferretto.Common.BLL.Interfaces.Models
+{
+  public interface IItem : IModel<int>
+  {
+    string Code { get; set; }
+    string Description { get; set; }
+    string ClassId { get; set; }
+    int? MeasureUnitId { get; set; }
+    int? Width { get; set; }
+    int? Length { get; set; }
+    int? Height { get; set; }
+  }
+}
---
+++ Common/Modules/BLL/Models/Item.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ferretto.Common.BLL.Interfaces;
+
+namespace Ferretto.Common.Modules.BLL.Models
+{
+  public sealed class Item : IModel<int>
+  {
+    public int Id { get; set; }
+    public string Code { get; set; }
+    public string Description { get; set; }
+    public string ClassId { get; set; }
+    public int? MeasureUnitId { get; set; }
+
+    private int? width;
+    public int? Width
+    {
+      get { return this.width; }
+      set
+      {
+        if (value.HasValue && value <= 0)
+        {
+          throw new ArgumentOutOfRangeException(nameof(value), "Parameter must be strictly positive.");
+        }
+
+        this.width = value;
+      }
+    }
+
+    private int? length;
+    public int? Length
+    {
+      get { return this.length; }
+      set
+      {
+        if (value.HasValue && value <= 0)
+        {
+          throw new ArgumentOutOfRangeException(nameof(value), "Parameter must be strictly positive.");
+        }
+
+        this.length = value;
+      }
+    }
+
+    private int? height;
+    public int? Height
+    {
+      get { return this.height; }
+      set
+      {
+        if (value.HasValue && value <= 0)
+        {
+          throw new ArgumentOutOfRangeException(nameof(value), "Parameter must be strictly positive.");
+        }
+
+        this.height = value;
+      }
+    }
+  }
+}
