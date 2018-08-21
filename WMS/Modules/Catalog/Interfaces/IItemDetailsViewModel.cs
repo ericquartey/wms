f6cb27d92d00d6966a391a46543fa33d5dc0ea92
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-21 10:31:03 +0200

    refactor: disable analysis rule S4023

--- WMS/Modules/Catalog/Interfaces/IItemDetailsViewModel.cs
+++ WMS/Modules/Catalog/Interfaces/IItemDetailsViewModel.cs
@@ -1,9 +1,5 @@
 ﻿namespace Ferretto.WMS.Modules.Catalog
 {
-  [System.Diagnostics.CodeAnalysis.SuppressMessage(
-    "Minor Code Smell",
-    "S4023:Interfaces should not be empty",
-    Justification = "The interface is used for IoC.")]
   public interface IItemDetailsViewModel
   {
   }
