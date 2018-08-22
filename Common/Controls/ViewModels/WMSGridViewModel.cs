5c3b8d4fbec7b40309c1ca830a3099103c998f8a
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-22 10:29:33 +0200

    feat(wms-grid): add view model

---
+++ Common/Controls/ViewModels/WMSGridViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ferretto.Common.BLL.Interfaces;
+using Prism.Mvvm;
+
+namespace Ferretto.Common.Controls.ViewModels
+{
+  public class WmsGridViewModel<TModel, TId> : BindableBase where TModel : IModel<TId>
+  {
+    private readonly IEntityService<TModel, TId> entityService;
+    private TModel selectedItem;
+
+    protected WmsGridViewModel(IEntityService<TModel, TId> entityService)
+    {
+      this.entityService = entityService;
+
+      this.Initialize();
+    }
+
+    private void Initialize()
+    {
+      this.entityService.GetAll();
+    }
+
+    public IEnumerable<TModel> Items
+    {
+      get { return this.entityService.GetAll(); }
+    }
+
+    public TModel SelectedItem
+    {
+      get { return this.selectedItem; }
+      set { this.SetProperty(ref this.selectedItem, value); }
+    } 
+  }
+}
