a5048dc8d87b7c6562c448ed28a717725d1f68e5
Giovanni Basso <giovanni.basso@altran.com>
2018-08-22 14:10:42 +0200

    Add NavMenuTouchControl

---
+++ Common/Controls/Controls/Custom/NavMenuTouchControl/NavMenuTocuchControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Ferretto.Common.Controls
+{
+    public partial class NavMenuTouchControl : UserControl
+    {
+        public NavMenuTouchControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Common/Controls/Controls/Custom/NavMenuTouchControl/ViewModels/NavMenuItem.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Ferretto.Common.Controls.Interfaces;
+using Microsoft.Practices.ServiceLocation;
+using Prism.Commands;
+
+namespace Ferretto.Common.Controls
+{
+  public class NavMenuItem : IMenuItemViewModel
+  {
+    #region Ctor
+    public NavMenuItem()
+    {
+      this.Command = new DelegateCommand(this.CommandAction);
+    }
+
+    public NavMenuItem(System.String displayName, System.String backColor, System.String image, System.String moduleName, System.String viewName, ICommand command, ObservableCollection<NavMenuItem> children)
+    {
+      this.DisplayName = displayName;
+      this.BackColor = backColor;
+      this.Image = image;
+      this.ModuleName = moduleName;
+      this.ViewName = viewName;
+      this.Command = command;
+      this.Children = children;
+    }
+    #endregion
+
+    #region Properties
+    public string DisplayName { get; set; }
+
+    public string BackColor { get; set; }
+
+    public string Image { get; set; }
+
+    public string ModuleName { get; set; }
+
+    public string ViewName { get; set; }
+
+    public ICommand Command { get; set; }
+
+    public ObservableCollection<NavMenuItem> Children
+    {
+      get;
+      set;
+    }
+
+    public bool HasChildren
+    {
+      get
+      {
+        return this.Children != null && this.Children.Count > 0;
+      }
+    }
+    #endregion
+
+    #region Methods
+    public void CommandAction()
+    {
+      var navigationService = ServiceLocator.Current.GetInstance<INavigationService>();
+      navigationService.Appear(this.ModuleName, this.ViewName);
+    }
+    #endregion
+  }
+}
