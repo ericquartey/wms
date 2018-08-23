ed1092b56c004cc49be605ee123765d62f67d884
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-23 10:04:54 +0200

    feat(wms-grid): update model for `Item` entity

--- Common/BLL.Interfaces/Models/IItem.cs
+++ Common/BLL.Interfaces/Models/IItem.cs
@@ -1,4 +1,5 @@
-﻿using Ferretto.Common.DAL.Models;
+﻿using System;
+using Ferretto.Common.DAL.Models;
 
 namespace Ferretto.Common.BLL.Interfaces.Models
 {
@@ -6,11 +7,26 @@ namespace Ferretto.Common.BLL.Interfaces.Models
   {
     string Code { get; set; }
     string Description { get; set; }
-    AbcClass AbcClass { get; set; }
-    MeasureUnit MeasureUnit { get; set; }
     int? Width { get; set; }
     int? Length { get; set; }
     int? Height { get; set; }
+    int? FifoTimeStore { get; set; }
+    int? FifoTimePick { get; set; }
+    int? ReorderPoint { get; set; }
+    int? ReorderQuantity { get; set; }
+    int? AverageWeight { get; set; }
+    int? PickTolerance { get; set; }
+    int? StoreTolerance { get; set; }
+    int? InventoryTolerance { get; set; }
+    DateTime CreationDate { get; set; }
+    DateTime? LastModificationDate { get; set; }
+    DateTime? InventoryDate { get; set; }
+    DateTime? LastPickDate { get; set; }
+    DateTime? LastStoreDate { get; set; }
     string Image { get; set; }
+    string Note { get; set; }
+    AbcClass AbcClass { get; set; }
+    MeasureUnit MeasureUnit { get; set; }
+    ItemManagementType ItemManagementType { get; set; }
   }
 }
