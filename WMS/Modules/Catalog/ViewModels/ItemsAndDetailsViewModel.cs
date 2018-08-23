3ac2ebb984990a0cd88e475f671067ce464130a4
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-23 10:03:50 +0200

    refactor: remove unnecessary usings

--- WMS/Modules/Catalog/ViewModels/ItemsAndDetailsViewModel.cs
+++ WMS/Modules/Catalog/ViewModels/ItemsAndDetailsViewModel.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections.Generic;
-using Ferretto.Common.BLL.Interfaces;
-using Ferretto.Common.BLL.Interfaces.Models;
-using Ferretto.Common.Controls;
+﻿using Ferretto.Common.Controls;
 
 namespace Ferretto.WMS.Modules.Catalog
 {
   class ItemsAndDetailsViewModel : BaseNavigationViewModel
   {
+
   }
 }
