421d97ce156edb420726c6836beaf16b72bfa114
Alessandro Salomone <alessandro.salomone@altran.com>
2018-08-23 12:24:52 +0200

    feat(splash-screen): create base splash screen

---
+++ WMS/App/SplashScreen.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Ferretto.WMS.App
+{
+  public partial class SplashScreen : UserControl
+  {
+    public SplashScreen()
+    {
+      this.InitializeComponent();
+    }
+  }
+}
