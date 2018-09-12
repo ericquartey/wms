0f9fd2f428f59b13de3ea80abb8934e2d32c0dd1
Giovanni Basso <giovanni.basso@altran.com>
2018-09-12 16:57:41 +0200

    Add DataService

---
+++ Common/Modules/BLL/Services/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ferretto.Common.BLL.Interfaces;
+using Ferretto.Common.Modules.DAL.EF;
+
+namespace Ferretto.Common.Modules.BLL.Services
+{
+  public class DataService : IDataService
+  {
+    private readonly DatabaseContext context;
+
+    public DataService(DatabaseContext context)
+    {
+      this.context = context;
+    }
+
+    public IQueryable<TEntity> GetData<TEntity>(Func<IQueryable<TEntity>, IQueryable<TEntity>> predicate = null) where TEntity : class
+    {
+      if (predicate == null)
+      {
+        return this.context.Set<TEntity>();
+      }
+      return predicate.Invoke(this.context.Set<TEntity>());
+    }
+  }
+
+}
