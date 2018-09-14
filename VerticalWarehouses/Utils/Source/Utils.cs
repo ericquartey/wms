c7c981fa08105fe3ec69859acd8e506572f48474
Alessandro Gennaro <agennaro@ferrettogroup.com>
2018-09-14 11:18:41 +0200

    chore: create the barebone solution structure

---
+++ VerticalWarehouses/Utils/Source/Utils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferretto.VW.Utils
+{
+  public class Utils
+  { }
+
+  /// <summary>
+  /// My class implementation 
+  /// </summary>
+  public class CMyClass
+  {
+    const int NITEMS = 10;
+
+    private int m_myVariable;
+
+    /// <summary>
+    /// Default c-tor.
+    /// </summary>
+    public CMyClass()
+    {
+      m_myVariable = 0;
+    }
+
+
+  }
+  
+}
