36a7d898a4ab1a41c551648553f7a1d58faa1b74
Alessandro Salomone <alessandro.salomone@altran.com>
2018-09-17 15:15:42 +0200

    feat(image-control): add user control

---
+++ Common/Controls/Controls/Image/Image.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ferretto.Common.Controls
+{
+    public partial class Image : UserControl
+    {
+        #region Fields
+
+        public static readonly DependencyProperty PathProperty = DependencyProperty.Register(
+         nameof(Path), typeof(string), typeof(Image), new PropertyMetadata(default(string), new PropertyChangedCallback(OnPathChanged)));
+
+        #endregion Fields
+
+        #region Constructors
+
+        public Image()
+        {
+            this.InitializeComponent();
+
+            this.DataContext = new ImageViewModel();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string Path
+        {
+            get => (string)this.GetValue(PathProperty);
+            set => this.SetValue(PathProperty, value);
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        private static void OnPathChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Image image
+                &&
+                image.DataContext is ImageViewModel viewModel
+                &&
+                e.NewValue is string imagePath
+                &&
+                string.IsNullOrWhiteSpace(imagePath) == false)
+            {
+                viewModel.RetrieveImage(imagePath);
+            }
+        }
+
+        #endregion Methods
+    }
+}
---
+++ Common/Controls/Controls/Image/ViewModels/ImageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Media;
+using Ferretto.Common.BLL.Interfaces;
+using Microsoft.Practices.ServiceLocation;
+using Prism.Mvvm;
+
+namespace Ferretto.Common.Controls
+{
+    internal class ImageViewModel : BindableBase
+    {
+        #region Fields
+
+        private readonly IImageService imageService;
+        private ImageSource imageSource;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public ImageViewModel()
+        {
+            this.imageService = ServiceLocator.Current.GetInstance<IImageService>();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public ImageSource Source
+        {
+            get => this.imageSource;
+            set => this.SetProperty(ref this.imageSource, value);
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public void RetrieveImage(string imagePath)
+        {
+            this.imageSource = this.imageService.GetImage(imagePath);
+        }
+
+        #endregion Methods
+    }
+}
