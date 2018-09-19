1380558b2769a4842112dac36cfd46a4454c305f
Giovanni Basso <giovanni.basso@altran.com>
2018-09-19 11:36:07 +0200

    Add StatusBar

---
+++ Common/Controls/Controls/StatusBar/StatusBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Ferretto.Common.Controls
+{
+    public class StatusBar : ContentControl
+    {
+        public StatusBar()
+        {
+            this.DataContext = new StatusBarViewModel();
+        }
+    }
+}
---
+++ Common/Controls/Controls/StatusBar/StatusBarViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Ferretto.Common.Controls
+{
+    public class StatusBarViewModel : BindableBase
+    {
+        private string info;
+
+        public StatusBarViewModel()
+        {
+        }
+
+        public string Info
+        {
+            get => this.info;
+            set => this.SetProperty(ref this.info, value);
+        }
+    }
+}
