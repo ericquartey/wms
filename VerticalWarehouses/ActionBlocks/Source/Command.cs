00ca14990471d1392352a33d14832e16adbd145d
Alessandro Gennaro <agennaro@ferrettogroup.com>
2018-09-21 18:01:10 +0200

    no message

--- VerticalWarehouses/ActionBlocks/Source/Command.cs
+++ VerticalWarehouses/ActionBlocks/Source/Command.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Ferretto.VW.Utils;
-using Ferretto.VW.InverterDriver;
-
-
+﻿
 namespace Ferretto.VW.ActionBlocks
 {
-  public class CCommand
-  {
-
-  }
+    public class ActionCmds
+    { }
 
 
 }
