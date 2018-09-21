c938b0eac8bd5e1f91d23ed6208ea8dc186ba91d
Nicolò Padovani <nicolo.padovani@autoware.it>
2018-09-21 12:18:16 +0200

    Added buttons and image to CompartmentationPage. Changed mainwindow xaml code for loading the images (it's a relative path now)

--- VerticalWarehouses/VerticalWarehousesApp/Views/CompartmentationPageView.xaml.cs
+++ VerticalWarehouses/VerticalWarehousesApp/Views/CompartmentationPageView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Shapes;
@@ -16,6 +15,8 @@ namespace Ferretto.VW.VerticalWarehousesApp.Views
         Rectangle currentRect;
         List<Rectangle> rects;
 
+        CompartmentActionMode currentActionMode = CompartmentActionMode.DoNothing;
+
         bool isDrawing = false;
 
         public CompartmentationPageView()
@@ -26,54 +27,90 @@ namespace Ferretto.VW.VerticalWarehousesApp.Views
 
         private void imgCamera_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (this.isDrawing)
+            if (this.currentActionMode == CompartmentActionMode.CreateCompartment)
             {
+                if (this.isDrawing)
+                {
 
-            } else
-            {
-                this.testPoint = e.GetPosition(this);
-                this.currentRectStartPoint = this.testPoint;
-                this.currentRect = new Rectangle();
-                this.currentRect.Height = 0;
-                this.currentRect.Width = 0;
-                this.currentRect.Fill = Brushes.Red;
-                this.cnvImage.Children.Add(this.currentRect);
+                }
+                else
+                {
+                    this.testPoint = e.GetPosition(this);
+                    this.currentRectStartPoint = this.testPoint;
+                    this.currentRect = new Rectangle();
+                    this.currentRect.Height = 0;
+                    this.currentRect.Width = 0;
+                    this.currentRect.Fill = Brushes.Red;
+                    this.cnvImage.Children.Add(this.currentRect);
 
-                this.currentRect.SetValue(Canvas.LeftProperty, this.testPoint.X);
-                this.currentRect.SetValue(Canvas.TopProperty, this.testPoint.Y + 50);
+                    this.currentRect.SetValue(Canvas.LeftProperty, this.testPoint.X);
+                    this.currentRect.SetValue(Canvas.TopProperty, this.testPoint.Y + 50);
 
-                this.isDrawing = true;
+                    this.isDrawing = true;
+                }
             }
         }
 
         private void imgCamera_MouseMove(object sender, MouseEventArgs e)
         {
-            if (this.isDrawing)
+            if (this.currentActionMode == CompartmentActionMode.CreateCompartment)
             {
-                this.testPoint = e.GetPosition(this);
-
-                double x = this.testPoint.X - this.currentRectStartPoint.X;
-                double y = this.testPoint.Y - this.currentRectStartPoint.Y;
-
-                if (x >= 0)
+                if (this.isDrawing)
                 {
-                    this.currentRect.Width = x;
-                }
-                if (y >= 0)
-                {
-                    this.currentRect.Height = y;
+                    this.testPoint = e.GetPosition(this);
+
+                    double x = this.testPoint.X - this.currentRectStartPoint.X;
+                    double y = this.testPoint.Y - this.currentRectStartPoint.Y;
+
+                    if (x >= 0)
+                    {
+                        this.currentRect.Width = x;
+                    }
+                    if (y >= 0)
+                    {
+                        this.currentRect.Height = y;
+                    }
                 }
             }
         }
 
         private void imgCamera_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (this.isDrawing)
+            if (this.currentActionMode == CompartmentActionMode.CreateCompartment)
             {
-                this.rects.Add(this.currentRect);
-                this.currentRect = null;
-                this.isDrawing = false;
+                if (this.isDrawing)
+                {
+                    this.rects.Add(this.currentRect);
+                    this.currentRect = null;
+                    this.isDrawing = false;
+                }
             }
         }
+
+        private void ButtonSetActionToCreateCompartment(object sender, RoutedEventArgs e)
+        {
+            this.SetActionToCreateCompartment();
+        }
+
+        private void ButtonSetActionToDoNothing(object sender, RoutedEventArgs e)
+        {
+            this.SetActionToDoNothing();
+        }
+
+        private void SetActionToCreateCompartment()
+        {
+            this.currentActionMode = CompartmentActionMode.CreateCompartment;
+        }
+
+        private void SetActionToDoNothing()
+        {
+            this.currentActionMode = CompartmentActionMode.DoNothing;
+        }
+    }
+
+    public enum CompartmentActionMode
+    {
+        CreateCompartment,
+        DoNothing,
     }
 }
