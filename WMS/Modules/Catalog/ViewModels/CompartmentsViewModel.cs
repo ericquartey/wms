50f41bfc1aa107cd5cd0682e66abd9e2eaa749af
Nicola Moro <nmoro@ferrettogroup.com>
2018-09-21 15:25:04 +0200

    feat (compartments-list): add compartments master grid with very basic filters, toolbar buttons, columns and empty detail view

---
+++ WMS/Modules/Catalog/ViewModels/CompartmentsViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Ferretto.Common.BLL.Interfaces;
+using Ferretto.Common.Controls;
+using Ferretto.Common.Controls.Services;
+using Ferretto.Common.Utils;
+using Microsoft.Practices.ServiceLocation;
+using Prism.Commands;
+
+namespace Ferretto.WMS.Modules.Catalog
+{
+    public class CompartmentsViewModel : BaseNavigationViewModel
+    {
+        #region Fields
+
+        private readonly IDataSourceService filterService;
+        private IDataSource<Common.Models.Compartment> currentDataSource;
+
+        private ICommand viewDetailsCommand;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public CompartmentsViewModel()
+        {
+            this.filterService = ServiceLocator.Current.GetInstance<IDataSourceService>();
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public IDataSource<Common.Models.Compartment> CurrentDataSource
+        {
+            get => this.currentDataSource;
+            set => this.SetProperty(ref this.currentDataSource, value);
+        }
+
+        public IEnumerable<IDataSource<Common.Models.Compartment>> Filters =>
+                    this.filterService.GetAll(MvvmNaming.GetViewNameFromViewModelName(nameof(CompartmentsViewModel))) as IEnumerable<IDataSource<Common.Models.Compartment>>;
+
+        public ICommand ViewDetailsCommand => this.viewDetailsCommand ??
+            (this.viewDetailsCommand = new DelegateCommand(ExecuteViewDetailsCommand));
+
+        #endregion Properties
+
+        #region Methods
+
+        private static void ExecuteViewDetailsCommand()
+        {
+            ServiceLocator.Current.GetInstance<IEventService>()
+                .Invoke(new ShowDetailsEventArgs<Common.Models.Compartment>(true));
+        }
+
+        #endregion Methods
+    }
+}
---
+++ WMS/Modules/Catalog/Views/CompartmentsView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Ferretto.Common.Controls;
+
+namespace Ferretto.WMS.Modules.Catalog
+{
+    public partial class CompartmentsView : WmsView
+    {
+        #region Constructors
+
+        public CompartmentsView()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion Constructors
+    }
+}
