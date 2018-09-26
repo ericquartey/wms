e408830efdcaa659a237d7748ce8da7cfe2544e5
Nicola Moro <nmoro@ferrettogroup.com>
2018-09-26 15:19:31 +0200

    chore: merge architectural and db modifications

--- Common/DataModels/ItemCompartmentType.cs
+++ Common/DataModels/ItemCompartmentType.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace Ferretto.Common.Models
+namespace Ferretto.Common.DataModels
 {
     // Articolo - Tipo Scomparto
     public sealed class ItemCompartmentType
