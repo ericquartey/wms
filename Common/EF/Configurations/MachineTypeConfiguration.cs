b2b88ffeaf17395d56155625651858ac93c12c49
Alessandro Salomone <alessandro.salomone@altran.com>
2018-10-01 17:50:52 +0200

    refactor: introduce check on input parameter for EF configurations

--- Common/EF/Configurations/MachineTypeConfiguration.cs
+++ Common/EF/Configurations/MachineTypeConfiguration.cs
@@ -6,12 +6,21 @@ namespace Ferretto.Common.EF.Configurations
 {
     public class MachineTypeConfiguration : IEntityTypeConfiguration<MachineType>
     {
+        #region Methods
+
         public void Configure(EntityTypeBuilder<MachineType> builder)
         {
+            if (builder == null)
+            {
+                throw new System.ArgumentNullException(nameof(builder));
+            }
+
             builder.HasKey(m => m.Id);
 
             builder.Property(m => m.Id).HasColumnType("char(1)");
             builder.Property(m => m.Description).IsRequired();
         }
+
+        #endregion Methods
     }
 }
