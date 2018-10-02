e9aa4521a586ab8042bbeb4dcfffd0942697d901
Giovanni Basso <giovanni.basso@altran.com>
2018-10-02 14:26:29 +0200

    Add stratup project Ferretto.WMS.App.Compartment

---
+++ WMS/Modules/Compartment/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Ferretto.Common.Controls;
+using Ferretto.Common.Controls.Interfaces;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Ferretto.WMS.Modules.Compartment
+{
+    public class LayoutViewModel : BaseNavigationViewModel
+    {
+        #region Methods
+
+        protected override void OnAppear()
+        {
+            base.OnAppear();
+            var navigationService = ServiceLocator.Current.GetInstance<INavigationService>();
+            navigationService.Appear(nameof(Ferretto.Common.Utils.Modules.Compartment), Ferretto.Common.Utils.Modules.Compartment.COMPARTMENT);
+        }
+
+        #endregion Methods
+    }
+}
