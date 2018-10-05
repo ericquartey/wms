faeab8433d5a64b9d985be9beffe740b21bbb2ed
Alessandro Salomone <alessandro.salomone@altran.com>
2018-10-05 14:58:49 +0200

    feat(data-annotations): add label localisation for InfoText control

--- Common/Controls/Controls/InfoText/InfoText.xaml.cs
+++ Common/Controls/Controls/InfoText/InfoText.xaml.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Ferretto.Common.Controls
 {
-    public partial class InfoText : UserControl
+    public partial class InfoText : FormControl
     {
         #region Fields
 
+        public static readonly DependencyProperty FieldNameProperty = DependencyProperty.Register(
+         nameof(FieldName), typeof(string), typeof(InfoText), new PropertyMetadata(default(string), new PropertyChangedCallback(OnFieldNameChanged)));
+
         public static readonly DependencyProperty LabelProperty = DependencyProperty.Register(
             nameof(Label), typeof(string), typeof(InfoText), new PropertyMetadata(default(string)));
-
-        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
-            nameof(Text), typeof(string), typeof(InfoText), new FrameworkPropertyMetadata(default(string))
-            { CoerceValueCallback = CoerceValueWhenEmpty });
 
         #endregion Fields
 
@@ -23,22 +22,25 @@ namespace Ferretto.Common.Controls
         {
             this.InitializeComponent();
             this.InfoTextGrid.DataContext = this;
+
+            this.DataContextChanged += this.InfoText_DataContextChanged;
+            this.Loaded += this.InfoText_Loaded;
         }
 
         #endregion Constructors
 
         #region Properties
 
+        public string FieldName
+        {
+            get => (string)this.GetValue(FieldNameProperty);
+            set => this.SetValue(FieldNameProperty, value);
+        }
+
         public string Label
         {
             get => (string)this.GetValue(LabelProperty);
             set => this.SetValue(LabelProperty, value);
-        }
-
-        public string Text
-        {
-            get => (string)this.GetValue(TextProperty);
-            set => this.SetValue(TextProperty, value);
         }
 
         #endregion Properties
@@ -50,6 +52,44 @@ namespace Ferretto.Common.Controls
             var textValue = (string)baseValue;
 
             return string.IsNullOrWhiteSpace(textValue) ? "-" : baseValue;
+        }
+
+        private static void OnFieldNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is InfoText infoText)
+            {
+                SetTextBinding(infoText);
+
+                infoText.Label = RetrieveLocalizedFieldName(infoText.InnerText.DataContext, infoText.FieldName);
+            }
+        }
+
+        private static void SetTextBinding(InfoText infoText)
+        {
+            var binding = new Binding(infoText.FieldName)
+            {
+                Mode = BindingMode.OneWay,
+                FallbackValue = "-"
+            };
+
+            infoText.InnerText.SetBinding(System.Windows.Controls.Label.ContentProperty, binding);
+        }
+
+        private void InfoText_DataContextChanged(Object sender, DependencyPropertyChangedEventArgs e)
+        {
+            this.InnerText.DataContext = e.NewValue;
+
+            if (this.FieldName != null)
+            {
+                SetTextBinding(this);
+            }
+        }
+
+        private void InfoText_Loaded(Object sender, RoutedEventArgs e)
+        {
+            SetTextBinding(this);
+
+            this.Label = RetrieveLocalizedFieldName(this.InnerText.DataContext, this.FieldName);
         }
 
         #endregion Methods
