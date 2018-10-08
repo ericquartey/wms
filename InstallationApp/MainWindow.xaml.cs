51525f5cf63992830e05f50b061f82f09d9e47cf
Nicolò Padovani <nicolo.padovani@autoware.it>
2018-10-08 17:56:06 +0200

    Init Installation feature

---
+++ InstallationApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ferretto.VW.Utils.Source;
+
+namespace Ferretto.VW.InstallationApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructors
+
+        public MainWindow(CellsManager cm)
+        {
+            this.InitializeComponent();
+            Debug.Print(cm.ToString());
+        }
+
+        #endregion Constructors
+    }
+}
