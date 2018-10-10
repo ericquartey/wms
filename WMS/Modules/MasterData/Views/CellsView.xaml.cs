0ef299678bdedb24bf42210b1f09beb0baf2500f
Alessandro Salomone <alessandro.salomone@altran.com>
2018-10-10 16:22:27 +0200

    feat(cells-view): add view and view model

---
+++ WMS/Modules/MasterData/Views/CellsView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Ferretto.Common.Controls;
+using Ferretto.Common.Controls.Services;
+using Ferretto.Common.Modules.BLL.Models;
+
+namespace Ferretto.WMS.Modules.MasterData
+{
+    public partial class CellsView : WmsView
+    {
+        #region Constructors
+
+        public CellsView()
+        {
+            this.InitializeComponent();
+
+            this.MainGridControl.AsyncOperationCompleted += this.MainGridControl_AsyncOperationCompleted;
+
+            SplashScreenService.SetMessage(Common.Resources.DesktopApp.InitializingMainWindow);
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private async void MainGridControl_AsyncOperationCompleted(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (this.DataContext is EntityListViewModel<Item> viewModel)
+            {
+                await viewModel.UpdateFilterTilesCountsAsync().ConfigureAwait(true);
+                this.MainGridControl.AsyncOperationCompleted -= this.MainGridControl_AsyncOperationCompleted;
+            }
+        }
+
+        #endregion Methods
+    }
+}
