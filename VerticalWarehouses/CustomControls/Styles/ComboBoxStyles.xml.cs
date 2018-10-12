055f66811f6ac32ca75baa3946e351a15def54e0
Nicolò Padovani <nicolo.padovani@autoware.it>
2018-10-12 17:49:40 +0200

    back up commit

---
+++ VerticalWarehouses/CustomControls/Styles/ComboBoxStyles.xml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Ferretto.VW.CustomControls.Styles
+{
+    public partial class ComboBoxStyles : ResourceDictionary
+    {
+        #region Constructors
+
+        public ComboBoxStyles()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public void OpenDropDownList(object sender, MouseButtonEventArgs e)
+        {
+            Debug.Print("flag\n");
+            if (sender is ComboBox comboBox)
+            {
+                comboBox.IsDropDownOpen = !comboBox.IsDropDownOpen;
+            }
+        }
+
+        #endregion Methods
+    }
+}
