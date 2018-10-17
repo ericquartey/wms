b60ba0f59ff9f9c9f74d3771e5b4cc36d97bc144
Nicolò Padovani <nicolo.padovani@autoware.it>
2018-10-17 14:27:59 +0200

    eliminated unecessary methods

--- VerticalWarehouses/InstallationApp/MainWindow.xaml.cs
+++ VerticalWarehouses/InstallationApp/MainWindow.xaml.cs
@@ -31,60 +31,9 @@ namespace Ferretto.VW.InstallationApp
             this.BackToVWApp();
         }
 
-        public void ChangeComboBox1IsDropDownOpenButtonMethod(object sender, MouseButtonEventArgs e)
-        {
-            this.ChangeComboBox1IsDropDownOpen();
-        }
-
-        public void ChangeComboBox2IsDropDownOpenButtonMethod(object sender, MouseButtonEventArgs e)
-        {
-            this.ChangeComboBox2IsDropDownOpen();
-        }
-
-        public void ChangeComboBox3IsDropDownOpenButtonMethod(object sender, MouseButtonEventArgs e)
-        {
-            this.ChangeComboBox3IsDropDownOpen();
-        }
-
         private void BackToVWApp()
         {
             NavigationService.RaiseBackToVWAppEvent();
-        }
-
-        private void ChangeComboBox1IsDropDownOpen()
-        {
-            var d = this.DataContext as MainWindowViewModel;
-            if (d != null)
-            {
-                if (d.EnableComboBox1IsDropDownOpen)
-                {
-                    Action action = () => d.EnableComboBox1IsDropDownOpen = false;
-                    Application.Current.Dispatcher.BeginInvoke(action);
-                }
-                else
-                {
-                    Action action = () => d.EnableComboBox1IsDropDownOpen = true;
-                    Application.Current.Dispatcher.BeginInvoke(action);
-                }
-            }
-        }
-
-        private void ChangeComboBox2IsDropDownOpen()
-        {
-            var d = this.DataContext as MainWindowViewModel;
-            if (d != null)
-            {
-                d.EnableComboBox2IsDropDownOpen = !d.EnableComboBox2IsDropDownOpen;
-            }
-        }
-
-        private void ChangeComboBox3IsDropDownOpen()
-        {
-            MainWindowViewModel d = (MainWindowViewModel)this.DataContext;
-            if (d != null)
-            {
-                d.EnableComboBox3IsDropDownOpen = !d.EnableComboBox3IsDropDownOpen;
-            }
         }
 
         private void CloseThisMainWindow()
