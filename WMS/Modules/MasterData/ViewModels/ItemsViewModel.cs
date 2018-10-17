7c1711ffce6f94029f9de4bf4e13d755673d7ff0
Giovanni Basso <giovanni.basso@altran.com>
2018-10-17 14:04:05 +0200

    Add WmsHistoryViewAppear custom trigger

--- WMS/Modules/MasterData/ViewModels/ItemsViewModel.cs
+++ WMS/Modules/MasterData/ViewModels/ItemsViewModel.cs
@@ -5,13 +5,5 @@ namespace Ferretto.WMS.Modules.MasterData
 {
     public class ItemsViewModel : EntityListViewModel<Item, int>
     {
-        #region Methods
-
-        public override void ExecuteViewDetailsCommand()
-        {
-            this.HistoryViewService.Appear(nameof(Common.Utils.Modules.MasterData), Common.Utils.Modules.MasterData.ITEMDETAILS, this.CurrentItem?.Id);
-        }
-
-        #endregion Methods
     }
 }
