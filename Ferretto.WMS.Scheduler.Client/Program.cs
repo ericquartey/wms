94e79afef58a9303d703b20643da8dffb675b920
Alessandro Salomone <alessandro.salomone@altran.it>
2018-10-22 15:23:03 +0200

    chore(scheduler): add demo client project

---
+++ Ferretto.WMS.Scheduler.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Ferretto.WMS.Scheduler.WebAPI;
+
+namespace Ferretto.WMS.Scheduler.Client
+{
+    internal class Program
+    {
+        #region Methods
+
+        private static async Task Main(string[] args)
+        {
+            var machinesClient1 = new MachinesClient("http://localhost:5000/");
+            var machinesClient2 = new MachinesClient("http://localhost:5000/");
+
+            var machines = await machinesClient1.GetAllAsync();
+            Console.WriteLine($"Machines Retrieved (machine[0] is {machines[0].AisleName}).");
+
+            Console.WriteLine("Presse <Enter> to exit.");
+            Console.ReadKey();
+        }
+
+        #endregion Methods
+    }
+}
