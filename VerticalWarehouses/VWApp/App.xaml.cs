1449bafadcb46a57db3a929b6c648279d9671089
Nicolò Padovani <nicolo.padovani@autoware.it>
2018-10-23 08:34:42 +0200

    Traduced some items

--- VerticalWarehouses/VWApp/App.xaml.cs
+++ VerticalWarehouses/VWApp/App.xaml.cs
@@ -3,7 +3,7 @@
 namespace Ferretto.VW.VWApp
 {
     public partial class App : Application
-    {//Instances of differents' projects MainWindow are declared in App.xaml.cs, so that there will be no running processes after closing VWApp.
+    {
         #region Properties
 
         public InstallationApp.MainWindow InstallationMainWindowInstance { get; set; }
