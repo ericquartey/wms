b78ed2e544021a63edefabe4e27e15c874e5df6f
Nicolò Padovani <nicolo.padovani@autoware.it>
2018-10-29 18:30:00 +0100

    Minor graphic changes

---
+++ VerticalWarehouses/InstallationApp/ViewsAndViewModels/GatesControl/GatesControlNavigationButtonsView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace Ferretto.VW.InstallationApp.ViewsAndViewModels.GatesControl
+{
+    /// <summary>
+    /// Interaction logic for GatesControlNavigationButtonsView.xaml
+    /// </summary>
+    public partial class GatesControlNavigationButtonsView : UserControl
+    {
+        #region Constructors
+
+        public GatesControlNavigationButtonsView()
+        {
+            this.InitializeComponent();
+            this.DataContext = new GatesControlNavigationButtonsViewModel();
+        }
+
+        #endregion Constructors
+    }
+}
