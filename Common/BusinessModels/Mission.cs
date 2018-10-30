71e279f77ad6f7715b1288ea77f1653f6a67ae96
Alessandro Salomone <alessandro.salomone@altran.it>
2018-10-30 09:03:23 +0100

    feat(scheduler): add models for item withdrawal

---
+++ Common/BusinessModels/Mission.cs
@@ -0,0 +1,17 @@
+﻿namespace Ferretto.Common.BusinessModels
+{
+    public enum MissionType { Withdrawal, Insertion }
+
+    public class Mission
+    {
+        #region Properties
+
+        public int BayId { get; set; }
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public MissionType Type { get; set; }
+        public System.Object ItemId { get; set; }
+
+        #endregion Properties
+    }
+}
