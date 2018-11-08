e505521ac29481c5c77c45ff0882aebf5a01dd07
Alessandro Salomone <alessandro.salomone@altran.it>
2018-11-08 15:04:22 +0100

    feat(automation-service): create new mock solution

---
+++ WMS/AutomationServiceMock/AutomationService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading.Tasks;
+using Ferretto.WMS.Scheduler.WebAPI.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Ferretto.WMS.AutomationServiceMock
+{
+    internal class AutomationService : IAutomationService
+    {
+        #region Fields
+
+        private readonly IBaysClient baysClient;
+        private readonly IConfiguration configuration;
+
+        private readonly ILogger logger;
+        private readonly IMissionsClient missionsClient;
+
+        private readonly Random random = new Random();
+        private readonly IWakeupHubClient wakeupHubClient;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public AutomationService(
+            IConfiguration configuration,
+            ILogger<AutomationService> logger,
+            IWakeupHubClient wakeupHubClient,
+            IMissionsClient missionsClient,
+            IBaysClient baysClient)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+            this.missionsClient = missionsClient;
+            this.baysClient = baysClient;
+            this.wakeupHubClient = wakeupHubClient;
+
+            this.wakeupHubClient.WakeupReceived += this.WakeupReceived;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public async Task Initialize()
+        {
+            await this.wakeupHubClient.ConnectAsync();
+
+            this.logger.LogInformation("Automation service initialized");
+        }
+
+        public async Task NotifyUserLogin(int bayId)
+        {
+            this.logger.LogInformation($"Notifying the scheduler that bay '{bayId}' is operational.");
+            await this.baysClient.MarkAsOperationalAsync(bayId);
+        }
+
+        private async Task ExecuteMission(string mission)
+        {
+            this.logger.LogInformation($"Executing mission '{mission}'");
+
+            // simulate mission execution
+            await Task.Delay(1000);
+
+            var success = this.random.NextDouble() > 0.5;
+            if (success)
+            {
+                this.logger.LogInformation($"Mission '{mission}' completed.");
+                await this.missionsClient.CompleteAsync(new Mission());
+            }
+            else
+            {
+                this.logger.LogWarning($"Mission '{mission}' failed.");
+                await this.missionsClient.AbortAsync(new Mission());
+            }
+        }
+
+        private async void WakeupReceived(object sender, WakeUpEventArgs e)
+        {
+            this.logger.LogInformation($"Wakeup from Scheduler received.");
+
+            await this.ExecuteMission(e.Message);
+        }
+
+        #endregion Methods
+    }
+}
