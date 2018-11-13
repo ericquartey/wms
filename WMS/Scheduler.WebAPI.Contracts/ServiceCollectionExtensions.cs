dafe713091ba6bc40bdb8573a37d0d938077395f
Alessandro Salomone <alessandro.salomone@altran.it>
2018-11-13 14:31:05 +0100

    feat(scheduler): add extension method to configure WebAPI client classes

---
+++ WMS/Scheduler.WebAPI.Contracts/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ferretto.WMS.Scheduler.WebAPI.Contracts
+{
+    public static class WebAPIServiceCollectionExtensions
+    {
+        #region Methods
+
+        public static IServiceCollection AddWebAPIClients(
+             this IServiceCollection serviceCollection, string baseUrl)
+        {
+            serviceCollection.AddTransient<IItemsClient>(s => new ItemsClient(baseUrl));
+            serviceCollection.AddTransient<IMissionsClient>(s => new MissionsClient(baseUrl));
+            serviceCollection.AddTransient<IBaysClient>(s => new BaysClient(baseUrl));
+
+            return serviceCollection;
+        }
+
+        #endregion Methods
+    }
+}
