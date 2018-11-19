ee6ccacbb5a82daffaef5fa8da0ea81a10421438
Alessandro Salomone <alessandro.salomone@altran.it>
2018-11-19 11:39:29 +0100

    feat(scheduler-request): update models and providers

--- Common/BusinessModels/SchedulerRequest/SchedulerRequest.cs
+++ Common/BusinessModels/SchedulerRequest/SchedulerRequest.cs
@@ -1,14 +1,49 @@
 ﻿namespace Ferretto.Common.BusinessModels
 {
-    public abstract class SchedulerRequest : BusinessObject
+    public class SchedulerRequest : BusinessObject
     {
+        #region Fields
+
+        private int requestedQuantity;
+
+        #endregion Fields
+
         #region Properties
 
         public int? AreaId { get; set; }
         public int? BayId { get; set; }
-        public System.DateTime CreationDate { get; set; }
-        public string OperationType { get; set; }
+        public System.DateTime? CreationDate { get; set; }
+        public bool IsInstant { get; set; }
+        public int ItemId { get; set; }
+        public int? LoadingUnitId { get; set; }
+        public int? LoadingUnitTypeId { get; set; }
+        public string Lot { get; set; }
+        public int? MaterialStatusId { get; set; }
+        public int? PackageTypeId { get; set; }
+        public string RegistrationNumber { get; set; }
+
+        public int RequestedQuantity
+        {
+            get => this.requestedQuantity;
+            set => this.SetIfStrictlyPositive(ref this.requestedQuantity, value);
+        }
+
+        public string Sub1 { get; set; }
+        public string Sub2 { get; set; }
+        public OperationType Type { get; set; }
 
         #endregion Properties
+
+        // public ItemList List { get; set; }
+        // public ItemListRow ListRow { get; set; }
+
+        #region Methods
+
+        public SchedulerRequest Clone()
+        {
+            return (SchedulerRequest)this.MemberwiseClone();
+        }
+
+        #endregion Methods
     }
 }
--- Common/BusinessProviders/Interfaces/ISchedulerRequestProvider.cs
+++ Common/BusinessProviders/Interfaces/ISchedulerRequestProvider.cs
@@ -1,8 +1,16 @@
-﻿using Ferretto.Common.BusinessModels;
+﻿using System.Threading.Tasks;
+using Ferretto.Common.BusinessModels;
 
 namespace Ferretto.Common.BusinessProviders
 {
     public interface ISchedulerRequestProvider : IBusinessProvider<SchedulerRequest, SchedulerRequest>
     {
+        #region Methods
+
+        Task<SchedulerRequest> FullyQualifyRequest(SchedulerRequest schedulerRequest);
+
+        Task<int> GetAvailableQuantity(SchedulerRequest schedulerRequest);
+
+        #endregion Methods
     }
 }
