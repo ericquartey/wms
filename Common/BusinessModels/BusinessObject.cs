d6b1beda60f46bcb96e08bb11c77814b5a4d8883
Alessandro Salomone <alessandro.salomone@altran.it>
2018-11-19 17:33:50 +0100

    feat(Item-details): add change detection for save and revert actions

--- Common/BusinessModels/BusinessObject.cs
+++ Common/BusinessModels/BusinessObject.cs
@@ -1,16 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Ferretto.Common.BLL.Interfaces;
 using Ferretto.Common.Resources;
 
 namespace Ferretto.Common.BusinessModels
 {
-    public abstract class BusinessObject : BindableBase, IBusinessObject
+    public abstract class BusinessObject : BindableBase, ICloneable, IBusinessObject
     {
+        #region Fields
+
+        private bool isModified;
+        private ISet<string> modifiedProperties;
+        private BusinessObject snapshot;
+
+        #endregion Fields
+
         #region Constructors
 
         public BusinessObject()
-        { }
+        {
+            this.PropertyChanged += BusinessObject_PropertyChanged;
+        }
 
         #endregion Constructors
 
@@ -18,9 +29,27 @@ namespace Ferretto.Common.BusinessModels
 
         public int Id { get; set; }
 
+        public bool IsModified
+        {
+            get => this.isModified;
+            private set => this.SetProperty(ref this.isModified, value);
+        }
+
         #endregion Properties
 
         #region Methods
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public void TakeSnapshot()
+        {
+            this.snapshot = this.Clone() as BusinessObject;
+            this.modifiedProperties = new HashSet<string>();
+            this.IsModified = false;
+        }
 
         protected bool SetIfPositive(ref int? member, int? value, [CallerMemberName] string propertyName = null)
         {
@@ -76,6 +105,32 @@ namespace Ferretto.Common.BusinessModels
             }
 
             return this.SetProperty(ref member, value, propertyName);
+        }
+
+        private static void BusinessObject_PropertyChanged(Object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var bo = sender as BusinessObject;
+            if (bo.snapshot == null || e.PropertyName == nameof(IsModified))
+            {
+                return;
+            }
+
+            var propertyInfo = bo.GetType().GetProperty(e.PropertyName);
+            var newValue = propertyInfo.GetValue(sender);
+            var snapshotValue = propertyInfo.GetValue(bo.snapshot);
+            if (newValue.Equals(snapshotValue) == false)
+            {
+                if (bo.modifiedProperties.Contains(e.PropertyName) == false)
+                {
+                    bo.modifiedProperties.Add(e.PropertyName);
+                }
+            }
+            else if (bo.modifiedProperties.Contains(e.PropertyName))
+            {
+                bo.modifiedProperties.Remove(e.PropertyName);
+            }
+
+            bo.IsModified = bo.modifiedProperties.Count > 0;
         }
 
         #endregion Methods
