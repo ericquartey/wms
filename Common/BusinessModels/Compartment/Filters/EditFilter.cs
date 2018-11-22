b84a1bdec002053f0f0acc75449fa45bb1748384
Alice Orsato <alice.orsato@autoware.it>
2018-11-22 14:02:38 +0100

    new filter - color func of compartment

---
+++ Common/BusinessModels/Compartment/Filters/EditFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ferretto.Common.BusinessModels
+{
+    public class EditFilter : IFilter
+    {
+        #region Fields
+
+        private static readonly Func<CompartmentDetails, CompartmentDetails, string> colorFunc =
+            delegate (CompartmentDetails compartment, CompartmentDetails selected)
+            {
+                var color = "#57A639";
+
+                return color;
+            };
+
+        #endregion Fields
+
+        #region Properties
+
+        public Func<CompartmentDetails, CompartmentDetails, string> ColorFunc => colorFunc;
+        public string Description => "Compartment";
+        public int Id => 2;
+        public CompartmentDetails Selected { get; set; }
+
+        #endregion Properties
+    }
+}
