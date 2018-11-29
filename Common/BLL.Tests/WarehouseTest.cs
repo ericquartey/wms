4809374c1baf944a40d7799587b982008ea63bd9
Alessandro Salomone <alessandro.salomone@altran.it>
2018-11-29 11:46:16 +0100

    chore: add tests for bay selection and full bays

--- Common/BLL.Tests/WarehouseTest.cs
+++ Common/BLL.Tests/WarehouseTest.cs
@@ -69,7 +69,6 @@ namespace Ferretto.Common.BLL.Tests
 
             var request1 = new DataModels.SchedulerRequest
             {
-                Id = 1,
                 ItemId = this.itemFifo.Id,
                 AreaId = this.area1.Id,
                 BayId = this.bay1.Id,
@@ -104,7 +103,6 @@ namespace Ferretto.Common.BLL.Tests
 
                 #region Assert
 
-                Assert.IsNotNull(missions);
                 Assert.AreEqual(2, missions.Count());
 
                 var updatedRequest = context.SchedulerRequests.Single(r => r.Id == request1.Id);
@@ -120,12 +118,13 @@ namespace Ferretto.Common.BLL.Tests
         [TestInitialize]
         public void Initialize()
         {
-            this.area1 = new DataModels.Area { Id = 1, Name = "Area #1" };
-            this.aisle1 = new DataModels.Aisle { Id = 1, AreaId = this.area1.Id, Name = "Aisle #1" };
+            this.area1 = new DataModels.Area { Id = 1 };
+            this.aisle1 = new DataModels.Aisle { Id = 1, AreaId = this.area1.Id };
             this.cell1 = new DataModels.Cell { Id = 1, AisleId = this.aisle1.Id };
-            this.loadingUnit1 = new DataModels.LoadingUnit { Id = 1, Code = "Loading Unit #1", CellId = this.cell1.Id };
-            this.bay1 = new DataModels.Bay { Id = 1, Description = "Bay #1", AreaId = this.area1.Id, LoadingUnitsBufferSize = 2 };
-            this.itemFifo = new DataModels.Item { Id = 1, Code = "Item #1", ManagementType = DataModels.ItemManagementType.FIFO };
+            this.loadingUnit1 = new DataModels.LoadingUnit { Id = 1, CellId = this.cell1.Id };
+            this.bay1 = new DataModels.Bay { Id = 1, AreaId = this.area1.Id, LoadingUnitsBufferSize = 2 };
+
+            this.itemFifo = new DataModels.Item { Id = 1, ManagementType = DataModels.ItemManagementType.FIFO };
 
             using (var context = this.CreateContext())
             {
@@ -137,6 +136,165 @@ namespace Ferretto.Common.BLL.Tests
                 context.Items.Add(this.itemFifo);
 
                 context.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        [TestProperty("Description",
+            @"GIVEN a request for an item on an area \
+                AND a compartment that can satisfy the request \
+                AND two bays, one of which has a mission already assigned, while the other one has none \
+               WHEN the request is processed \
+               THEN a new mission is created on the bay that has no missions on it")]
+        public async Task MultipleBays()
+        {
+            #region Arrange
+
+            var now = System.DateTime.Now;
+
+            var mission1 = new DataModels.Mission
+            {
+                BayId = this.bay1.Id,
+                RequiredQuantity = 1,
+                Status = DataModels.MissionStatus.New
+            };
+
+            var compartment1 = new DataModels.Compartment
+            {
+                ItemId = this.itemFifo.Id,
+                LoadingUnitId = this.loadingUnit1.Id,
+                Stock = 10,
+                ReservedForPick = mission1.RequiredQuantity
+            };
+
+            var request1 = new DataModels.SchedulerRequest
+            {
+                ItemId = this.itemFifo.Id,
+                AreaId = this.area1.Id,
+                IsInstant = true,
+                RequestedQuantity = 5,
+                OperationType = DataModels.OperationType.Withdrawal
+            };
+
+            var bay2 = new DataModels.Bay
+            {
+                Id = 2,
+                AreaId = this.area1.Id,
+                LoadingUnitsBufferSize = 2
+            };
+
+            using (var context = this.CreateContext())
+            {
+                context.Compartments.Add(compartment1);
+                context.Bays.Add(bay2);
+                context.Missions.Add(mission1);
+                context.SchedulerRequests.Add(request1);
+
+                context.SaveChanges();
+            }
+
+            #endregion Arrange
+
+            using (var context = this.CreateContext())
+            {
+                #region Act
+
+                var warehouse = new Warehouse(
+                    new DataProvider(context),
+                    new SchedulerRequestProvider(context),
+                    new Mock<ILogger<Warehouse>>().Object);
+
+                var missions = await warehouse.DispatchRequests();
+
+                #endregion Act
+
+                #region Assert
+
+                Assert.AreEqual(1, missions.Count());
+                Assert.AreEqual(bay2.Id, missions.First().BayId);
+
+                #endregion Assert
+            }
+        }
+
+        [TestMethod]
+        [TestProperty("Description",
+            @"GIVEN a request for an item on a bay \
+                AND a compartment that can satisfy the request \
+                AND a bay with no more buffer availability to accept a new mission \
+               WHEN the request is processed \
+               THEN no new missions are created")]
+        public async Task OneFullBay()
+        {
+            #region Arrange
+
+            var now = System.DateTime.Now;
+
+            var request1 = new DataModels.SchedulerRequest
+            {
+                Id = 1,
+                ItemId = this.itemFifo.Id,
+                AreaId = this.area1.Id,
+                IsInstant = true,
+                RequestedQuantity = 5,
+                OperationType = DataModels.OperationType.Withdrawal
+            };
+
+            var mission1 = new DataModels.Mission
+            {
+                Id = 1,
+                BayId = this.bay1.Id,
+                RequiredQuantity = 1,
+                Status = DataModels.MissionStatus.New
+            };
+
+            var mission2 = new DataModels.Mission
+            {
+                Id = 2,
+                BayId = this.bay1.Id,
+                RequiredQuantity = 1,
+                Status = DataModels.MissionStatus.New
+            };
+
+            var compartment1 = new DataModels.Compartment
+            {
+                Id = 1,
+                ItemId = this.itemFifo.Id,
+                LoadingUnitId = this.loadingUnit1.Id,
+                Stock = 100,
+                ReservedForPick = mission1.RequiredQuantity + mission2.RequiredQuantity
+            };
+
+            using (var context = this.CreateContext())
+            {
+                context.Compartments.Add(compartment1);
+                context.Missions.Add(mission1);
+                context.Missions.Add(mission2);
+                context.SchedulerRequests.Add(request1);
+
+                context.SaveChanges();
+            }
+
+            #endregion Arrange
+
+            using (var context = this.CreateContext())
+            {
+                #region Act
+
+                var warehouse = new Warehouse(
+                    new DataProvider(context),
+                    new SchedulerRequestProvider(context),
+                    new Mock<ILogger<Warehouse>>().Object);
+
+                var missions = await warehouse.DispatchRequests();
+
+                #endregion Act
+
+                #region Assert
+
+                Assert.IsFalse(missions.Any());
+
+                #endregion Assert
             }
         }
 
