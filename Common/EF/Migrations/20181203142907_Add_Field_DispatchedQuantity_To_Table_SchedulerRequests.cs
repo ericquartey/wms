6d7f274f1c307146169bcefcb6cc6eefef8e8874
Alessandro Salomone <alessandro.salomone@altran.it>
2018-12-03 15:32:35 +0100

    feat(scheduler): add migrations

---
+++ Common/EF/Migrations/20181203142907_Add_Field_DispatchedQuantity_To_Table_SchedulerRequests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ferretto.Common.EF.Migrations
+{
+    public partial class Add_Field_DispatchedQuantity_To_Table_SchedulerRequests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Missions_Bays_BayId1",
+                table: "Missions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Missions_BayId1",
+                table: "Missions");
+
+            migrationBuilder.DropColumn(
+                name: "BayId1",
+                table: "Missions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Missions",
+                type: "char(1)",
+                nullable: false,
+                defaultValueSql: "'N'",
+                oldClrType: typeof(string),
+                oldType: "char(1)",
+                oldDefaultValueSql: "N");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Missions",
+                type: "char(1)",
+                nullable: false,
+                defaultValueSql: "N",
+                oldClrType: typeof(string),
+                oldType: "char(1)",
+                oldDefaultValueSql: "'N'");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BayId1",
+                table: "Missions",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missions_BayId1",
+                table: "Missions",
+                column: "BayId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Missions_Bays_BayId1",
+                table: "Missions",
+                column: "BayId1",
+                principalTable: "Bays",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
