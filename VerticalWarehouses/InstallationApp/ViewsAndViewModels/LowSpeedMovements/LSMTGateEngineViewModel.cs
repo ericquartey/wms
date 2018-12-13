e374ca8d0e4e531c1dd35caa455aff0e89419b9d
Marco Morbelli <marco.morbelli@altran.it>
2018-12-13 17:05:56 +0100

    Chore():

--- VerticalWarehouses/InstallationApp/ViewsAndViewModels/LowSpeedMovements/LSMTGateEngineViewModel.cs
+++ VerticalWarehouses/InstallationApp/ViewsAndViewModels/LowSpeedMovements/LSMTGateEngineViewModel.cs
@@ -7,8 +7,6 @@ namespace Ferretto.VW.InstallationApp.ViewsAndViewModels.LowSpeedMovements
 {
     internal class LSMTGateEngineViewModel : BindableBase
     {
-        ICommand action;
 
-        public ICommand Action => this.action ?? (this.action = new DelegateCommand(() => Debug.Print("Ciao")));
     }
 }
