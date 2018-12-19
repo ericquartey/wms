4d3a28f9a842eff5f14f46e65acf8664ecab007a
Alessandro Gennaro <agennaro@ferrettogroup.com>
2018-12-19 15:47:31 +0100

    chore: Create a pool of files containing the interfaces for all actions Create a pool of files for the interfaces. Each action class has an abstract interface contained in a single file.

---
+++ VerticalWarehouses/ActionBlocks/Source/ActionsBasic/IDrawerWeightDetection.cs
@@ -0,0 +1,45 @@
+﻿namespace Ferretto.VW.ActionBlocks
+{
+    /// <summary>
+    /// Interface for Drawer weight detection.
+    /// </summary>
+    public interface IDrawerWeightDetection
+    {
+        #region Properties
+
+        /// <summary>
+        /// Set positioning interface.
+        /// </summary>
+        PositioningDrawer SetPositioningDrawerInterface { set; }
+
+        /// <summary>
+        /// Get/Set the weight of drawer.
+        /// </summary>
+        float Weight { get; set; }
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Initialize the weight detection routine.
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Run the routine to detect the weight.
+        /// </summary>
+        /// <param name="d">Target position (relative)</param>
+        /// <param name="v">Speed</param>
+        /// <param name="acc">Acceleration</param>
+        /// <param name="dec">Deceleration</param>
+        void Run(decimal d, float v, float acc, float dec);
+
+        /// <summary>
+        /// Terminate the weight detection routine.
+        /// </summary>
+        void Terminate();
+
+        #endregion Methods
+    }
+}
