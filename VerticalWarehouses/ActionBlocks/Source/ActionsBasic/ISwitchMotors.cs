14e6b72069d906c949910376ffb0e602a4b5f2c8
Ali Asghar Fathi <aliasghar.fathi@altran.it>
2018-12-21 14:45:39 +0100

    feat: Add switching motors action to the actions library

---
+++ VerticalWarehouses/ActionBlocks/Source/ActionsBasic/ISwitchMotors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ferretto.VW.RemoteIODriver.Source;
+
+namespace Ferretto.VW.ActionBlocks
+{
+    public interface ISwitchMotors
+    {
+        RemoteIO SetRemoteIOInterface { set; }
+        InverterDriver.InverterDriver SetInverterDriverInterface { set; }
+        void Initialize();
+        void SwitchHorizToVert();
+        void SwitchVertToHoriz();
+        void Terminate();
+    }
+}
