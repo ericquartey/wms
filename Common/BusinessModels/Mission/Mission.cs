0fe1013ad9a3d6f9a43c961678c7fbb115806328
Alice Orsato <alice.orsato@autoware.it>
2019-01-08 09:52:05 +0100

    [add] Business Model Mission

---
+++ Common/BusinessModels/Mission/Mission.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferretto.Common.BusinessModels
+{
+    public class Mission : BusinessObject
+    {
+        #region Properties
+
+        public int? BayId { get; set; }
+
+        public int? CellId { get; set; }
+
+        public int? CompartmentId { get; set; }
+
+        public int? ItemId { get; set; }
+
+        public int? ItemListId { get; set; }
+
+        public int? ItemListRowId { get; set; }
+
+        public int? LoadingUnitId { get; set; }
+
+        public string Lot { get; set; }
+
+        public int? MaterialStatusId { get; set; }
+
+        public int? PackageTypeId { get; set; }
+
+        public int? Priority { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public int RequiredQuantity { get; set; }
+
+        public MissionStatus Status { get; set; } = MissionStatus.New;
+
+        public string Sub1 { get; set; }
+
+        public string Sub2 { get; set; }
+
+        public MissionType Type { get; set; }
+
+        #endregion Properties
+    }
+}
---
+++ Common/BusinessModels/Mission/MissionType.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Ferretto.Common.Resources;
+
+namespace Ferretto.Common.BusinessModels
+{
+    public enum MissionType
+    {
+        [Display(Name = nameof(BusinessObjects.MissionTypeBypass), ResourceType = typeof(BusinessObjects))]
+        Bypass = 'B',
+
+        [Display(Name = nameof(BusinessObjects.MissionTypeInventory), ResourceType = typeof(BusinessObjects))]
+        Inventory = 'I',
+
+        [Display(Name = nameof(BusinessObjects.MissionTypePick), ResourceType = typeof(BusinessObjects))]
+        Pick = 'P',
+
+        [Display(Name = nameof(BusinessObjects.MissionTypePut), ResourceType = typeof(BusinessObjects))]
+        Put = 'T',
+
+        [Display(Name = nameof(BusinessObjects.MissionTypeReorder), ResourceType = typeof(BusinessObjects))]
+        Reorder = 'O',
+
+        [Display(Name = nameof(BusinessObjects.MissionTypeReplace), ResourceType = typeof(BusinessObjects))]
+        Replace = 'R'
+    }
+}
