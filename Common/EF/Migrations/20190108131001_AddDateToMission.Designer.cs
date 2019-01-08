4c209045822bfa13ee936c1d6353c0b7799a91e6
Alice Orsato <alice.orsato@autoware.it>
2019-01-08 14:16:12 +0100

    [add] fields date to Mission

--- Common/EF/Migrations/20190108131001_AddDateToMission.Designer.cs
+++ Common/EF/Migrations/20190108131001_AddDateToMission.Designer.cs
@@ -4,14 +4,16 @@ using Ferretto.Common.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ferretto.Common.EF.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190108131001_AddDateToMission")]
+    partial class AddDateToMission
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
