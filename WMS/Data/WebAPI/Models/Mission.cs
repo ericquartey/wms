2d14ea2d2cd30717075726ccf2068bf6c26a1021
Alessandro Salomone <alessandro.salomone@altran.it>
2019-01-11 11:23:05 +0100

    feat(data-service): update business models for missions and lists

--- WMS/Data/WebAPI/Models/Mission.cs
+++ WMS/Data/WebAPI/Models/Mission.cs
@@ -2,5 +2,50 @@
 {
     public class Mission : Model<int>
     {
+        #region Fields
+
+        private int quantity;
+
+        #endregion Fields
+
+        #region Properties
+
+        public int? BayId { get; set; }
+
+        public int? CellId { get; set; }
+
+        public int? CompartmentId { get; set; }
+
+        public int? ItemId { get; set; }
+
+        public int? ItemListId { get; set; }
+
+        public int? ItemListRowId { get; set; }
+
+        public int? LoadingUnitId { get; set; }
+
+        public string Lot { get; set; }
+
+        public int? MaterialStatusId { get; set; }
+
+        public int? PackageTypeId { get; set; }
+
+        public int Quantity
+        {
+            get => this.quantity;
+            set => this.quantity = this.CheckIfStrictlyPositive(value);
+        }
+
+        public string RegistrationNumber { get; set; }
+
+        public MissionStatus Status { get; set; } = MissionStatus.New;
+
+        public string Sub1 { get; set; }
+
+        public string Sub2 { get; set; }
+
+        public MissionType Type { get; set; }
+
+        #endregion Properties
     }
 }
