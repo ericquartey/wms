c9f60d8a5a4a4338c809376c45e226027b0e526a
Alice Orsato <alice.orsato@autoware.it>
2019-01-11 11:09:27 +0100

    [add] new business models

---
+++ Common/BusinessModels/SchedulerRequest/OperationType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Ferretto.Common.Resources;
+
+namespace Ferretto.Common.BusinessModels
+{
+    public enum OperationType
+    {
+        [Display(Name = nameof(BusinessObjects.OperationInsert), ResourceType = typeof(BusinessObjects))]
+        Insertion = 'I',
+
+        [Display(Name = nameof(BusinessObjects.OperationWithdraw), ResourceType = typeof(BusinessObjects))]
+        Withdrawal = 'W',
+
+        [Display(Name = nameof(BusinessObjects.OperationReplace), ResourceType = typeof(BusinessObjects))]
+        Replacement = 'R',
+
+        [Display(Name = nameof(BusinessObjects.OperationReorder), ResourceType = typeof(BusinessObjects))]
+        Reorder = 'O'
+    }
+}
