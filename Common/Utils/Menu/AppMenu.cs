82fafbc9f15a30c7633134df83468991ff471287
Alessandro Salomone <alessandro.salomone@altran.it>
2019-01-18 10:52:08 +0100

    feature(app-menu): remove list types from menus

--- Common/Utils/Menu/AppMenu.cs
+++ Common/Utils/Menu/AppMenu.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using Ferretto.Common.Resources;
+﻿using Ferretto.Common.Resources;
 
 namespace Ferretto.Common.Utils.Menu
 {
@@ -85,34 +83,34 @@ namespace Ferretto.Common.Utils.Menu
             var schedulerMenu =
                 new MainMenuItem(Navigation.Scheduler, bgColor, Icons.Scheduler, string.Empty, string.Empty);
             this.menu.Items.Add(schedulerMenu);
-               schedulerMenu.Children.AddRange(new[]
-               {
+            schedulerMenu.Children.AddRange(new[]
+            {
                    new MainMenuItem(Navigation.Missions, bgColor, nameof(Navigation.Missions), nameof(Modules.Scheduler), Modules.Scheduler.MISSIONS),
                    new MainMenuItem(Navigation.SchedulerRequests, bgColor, nameof(Navigation.SchedulerRequests), nameof(Modules.Scheduler), Modules.Scheduler.SCHEDULERREQUESTS),
                });
 
-               var importExportMenu = new MainMenuItem(
-                   Navigation.ImportExport,
-                   bgColor,
-                   Icons.ImportExport,
-                   string.Empty,
-                   string.Empty);
-               this.menu.Items.Add(importExportMenu);
-               importExportMenu.Children.AddRange(new[]
-               {
+            var importExportMenu = new MainMenuItem(
+                Navigation.ImportExport,
+                bgColor,
+                Icons.ImportExport,
+                string.Empty,
+                string.Empty);
+            this.menu.Items.Add(importExportMenu);
+            importExportMenu.Children.AddRange(new[]
+            {
                    new MainMenuItem(Navigation.Import, bgColor, nameof(Navigation.Import), string.Empty, string.Empty),
                    new MainMenuItem(Navigation.Export, bgColor, nameof(Navigation.Export), string.Empty, string.Empty)
                });
 
-               var configurationMenu = new MainMenuItem(
-                   Navigation.Configuration,
-                   bgColor,
-                   Icons.Configuration,
-                   string.Empty,
-                   string.Empty);
-               this.menu.Items.Add(configurationMenu);
-               configurationMenu.Children.AddRange(new[]
-               {
+            var configurationMenu = new MainMenuItem(
+                Navigation.Configuration,
+                bgColor,
+                Icons.Configuration,
+                string.Empty,
+                string.Empty);
+            this.menu.Items.Add(configurationMenu);
+            configurationMenu.Children.AddRange(new[]
+            {
                    new MainMenuItem(Navigation.MeasureUnits, bgColor, nameof(Navigation.MeasureUnits), string.Empty, string.Empty),
                    new MainMenuItem(Navigation.AbcClasses, bgColor, nameof(Navigation.AbcClasses), string.Empty, string.Empty),
                    new MainMenuItem(Navigation.CellStatuses, bgColor, nameof(Navigation.CellStatuses), string.Empty, string.Empty),
@@ -121,16 +119,13 @@ namespace Ferretto.Common.Utils.Menu
                    new MainMenuItem(Navigation.CompartmentStatuses, bgColor, nameof(Navigation.CompartmentStatuses), string.Empty, string.Empty),
                    new MainMenuItem(Navigation.PackageTypes, bgColor, nameof(Navigation.PackageTypes), string.Empty, string.Empty),
                    new MainMenuItem(Navigation.MaterialStatuses, bgColor, nameof(Navigation.MaterialStatuses), string.Empty, string.Empty),
-                   new MainMenuItem(Navigation.ListTypes, bgColor,  nameof(Navigation.ListTypes), string.Empty, string.Empty),
-                   new MainMenuItem(Navigation.ListStatuses, bgColor, nameof(Navigation.ListStatuses),  string.Empty, string.Empty),
-                   new MainMenuItem(Navigation.ListRowStatuses, bgColor, nameof(Navigation.ListRowStatuses), string.Empty, string.Empty),
                    new MainMenuItem(Navigation.MachineTypes, bgColor, nameof(Navigation.MachineTypes), string.Empty, string.Empty)
                });
 
-               var othersMenu = new MainMenuItem(Navigation.Others, bgColor, Icons.Others, string.Empty, string.Empty);
-               this.menu.Items.Add(othersMenu);
-               othersMenu.Children.AddRange(new[]
-               {
+            var othersMenu = new MainMenuItem(Navigation.Others, bgColor, Icons.Others, string.Empty, string.Empty);
+            this.menu.Items.Add(othersMenu);
+            othersMenu.Children.AddRange(new[]
+            {
                    new MainMenuItem(Navigation.UnderStockItems, bgColor, nameof(Navigation.UnderStockItems), string.Empty, string.Empty),
                    new MainMenuItem(Navigation.Inventory, bgColor, nameof(Navigation.Inventory), string.Empty, string.Empty)
                });
