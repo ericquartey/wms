e3fde3bf8d3bbc2588008ca5ab4007dfc14474f7
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-01-28 13:53:06 +0100

    Changed default namespaces for all the MAS folder projects

---
+++ VerticalWarehouses/Machine Automation Service/MAS_MachineManager/MachineManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferretto.VW.MAS_MachineManager
+{
+	public class MachineManager
+	{
+		MachineManager()
+		{
+		}
+
+		class MachineManagerCreator
+		{
+			static MachineManagerCreator() { }
+
+			internal static readonly MachineManager uniqueInstance = new MachineManager();
+		}
+
+		public static MachineManager Instance
+		{
+			get { return MachineManagerCreator.uniqueInstance; }
+		}
+	}
+}
