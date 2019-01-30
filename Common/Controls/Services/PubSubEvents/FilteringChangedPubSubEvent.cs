75066d8255f38965d69207f6117a849603320207
Alessandro Salomone <alessandro.salomone@altran.it>
2019-01-30 15:06:40 +0100

    feat(filter-dialog): add filter changed event

---
+++ Common/Controls/Services/PubSubEvents/FilteringChangedPubSubEvent.cs
@@ -0,0 +1,37 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.Xpf.Core.FilteringUI;
+using Ferretto.Common.BLL.Interfaces;
+
+namespace Ferretto.Common.Controls.Services
+{
+    public class FilteringChangedPubSubEvent : Prism.Events.PubSubEvent, IPubSubEvent
+    {
+        #region Fields
+
+        private readonly CriteriaOperator filter;
+
+        private readonly FilteringUIContext filteringContext;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public FilteringChangedPubSubEvent(CriteriaOperator filter, FilteringUIContext filteringContext)
+        {
+            this.filter = filter;
+            this.filteringContext = filteringContext;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public object Filter => this.filter;
+
+        public object FilteringContext => this.filteringContext;
+
+        public string Token { get; }
+
+        #endregion Properties
+    }
+}
