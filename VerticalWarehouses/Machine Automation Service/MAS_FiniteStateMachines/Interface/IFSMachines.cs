d1ab792b133c98f02bb2d90ee79146c852a4090d
Alessandro Gennaro <agennaro@ferrettogroup.com>
2019-01-30 18:07:35 +0100

    feat: Create Finite State Machines structure

---
+++ VerticalWarehouses/Machine Automation Service/MAS_FiniteStateMachines/Interface/IFSMachines.cs
@@ -0,0 +1,43 @@
+﻿namespace Ferretto.VW.MAS_FiniteStateMachines
+{
+    public interface IFSMachines
+    {
+        #region Methods
+
+        void DoHoming();
+
+        void DoVerticalHoming();
+
+        #endregion Methods
+    }
+
+    // --------------------------
+    // Interface IState
+    public interface IState
+    {
+        #region Properties
+
+        string Type { get; }
+
+        #endregion Properties
+
+        #region Methods
+
+        void DoAction(IdOperation code);
+
+        #endregion Methods
+    }
+
+    public interface IStateMachine
+    {
+        #region Methods
+
+        void ChangeState(IState newState);
+
+        void ExecuteOperation(IdOperation code);
+
+        void Start();
+
+        #endregion Methods
+    }
+}
