166efdcdf401d51112e38302a01f4a40089a8376
Alessandro Gennaro <agennaro@ferrettogroup.com>
2019-01-31 12:11:39 +0100

    chore: Use a treeview structure for the Finite State Machines project (use of subfolders)

---
+++ VerticalWarehouses/Machine Automation Service/MAS_FiniteStateMachines/Enum.cs
@@ -0,0 +1,13 @@
+﻿namespace Ferretto.VW.MAS_FiniteStateMachines
+{
+    public enum IdOperation
+    {
+        HorizontalHome = 0,
+
+        VerticalHome = 1,
+
+        SwitchHorizontalToVertical = 2,
+
+        SwitchVerticalToHorizontal = 3
+    }
+}
---
+++ VerticalWarehouses/Machine Automation Service/MAS_FiniteStateMachines/Homing/VerticalSwitchDoneState.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Ferretto.VW.MAS_FiniteStateMachines.Homing
+{
+    // The vertical switch is done
+    public class VerticalSwitchDoneState : IState
+    {
+        #region Fields
+
+        private StateMachineHoming context;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public VerticalSwitchDoneState(StateMachineHoming parent)
+        {
+            this.context = parent;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string Type => "Vertical Switch Done";
+
+        #endregion Properties
+
+        #region Methods
+
+        public void DoAction(IdOperation code)
+        {
+            switch (code)
+            {
+                case IdOperation.HorizontalHome:
+                    {
+                        Console.WriteLine("Invalid operation");
+                        break;
+                    }
+                case IdOperation.SwitchHorizontalToVertical:
+                    {
+                        Console.WriteLine("Invalid operation");
+                        break;
+                    }
+                case IdOperation.SwitchVerticalToHorizontal:
+                    {
+                        Console.WriteLine("Invalid operation");
+                        break;
+                    }
+                case IdOperation.VerticalHome:
+                    {
+                        //TODO inverterDriver.VerticalHoming();
+                        //TODO await isOperationDone();
+                        this.context.ExecuteOperation(code);
+                        this.context.ChangeState(new VerticalHomingDoneState(this.context));
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid operation");
+                        break;
+                    }
+            }
+        }
+
+        #endregion Methods
+    }
+}
