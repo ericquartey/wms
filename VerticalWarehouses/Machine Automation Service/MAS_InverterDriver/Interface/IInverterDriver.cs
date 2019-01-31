33881015fa86af642f824827df4c33d497b22672
Ali Asghar Fathi <aliasghar.fathi@altran.it>
2019-01-31 16:34:51 +0100

    feat:Define the Interface for MAS_InverterDriver

--- VerticalWarehouses/Machine Automation Service/MAS_InverterDriver/Interface/IInverterDriver.cs
+++ VerticalWarehouses/Machine Automation Service/MAS_InverterDriver/Interface/IInverterDriver.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Ferretto.VW.InverterDriver.Interface
+namespace Ferretto.VW.MAS_InverterDriver
 {
     public interface IInverterDriver
     {
+
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+
+        void ExecuteVerticalHoming();
+        void ExecuteHorizontalHoming();
+        void ExecuteVerticalPosition(long target, float weight);
+        void ExecuteHorizontalPosition();
+        bool[] GetSensorsStates();
+        float GetDrawerWeight();
+
+        #endregion Methods  
     }
-}
+}   
--- VerticalWarehouses/Machine Automation Service/MAS_InverterDriver/InverterDriver.cs
+++ VerticalWarehouses/Machine Automation Service/MAS_InverterDriver/InverterDriver.cs
@@ -1,11 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Ferretto.VW.InverterDriver.Interface;
+using Ferretto.VW.MAS_InverterDriver;
 
-namespace Ferretto.VW.InverterDriver
+namespace Ferretto.VW.MAS_InverterDriver
 {
     public class InverterDriver : IInverterDriver
     {
+        #region Fields
+        #endregion Fields
+
+        #region Constructors
+
+        public InverterDriver()
+        {
+            return;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+        public void ExecuteVerticalHoming()
+        {
+            return;
+        }
+
+        public void ExecuteHorizontalHoming()
+        {
+            return;
+        }
+
+        public void ExecuteVerticalPosition(long target, float weight)
+        {
+            return;
+        }
+
+       public void ExecuteHorizontalPosition()
+       {
+            return;
+       }
+
+        public bool[] GetSensorsStates()
+        {
+            return null;
+        }
+
+        public float GetDrawerWeight()
+        {
+            return 0.0f;
+        }
+
+        #endregion Methods
     }
 }
