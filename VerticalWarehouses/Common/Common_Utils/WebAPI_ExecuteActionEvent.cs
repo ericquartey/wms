41006fa661f51d7b888e077423ddf44b5fe5df48
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-02-05 17:14:25 +0100

    Added WebAPI_ExecuteActionEvent class and WebAPI_Action enum

---
+++ VerticalWarehouses/Common/Common_Utils/WebAPI_ExecuteActionEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Events;
+
+namespace Ferretto.VW.Common_Utils
+{
+    public enum WebAPI_Action
+    {
+        VerticalHoming
+    }
+
+    internal class WebAPI_ExecuteActionEvent : PubSubEvent<WebAPI_Action>
+    {
+    }
+}
