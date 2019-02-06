5f863d94de5ce87efc40a52d96a146449f20b09e
Ali Asghar Fathi <aliasghar.fathi@altran.it>
2019-02-06 13:23:21 +0100

    feat:Transfering CalibrateAxis class from ActionBlocks project into the MAS_InverterDriver project with event aggregator and Creating unit test project for MAS_InverterDriver

---
+++ VerticalWarehouses/Machine Automation Service/MAS_InverterDriver/InverterDriver_CalibrateAxis.cs
@@ -0,0 +1,46 @@
+﻿using Ferretto.VW.Common_Utils.Events;
+using Ferretto.VW.InverterDriver;
+using Ferretto.VW.MAS_InverterDriver.Interface;
+using Prism.Events;
+using Ferretto.VW.MAS_InverterDriver.ActionBlocks;
+
+namespace Ferretto.VW.MAS_InverterDriver
+{
+    public partial class InverterDriver
+    {
+        #region Methods
+
+        public void ExecuteVerticalHoming()
+        {
+            if(this.inverterAction != null)
+            {
+                this.eventAggregator.GetEvent<InverterDriver_NotificationEvent>().Publish(InverterDriver_Notification.Error);
+            }
+            var inverterAction = new ActionBlocks.CalibrateAxis();
+            this.inverterAction = inverterAction;
+
+            this.inverterAction.EndEvent += this.Calibration_ThrowEndEvent;
+            this.inverterAction.ErrorEvent += this.Calibration_ThrowErrorEvent;
+            inverterAction.SetInverterDriverInterface = this.driver;
+            inverterAction.Initialize();
+
+            inverterAction.ActualCalibrationAxis = CalibrationType.VERTICAL_CALIBRATION;
+            inverterAction.SetAxisOrigin();
+            
+        }
+
+        private void Calibration_ThrowErrorEvent()
+        {
+            this.eventAggregator.GetEvent<InverterDriver_NotificationEvent>().Publish(InverterDriver_Notification.Error);
+            this.inverterAction.ErrorEvent -= this.Calibration_ThrowErrorEvent;
+        }
+
+        private void Calibration_ThrowEndEvent()
+        {
+            this.eventAggregator.GetEvent<InverterDriver_NotificationEvent>().Publish(InverterDriver_Notification.End);
+            this.inverterAction.EndEvent -= this.Calibration_ThrowEndEvent;
+        }
+
+        #endregion Methods
+    }
+}
