ea5c0c8286e3b6510ef213ab9ec6999ecfcd4e33
Alice Orsato <alice.orsato@autoware.it>
2019-02-06 11:26:08 +0100

    [update] control Label, add possibility of required field

---
+++ Common/Controls/Controls/TextEdit/WmsLabel.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ferretto.Common.Controls
+{
+    /// <summary>
+    /// Interaction logic for WmsLabel.xaml
+    /// </summary>
+    public partial class WmsLabel : UserControl
+    {
+        #region Fields
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.RegisterAttached(
+                   nameof(Title), typeof(string), typeof(WmsLabel), new UIPropertyMetadata());
+
+        #endregion
+
+        #region Constructors
+
+        public WmsLabel()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Title { get => (string)this.GetValue(TitleProperty); set => this.SetValue(TitleProperty, value); }
+
+        #endregion
+    }
+}
