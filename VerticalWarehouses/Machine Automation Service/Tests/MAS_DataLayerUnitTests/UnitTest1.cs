c071ea6180ce3e7b22903446cfed8d4ab92c22f8
Marco Morbelli <marco.morbelli@altran.it>
2019-02-07 10:05:47 +0100

    feat(): Datalayer Unit Test created

--- VerticalWarehouses/Machine Automation Service/Tests/MAS_DataLayerUnitTests/UnitTest1.cs
+++ VerticalWarehouses/Machine Automation Service/Tests/MAS_DataLayerUnitTests/UnitTest1.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ferretto.VW.MAS_DataLayer;
 using Prism.Events;
 using Moq;
+using Ferretto.Common.Common_Utils;
 
 namespace MAS_DataLayerUnitTests
 {
@@ -16,6 +17,8 @@ namespace MAS_DataLayerUnitTests
                 // Arrange
                 bool updateFeedback = false;
                 Mock<IEventAggregator> mockEventAggregator = new Mock<IEventAggregator>();
+                mockEventAggregator.Setup(s => s.GetEvent<WebAPI_ExecuteActionEvent>()).Returns(new WebAPI_ExecuteActionEvent());
+
                 var writeLogService = new WriteLogService(context, mockEventAggregator.Object);
 
                 // Act
