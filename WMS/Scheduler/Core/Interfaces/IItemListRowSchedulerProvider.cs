42418d8e12647c22f6194228c8d6ee17b8ec1cbd
Alessandro Salomone <alessandro.salomone@altran.it>
2019-02-12 17:13:24 +0100

    feat(item-list-row): create scheduler provider

---
+++ WMS/Scheduler/Core/Interfaces/IItemListRowSchedulerProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Ferretto.WMS.Data.Core.Models;
+
+namespace Ferretto.WMS.Scheduler.Core.Interfaces
+{
+    public interface IItemListRowSchedulerProvider
+    {
+        #region Methods
+
+        Task<OperationResult<ItemListRow>> UpdateAsync(ItemListRow model);
+
+        #endregion
+    }
+}
---
+++ WMS/Scheduler/Core/Providers/ItemListRowSchedulerProvider.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Ferretto.Common.EF;
+using Ferretto.WMS.Data.Core.Models;
+using Ferretto.WMS.Scheduler.Core.Interfaces;
+
+namespace Ferretto.WMS.Scheduler.Core.Providers
+{
+    internal class ItemListRowSchedulerProvider : IItemListRowSchedulerProvider
+    {
+        #region Fields
+
+        private readonly DatabaseContext databaseContext;
+
+        #endregion
+
+        #region Constructors
+
+        public ItemListRowSchedulerProvider(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<OperationResult<ItemListRow>> UpdateAsync(ItemListRow model)
+        {
+            if (model == null)
+            {
+                throw new System.ArgumentNullException(nameof(model));
+            }
+
+            var existingRow = this.databaseContext.ItemListRows.Find(model.Id);
+            this.databaseContext.Entry(existingRow).CurrentValues.SetValues(model);
+
+            await this.databaseContext.SaveChangesAsync();
+
+            return new SuccessOperationResult<ItemListRow>(model);
+        }
+
+        #endregion
+    }
+}
