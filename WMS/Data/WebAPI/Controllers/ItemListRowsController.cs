49d4c98819d12c0b07254a8b5ad6125f358c998c
Alessandro Salomone <alessandro.salomone@altran.it>
2019-02-12 11:31:05 +0100

    refactor: move item list rows controller

---
+++ WMS/Data/WebAPI/Controllers/ItemListRowsController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ferretto.WMS.Scheduler.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemListRowsController : ControllerBase
+    {
+        #region Fields
+
+        private readonly ILogger logger;
+
+        private readonly IServiceProvider serviceProvider;
+
+        private readonly Core.IWarehouse warehouse;
+
+        #endregion
+
+        #region Constructors
+
+        public ItemListRowsController(
+            IServiceProvider serviceProvider,
+            ILogger<ItemListRowsController> logger,
+            Core.IWarehouse warehouse)
+        {
+            this.logger = logger;
+            this.serviceProvider = serviceProvider;
+            this.warehouse = warehouse;
+        }
+
+        #endregion
+
+        #region Methods
+
+        [HttpPost(nameof(Execute))]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(422)]
+        public async Task<ActionResult> Execute(ListRowExecutionRequest request)
+        {
+            if (request == null)
+            {
+                return this.BadRequest();
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            try
+            {
+                var acceptedRequest = await this.warehouse.PrepareListRowForExecutionAsync(request.ListRowId, request.AreaId, request.BayId);
+                if (acceptedRequest == null)
+                {
+                    this.logger.LogWarning($"Request of execution for list row (id={request.ListRowId}) could not be processed.");
+
+                    return this.UnprocessableEntity(this.ModelState);
+                }
+
+                this.logger.LogInformation($"Request of execution for list row (id={request.ListRowId}) was accepted.");
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, $"An error occurred while processing the execution request for list row (id={request.ListRowId}).");
+                return this.BadRequest(ex.Message);
+            }
+
+            return this.Ok();
+        }
+
+        #endregion
+    }
+}
