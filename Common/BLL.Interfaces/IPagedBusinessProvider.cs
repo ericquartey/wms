60c0290269633cb1bf4e69f8fb50b42fae2ceb85
Alice Orsato <alice.orsato@autoware.it>
2019-02-12 10:37:57 +0100

    [update] IPagedBusinessProvider with interface behaviour

--- Common/BLL.Interfaces/IPagedBusinessProvider.cs
+++ Common/BLL.Interfaces/IPagedBusinessProvider.cs
@@ -1,27 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Ferretto.Common.BLL.Interfaces.Base;
 using Ferretto.Common.Utils.Expressions;
 
 namespace Ferretto.Common.BLL.Interfaces
 {
-    public interface IPagedBusinessProvider<TModel>
+    public interface IPagedBusinessProvider<TModel> :
+        IReadAllPagedAsyncProvider<TModel>,
+        IGetUniqueValuesAsyncProvider
         where TModel : IBusinessObject
     {
-        #region Methods
-
-        Task<IEnumerable<TModel>> GetAllAsync(
-          int take = 0,
-          int skip = 0,
-          string whereExpression = null,
-          IEnumerable<SortOption> orderBy = null,
-          string searchExpression = null);
-
-        Task<int> GetAllCountAsync(
-          string whereExpression = null,
-          string searchExpression = null);
-
-        Task<IEnumerable<object>> GetUniqueValuesAsync(string propertyName);
-
-        #endregion
     }
 }
