a66cbeadbc8deef6e5151408282c0fa9ab40802b
Alice Orsato <alice.orsato@autoware.it>
2019-02-12 12:00:25 +0100

    [update] item provider to async method + add register AbcDataService

--- Common/BusinessProviders/Interfaces/IItemProvider.cs
+++ Common/BusinessProviders/Interfaces/IItemProvider.cs
@@ -1,17 +1,23 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Ferretto.Common.BLL.Interfaces;
+using Ferretto.Common.BLL.Interfaces.Base;
 using Ferretto.Common.BusinessModels;
 
 namespace Ferretto.Common.BusinessProviders
 {
-    public interface IItemProvider : IBusinessProvider<Item, ItemDetails>, IPagedBusinessProvider<Item>
+    public interface IItemProvider :
+        IPagedBusinessProvider<Item>,
+        ICreateAsyncProvider<ItemDetails>,
+        IReadSingleAsyncProvider<ItemDetails, int>,
+        IUpdateAsyncProvider<ItemDetails>,
+        IDeleteAsyncProvider
     {
         #region Methods
 
         IQueryable<AllowedItemInCompartment> GetAllowedByCompartmentId(int compartmentId);
 
-        ItemDetails GetNew();
+        Task<ItemDetails> GetNewAsync();
 
         bool HasAnyCompartments(int itemId);
 
