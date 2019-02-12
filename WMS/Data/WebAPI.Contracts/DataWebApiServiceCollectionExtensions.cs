5a57f5bce5c2717289fc5264b639c032c5ac1087
Alessandro Salomone <alessandro.salomone@altran.it>
2019-02-12 15:34:26 +0100

    refactor: suppress S1200 CA warning for service extensions classes

--- WMS/Data/WebAPI.Contracts/DataWebApiServiceCollectionExtensions.cs
+++ WMS/Data/WebAPI.Contracts/DataWebApiServiceCollectionExtensions.cs
@@ -2,6 +2,10 @@
 
 namespace Ferretto.WMS.Data.WebAPI.Contracts
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+       "Major Code Smell",
+       "S1200:Classes should not be coupled to too many other classes (Single Responsibility Principle)",
+       Justification = "This class register services into container")]
     public static class DataWebApiServiceCollectionExtensions
     {
         #region Methods
--- WMS/Scheduler/Core/Extensions/ServiceCollectionSchedulerExtensions.cs
+++ WMS/Scheduler/Core/Extensions/ServiceCollectionSchedulerExtensions.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Ferretto.WMS.Scheduler.Core.Interfaces;
+using Ferretto.WMS.Scheduler.Core.Providers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Ferretto.WMS.Scheduler.Core.Extensions
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+       "Major Code Smell",
+       "S1200:Classes should not be coupled to too many other classes (Single Responsibility Principle)",
+       Justification = "This class register services into container")]
     public static class ServiceCollectionSchedulerExtensions
     {
         #region Methods
