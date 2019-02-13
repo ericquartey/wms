41b6930f6de1e9226e651be436797c1b8f0ba3c9
Alice Orsato <alice.orsato@autoware.it>
2019-02-13 13:48:48 +0100

    register data service & new providers

--- WMS/Data/WebAPI.Contracts/DataWebApiServiceCollectionExtensions.cs
+++ WMS/Data/WebAPI.Contracts/DataWebApiServiceCollectionExtensions.cs
@@ -17,7 +17,19 @@ namespace Ferretto.WMS.Data.WebAPI.Contracts
             serviceCollection.AddTransient(s => DataServiceFactory.GetService<IItemsDataService>(baseUrl));
             serviceCollection.AddTransient(s => DataServiceFactory.GetService<IMissionsDataService>(baseUrl));
             serviceCollection.AddTransient(s => DataServiceFactory.GetService<IItemListsDataService>(baseUrl));
+
             serviceCollection.AddTransient(s => DataServiceFactory.GetService<IAbcClassesDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<ICellPositionsDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<ICellStatusesDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<ICellTypesDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<ICompartmentStatusesDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<ICompartmentTypesDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<IItemCategoriesDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<ILoadingUnitStatusesDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<ILoadingUnitTypesDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<IMaterialStatusesDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<IMeasureUnitsDataService>(baseUrl));
+            serviceCollection.AddTransient(s => DataServiceFactory.GetService<IPackageTypesDataService>(baseUrl));
 
             return serviceCollection;
         }
