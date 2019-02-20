68fe3cbd5e8d51d7288227170276c849bb002faa
Alessandro Salomone <alessandro.salomone@altran.it>
2019-02-20 11:35:40 +0100

    chore: add migration for addition of Bays.IsActive field

--- Common/EF/Migrations/20190220103310_Add_Field_IsActive_To_Table_Bays.Designer.cs
+++ Common/EF/Migrations/20190220103310_Add_Field_IsActive_To_Table_Bays.Designer.cs
@@ -4,14 +4,16 @@ using Ferretto.Common.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ferretto.Common.EF.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190220103310_Add_Field_IsActive_To_Table_Bays")]
+    partial class Add_Field_IsActive_To_Table_Bays
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
