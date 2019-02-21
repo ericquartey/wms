dda4a1eb6f4038cfd50bfe76f12cd6f5a3129cd2
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-02-21 14:15:37 +0100

    Added properties and enums to MissionData class

--- VerticalWarehouses/Common/Common_Utils/Messages/Data/MissionData.cs
+++ VerticalWarehouses/Common/Common_Utils/Messages/Data/MissionData.cs
@@ -6,6 +6,14 @@ namespace Ferretto.VW.Common_Utils.Messages.Data
     {
         #region Properties
 
+        public int BayID { get; set; }
+
+        public int CellID { get; set; }
+
+        public int DrawerID { get; set; }
+
+        public MissionType MissionType { get; set; }
+
         public int Priority { get; set; }
 
         #endregion
--- VerticalWarehouses/Common/Common_Utils/Messages/Interfaces/IMissionMessageData.cs
+++ VerticalWarehouses/Common/Common_Utils/Messages/Interfaces/IMissionMessageData.cs
@@ -6,9 +6,26 @@ using System.Threading.Tasks;
 
 namespace Ferretto.VW.Common_Utils.Messages.Interfaces
 {
+    public enum MissionType
+    {
+        BayToCellMission,
+
+        CellToBayMission,
+
+        CellToCellMission
+    }
+
     public interface IMissionMessageData : IEventMessageData
     {
         #region Properties
+
+        int BayID { get; set; }
+
+        int CellID { get; set; }
+
+        int DrawerID { get; set; }
+
+        MissionType MissionType { get; set; }
 
         int Priority { get; set; }
 
