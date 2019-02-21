5689c5a0afb172341757e2a989c222b4a870151d
Marco Morbelli <marco.morbelli@altran.it>
2019-02-21 10:49:28 +0100

    feat(): split of DataLayer class with a partial class

---
+++ VerticalWarehouses/Machine Automation Service/MAS_DataLayer/DataLayer_CellsManagment.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ferretto.VW.MAS_DataLayer
+{
+    public partial class DataLayer : IDataLayer
+    {
+        #region Methods
+
+        public List<Cell> GetCellList()
+        {
+            var listCells = new List<Cell>();
+            foreach (var cell in this.inMemoryDataContext.Cells)
+            {
+                listCells.Add(cell);
+            }
+            return listCells;
+        }
+
+        public ReturnMissionPosition GetFreeBlockPosition(int drawerHeight)
+        {
+            var cellSpacing = this.GetIntegerConfigurationValue(ConfigurationValueEnum.cellSpacing);
+
+            // TEMP Drawer height conversion to the necessary cells number Ceiling to round a double to the upper integer
+            var cellNumber = (int)Math.Ceiling((decimal)drawerHeight / cellSpacing);
+
+            var cellEven = new Cell();
+            var cellOdd = new Cell();
+
+            var cellCounterEven = 0;
+            var cellCounterOdd = 0;
+
+            var returnMissionPosition = new ReturnMissionPosition();
+
+            foreach (var cell in this.inMemoryDataContext.Cells.OrderBy(cell => cell.CellId))
+            {
+                if (cell.Side == Side.FrontEven)
+                {
+                    if (cell.Status == Status.Free && cellCounterEven == 0)
+                    {
+                        cellEven = cell;
+                        cellCounterEven++;
+                    }
+
+                    if (cell.Status == Status.Free && cellCounterEven != 0)
+                    {
+                        cellCounterEven++;
+                    }
+
+                    if (cell.Status == Status.Disabled && cellCounterEven != 0)
+                    {
+                        cellCounterEven++;
+                    }
+
+                    if (cell.Status != Status.Occupied || cell.Status == Status.Unusable)
+                    {
+                        cellCounterEven = 0;
+                    }
+                }
+                else
+                {
+                    if (cell.Status == Status.Free && cellCounterOdd == 0)
+                    {
+                        cellOdd = cell;
+                        cellCounterOdd++;
+                    }
+
+                    if (cell.Status == Status.Free && cellCounterOdd != 0)
+                    {
+                        cellCounterOdd++;
+                    }
+
+                    if (cell.Status == Status.Disabled && cellCounterOdd != 0)
+                    {
+                        cellCounterOdd++;
+                    }
+
+                    if (cell.Status != Status.Occupied || cell.Status == Status.Unusable)
+                    {
+                        cellCounterOdd = 0;
+                    }
+                }
+
+                // INFO - if the block is high or higher the drawer we end to search for the block
+                if (cellCounterEven >= cellNumber)
+                {
+                    returnMissionPosition.ReturnCoord = cellEven.Coord;
+                    returnMissionPosition.ReturnSide = cellEven.Side;
+
+                    break;
+                }
+
+                if (cellCounterOdd >= cellNumber)
+                {
+                    returnMissionPosition.ReturnCoord = cellOdd.Coord;
+                    returnMissionPosition.ReturnSide = cellOdd.Side;
+
+                    break;
+                }
+            }
+
+            return returnMissionPosition;
+        }
+
+        public bool SetCellList(List<Cell> listCells)
+        {
+            var setCellList = false;
+
+            if (listCells != null)
+            {
+                setCellList = true;
+
+                foreach (var cell in listCells)
+                {
+                    var inMemoryCellCurrentValue = this.inMemoryDataContext.Cells.FirstOrDefault(s => s.CellId == cell.CellId);
+
+                    if (inMemoryCellCurrentValue != null)
+                    {
+                        inMemoryCellCurrentValue.Coord = cell.Coord;
+                        inMemoryCellCurrentValue.Priority = cell.Priority;
+                        inMemoryCellCurrentValue.Side = cell.Side;
+                        inMemoryCellCurrentValue.Status = cell.Status;
+
+                        this.inMemoryDataContext.SaveChanges();
+                    }
+                    else
+                    {
+                        throw new ArgumentNullException();
+                    }
+                }
+            }
+
+            return setCellList;
+        }
+
+        #endregion
+    }
+}
