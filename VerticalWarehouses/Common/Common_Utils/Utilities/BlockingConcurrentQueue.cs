d70cf66cb0a32bffe50b4ba43eecc447848eb3a7
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-02-26 15:01:22 +0100

    Correct some warning

--- VerticalWarehouses/Common/Common_Utils/Utilities/BlockingConcurrentQueue.cs
+++ VerticalWarehouses/Common/Common_Utils/Utilities/BlockingConcurrentQueue.cs
@@ -15,7 +15,7 @@ namespace Ferretto.VW.Common_Utils.Utilities
 
         public BlockingConcurrentQueue()
         {
-            this.dataReady = new ManualResetEventSlim( false );
+            this.dataReady = new ManualResetEventSlim(false);
         }
 
         #endregion
@@ -28,21 +28,21 @@ namespace Ferretto.VW.Common_Utils.Utilities
 
         #region Methods
 
-        public new void Enqueue( T item )
+        public new void Enqueue(T item)
         {
-            base.Enqueue( item );
+            base.Enqueue(item);
             this.dataReady?.Set();
         }
 
-        public new bool TryDequeue( int timeout, CancellationToken cancellationToken, out T result )
+        public bool TryDequeue(int timeout, CancellationToken cancellationToken, out T result)
         {
-            if(this.dataReady?.Wait( timeout, cancellationToken ) ?? false)
+            if (this.dataReady?.Wait(timeout, cancellationToken) ?? false)
             {
                 this.dataReady.Reset();
-                return base.TryDequeue( out result );
+                return base.TryDequeue(out result);
             }
 
-            result = default( T );
+            result = default(T);
 
             return false;
         }
--- VerticalWarehouses/Utils/Source/CellsManagement/Cell.cs
+++ VerticalWarehouses/Utils/Source/CellsManagement/Cell.cs
@@ -14,30 +14,35 @@ namespace Ferretto.VW.Utils.Source.CellsManagement
         {
             this.IdCell = id;
             this.Priority = id;
-            if (id % 2 == 0) //if id is even
+            if (id % 2 == 0) // if id is even
             {
                 this.Coord = (id == 2) ? CellManagementMethods.CELL_HEIGHT_MILLIMETERS : CellManagementMethods.CELL_HEIGHT_MILLIMETERS * (id / CellManagementMethods.AISLE_SIDES_COUNT);
                 this.Side = Side.FrontEven;
             }
-            else //if id is odd
+            else // if id is odd
             {
                 this.Coord = (id == 1) ? CellManagementMethods.CELL_HEIGHT_MILLIMETERS : CellManagementMethods.CELL_HEIGHT_MILLIMETERS * ((id / CellManagementMethods.AISLE_SIDES_COUNT) + 1);
                 this.Side = Side.BackOdd;
             }
+
             this.Status = Status.Free;
         }
 
-        #endregion Constructors
+        #endregion
 
         #region Properties
 
         public Int32 Coord { get; set; }
+
         public Int32 IdCell { get; set; }
+
         public Int32 Priority { get; set; }
+
         public Side Side { get; set; }
+
         public Status Status { get; set; }
 
-        #endregion Properties
+        #endregion
 
         // status code: 0 = free; 1 = disabled; 2 = occupied; 3 = unusable
     }
