b75d9cacb2f3b4add0fe1c8ab41f98557869d321
Marco Morbelli <marco.morbelli@altran.it>
2019-02-27 13:33:22 +0100

    Chore(): add the LogWriting(string) method in the Interface, it disappeared and add the HostedDataLayer.cs class.

---
+++ VerticalWarehouses/Machine Automation Service/MAS_DataLayer/HostedDataLayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Ferretto.VW.MAS_DataLayer
+{
+    public class HostedDataLayer : BackgroundService
+    {
+        #region Methods
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
+}
