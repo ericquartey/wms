9fb962def3a833cd2e85ce3f67497f4ce31667dd
Alice Orsato <alice.orsato@autoware.it>
2019-02-28 09:32:18 +0100

    [add] property color required to interface IRefreshDataEntityVM

--- Common/Controls/Interfaces/IRefreshDataEntityViewModel.cs
+++ Common/Controls/Interfaces/IRefreshDataEntityViewModel.cs
@@ -2,6 +2,12 @@ namespace Ferretto.Common.Controls.Interfaces
 {
     public interface IRefreshDataEntityViewModel
     {
+        #region Properties
+
+        ColorRequired ColorRequired { get; set; }
+
+        #endregion
+
         #region Methods
 
         void LoadRelatedData();
