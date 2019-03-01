e03a4c631d8c1278203d7cdca03b142082f856da
Ali Asghar Fathi <aliasghar.fathi@altran.it>
2019-03-01 13:34:12 +0100

    feat:Create Unit Testing for State Machines of Vertical Moving Drawer

---
+++ VerticalWarehouses/Machine Automation Service/Tests/MAS_InverterDriverUnitTests/StateMachines/VerticalMovingDrawer/EndStateUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using Ferretto.VW.Common_Utils.Enumerations;
+using Ferretto.VW.MAS_InverterDriver;
+using Ferretto.VW.MAS_InverterDriver.StateMachines;
+using Ferretto.VW.MAS_InverterDriver.StateMachines.VerticalMovingDrawer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MAS_InverterDriverUnitTests.StateMachines.VerticalMovingDrawer
+{
+    [TestClass]
+    public class EndStateUnitTest
+    {
+        #region Fields
+
+        private readonly ushort parameterValue;
+        private InverterMessage message;
+
+        #endregion
+
+        #region Methods
+
+        [TestMethod]
+        [TestCategory("Constructors")]
+
+        public void IsNotNullEndState()
+        {
+            var parentStateMachineMock = new Mock<IInverterStateMachine>();
+            var endState = new EndState(parentStateMachineMock.Object, Axis.Vertical);
+            var inverterMessage = new InverterMessage(0x00, (short)InverterParameterId.ControlWordParam, this.parameterValue);
+
+            Assert.IsNotNull(endState);
+            Assert.IsNotNull(inverterMessage);
+        }
+
+        /*
+        [TestMethod]
+        [TestCategory("NotifyMessage")]
+
+        public void IsTrueNotifyMessage()
+        {
+            var parentStateMachineMock = new Mock<IInverterStateMachine>();
+
+            var errorState = new ErrorState(parentStateMachineMock.Object, Axis.Vertical);
+
+            if (message.IsError)
+            {
+                //TODO Assert.IsTrue(errorState);
+            }
+        }
+
+       */
+
+        #endregion
+    }
+}
---
+++ VerticalWarehouses/Machine Automation Service/Tests/MAS_InverterDriverUnitTests/StateMachines/VerticalMovingDrawer/VoltageDisabledStateUnitTest.cs
@@ -0,0 +1,60 @@
+﻿using Ferretto.VW.Common_Utils.Enumerations;
+using Ferretto.VW.MAS_InverterDriver;
+using Ferretto.VW.MAS_InverterDriver.StateMachines;
+using Ferretto.VW.MAS_InverterDriver.StateMachines.VerticalMovingDrawer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+namespace MAS_InverterDriverUnitTests.StateMachines.VerticalMovingDrawer
+{
+    [TestClass]
+    public class VoltageDisabledStateUnitTest
+    {
+        #region Fields
+
+        private readonly ushort parameterValue;
+        private InverterMessage message;
+
+        #endregion
+
+        #region Methods
+
+        [TestMethod]
+        [TestCategory("Constructors")]
+
+        public void IsNotNullVoltageDisabledState()
+        {
+            var parentStateMachineMock = new Mock<IInverterStateMachine>();
+            var voltageDisabledState = new VoltageDisabledState(parentStateMachineMock.Object, Axis.Vertical);
+            var inverterMessage = new InverterMessage(0x00, (short)InverterParameterId.ControlWordParam, this.parameterValue);
+
+            Assert.IsNotNull(voltageDisabledState);
+            Assert.IsNotNull(inverterMessage);
+        }
+
+        /*
+        [TestMethod]
+        [TestCategory("NotifyMessage")]
+
+        public void IsTrueNotifyMessage()
+        {
+            var parentStateMachineMock = new Mock<IInverterStateMachine>();
+
+            var errorState = new ErrorState(parentStateMachineMock.Object, Axis.Vertical);
+            var operationModeState = new OperationModeState(parentStateMachineMock.Object, Axis.Vertical);
+
+            if (message.IsError)
+            {
+                //TODO Assert.IsTrue(errorState);
+            }
+
+            if (!message.IsWriteMessage && message.ParameterId == InverterParameterId.StatusWordParam)
+                if (message.ShortPayload == this.parameterValue)
+                {
+                    //TODO Assert.IsTrue(operationModeState);
+                }
+        }
+
+       */
+        #endregion
+    }
+}
