4af2da25dc78bc6506e82c70e097e8206cf59f62
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-03-06 10:39:42 +0100

    removed old folders

--- VerticalWarehouses/InstallationApp/ViewsAndViewModels/SingleViews/CellsSideControlViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ferretto.VW.InstallationApp.Interfaces;
-using Prism.Mvvm;
-
-namespace Ferretto.VW.InstallationApp
-{
-    public class CellsSideControlViewModel : BindableBase, ICellsSideControlViewModel
-    {
-    }
-}
