1c8631cf00db92b83180f44da502306b234175e9
Alessandro Salomone <alessandro.salomone@altran.it>
2019-03-06 11:48:42 +0100

    feat(scheduler): add loading unit provider

---
+++ WMS/Scheduler/Core/Interfaces/ILoadingUnitSchedulerProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Ferretto.WMS.Scheduler.Core.Models;
+
+namespace Ferretto.WMS.Scheduler.Core.Interfaces
+{
+    public interface ILoadingUnitSchedulerProvider
+    {
+        #region Methods
+
+        Task<LoadingUnit> GetByIdAsync(int id);
+
+        #endregion
+    }
+}
---
+++ WMS/Scheduler/Core/Providers/LoadingUnitSchedulerProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Ferretto.Common.EF;
+using Ferretto.WMS.Scheduler.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ferretto.WMS.Scheduler.Core.Providers
+{
+    internal class LoadingUnitSchedulerProvider : ILoadingUnitSchedulerProvider
+    {
+        #region Fields
+
+        private readonly DatabaseContext databaseContext;
+
+        #endregion
+
+        #region Constructors
+
+        public LoadingUnitSchedulerProvider(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<LoadingUnit> GetByIdAsync(int id)
+        {
+            return await this.databaseContext.LoadingUnits
+               .Select(l => new LoadingUnit
+               {
+                   Id = l.Id,
+                   LastPickDate = l.LastPickDate
+               })
+               .SingleOrDefaultAsync(l => l.Id == id);
+        }
+
+        #endregion
+    }
+}
