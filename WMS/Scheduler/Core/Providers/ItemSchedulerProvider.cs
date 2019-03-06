94211b49ef51de60bec57b17b2b07c750922878f
Alessandro Salomone <alessandro.salomone@altran.it>
2019-03-06 15:35:53 +0100

    feat(scheduler): add logic to support LastPickDate update

--- WMS/Scheduler/Core/Providers/ItemSchedulerProvider.cs
+++ WMS/Scheduler/Core/Providers/ItemSchedulerProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Ferretto.Common.BLL.Interfaces;
 using Ferretto.Common.EF;
 using Ferretto.WMS.Scheduler.Core.Interfaces;
 using Ferretto.WMS.Scheduler.Core.Models;
@@ -26,15 +27,31 @@ namespace Ferretto.WMS.Scheduler.Core.Providers
 
         #region Methods
 
-        public async Task<Item> GetByIdAsync(int itemId)
+        public async Task<Item> GetByIdAsync(int id)
         {
             return await this.databaseContext.Items
                .Select(i => new Item
                {
                    Id = i.Id,
                    ManagementType = (ItemManagementType)i.ManagementType,
+                   LastPickDate = i.LastPickDate
                })
-               .SingleAsync(i => i.Id == itemId);
+               .SingleAsync(i => i.Id == id);
+        }
+
+        public async Task<IOperationResult<Item>> UpdateAsync(Item model)
+        {
+            if (model == null)
+            {
+                throw new System.ArgumentNullException(nameof(model));
+            }
+
+            var existingModel = this.databaseContext.Items.Find(model.Id);
+            this.databaseContext.Entry(existingModel).CurrentValues.SetValues(model);
+
+            await this.databaseContext.SaveChangesAsync();
+
+            return new SuccessOperationResult<Item>(model);
         }
 
         #endregion
--- WMS/Scheduler/Core/Providers/LoadingUnitSchedulerProvider.cs
+++ WMS/Scheduler/Core/Providers/LoadingUnitSchedulerProvider.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Ferretto.Common.BLL.Interfaces;
 using Ferretto.Common.EF;
+using Ferretto.WMS.Scheduler.Core.Interfaces;
 using Ferretto.WMS.Scheduler.Core.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -34,6 +36,21 @@ namespace Ferretto.WMS.Scheduler.Core.Providers
                    LastPickDate = l.LastPickDate
                })
                .SingleOrDefaultAsync(l => l.Id == id);
+        }
+
+        public async Task<IOperationResult<LoadingUnit>> UpdateAsync(LoadingUnit model)
+        {
+            if (model == null)
+            {
+                throw new System.ArgumentNullException(nameof(model));
+            }
+
+            var existingModel = this.databaseContext.LoadingUnits.Find(model.Id);
+            this.databaseContext.Entry(existingModel).CurrentValues.SetValues(model);
+
+            await this.databaseContext.SaveChangesAsync();
+
+            return new SuccessOperationResult<LoadingUnit>(model);
         }
 
         #endregion
