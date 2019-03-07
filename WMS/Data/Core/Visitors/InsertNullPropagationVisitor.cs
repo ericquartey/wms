34fa7817100e8943f397f82011c51c2e92e2427f
Nicola Moro <nmoro@ferrettogroup.com>
2019-03-07 09:42:37 +0100

    fix: add null propagation visitor to where criteria parsing

---
+++ WMS/Data/Core/Visitors/InsertNullPropagationVisitor.cs
@@ -0,0 +1,49 @@
+using DevExpress.Data.Filtering;
+using DevExpress.Data.Filtering.Helpers;
+
+namespace Ferretto.WMS.Data.Core.Visitors
+{
+    public class InsertNullPropagationVisitor : ClientCriteriaLazyPatcherBase.AggregatesCommonProcessingBase
+    {
+        #region Methods
+
+        public static CriteriaOperator Patch(CriteriaOperator source)
+        {
+            return new InsertNullPropagationVisitor().Process(source);
+        }
+
+        public override CriteriaOperator Visit(FunctionOperator theOperator)
+        {
+            if (ReferenceEquals(theOperator, null))
+            {
+                return null;
+            }
+
+            var visitedOperator = (FunctionOperator)base.Visit(theOperator);
+            var parameter = theOperator.Operands[0];
+            return CriteriaOperator.And(new NotOperator(new NullOperator(parameter)), visitedOperator);
+        }
+
+        public override CriteriaOperator Visit(BinaryOperator theOperator)
+        {
+            var visitedOperator = (BinaryOperator)base.Visit(theOperator);
+
+            CriteriaOperator modifiedCriteriaOperator = visitedOperator;
+            if (visitedOperator.LeftOperand is FunctionOperator leftOperandFunction)
+            {
+                var parameter = leftOperandFunction.Operands[0];
+                modifiedCriteriaOperator = CriteriaOperator.And(new NotOperator(new NullOperator(parameter)), modifiedCriteriaOperator);
+            }
+
+            if (visitedOperator.RightOperand is FunctionOperator rightOperandFunction)
+            {
+                var parameter = rightOperandFunction.Operands[0];
+                modifiedCriteriaOperator = CriteriaOperator.And(new NotOperator(new NullOperator(parameter)), modifiedCriteriaOperator);
+            }
+
+            return modifiedCriteriaOperator;
+        }
+
+        #endregion
+    }
+}
