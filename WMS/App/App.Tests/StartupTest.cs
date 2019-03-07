2b4bb4fcc1b6243ccaec643fe5db40ecf6eca9db
Nicola Moro <nmoro@ferrettogroup.com>
2019-03-07 17:13:13 +0100

    tests: fix typo

--- WMS/App/App.Tests/StartupTest.cs
+++ WMS/App/App.Tests/StartupTest.cs
@@ -14,7 +14,7 @@ namespace Ferretto.WMS.App.Tests
         // #region Methods
         //    [TestMethod]
         //    [DeploymentItem("Ferretto.WMS.App.Themes.dll")]
-        //    [DeploymentItem("DevExpress.Xpf.Accordion.v18.2")]
+        //    [DeploymentItem("DevExpress.Xpf.Accordion.v18.2.dll")]
         //    [DeploymentItem("DevExpress.Xpf.Controls.v18.2.dll")]
         //    [DeploymentItem("DevExpress.Xpf.Core.v18.2.dll")]
         //    [DeploymentItem("DevExpress.Xpf.Core.v18.2.Extensions.dll")]
