e1815088769b95d2ccfeab35712ba30dd772cf27
Marco Morbelli <marco.morbelli@altran.it>
2019-03-11 16:57:43 +0100

    feat(): table creation

---
+++ VerticalWarehouses/Machine Automation Service/MAS_DataLayer/Models/InstallationInfo.cs
@@ -0,0 +1,39 @@
+﻿namespace Ferretto.VW.MAS_DataLayer
+{
+    public class InstallationInfo
+    {
+        #region Properties
+
+        public bool Belt_Burnishing { get; set; }
+
+        public int InstallationInfoId { get; set; }
+
+        public bool Machine_Ok { get; set; }
+
+        public bool Ok_Laser1 { get; set; }
+
+        public bool Ok_Laser2 { get; set; }
+
+        public bool Ok_Laser3 { get; set; }
+
+        public bool Ok_Shape1 { get; set; }
+
+        public bool Ok_Shape2 { get; set; }
+
+        public bool Ok_Shape3 { get; set; }
+
+        public bool Ok_Shutter1 { get; set; }
+
+        public bool Ok_Shutter2 { get; set; }
+
+        public bool Ok_Shutter3 { get; set; }
+
+        public bool Origin_X_Axis { get; set; }
+
+        public bool Origin_Y_Axis { get; set; }
+
+        public bool Set_Y_Resolution { get; set; }
+
+        #endregion
+    }
+}
