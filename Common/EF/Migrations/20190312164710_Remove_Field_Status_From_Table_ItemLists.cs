7b916eb3c71164d69f74188cdab044f8abb0fd2e
Alessandro Salomone <alessandro.salomone@altran.it>
2019-03-12 17:47:53 +0100

    feat(item-list): add migration to remove status column

---
+++ Common/EF/Migrations/20190312164710_Remove_Field_Status_From_Table_ItemLists.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ferretto.Common.EF.Migrations
+{
+    public partial class Remove_Field_Status_From_Table_ItemLists : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ItemLists");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "ItemLists",
+                type: "char(1)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
