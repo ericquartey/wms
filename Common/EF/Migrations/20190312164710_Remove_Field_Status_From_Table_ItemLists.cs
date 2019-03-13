f4d52cd9169eac75a8f267418f2178dc5b9655a9
Alessandro Salomone <alessandro.salomone@altran.it>
2019-03-13 10:47:46 +0100

    refactor(migrations): fix minor CA warnings

--- Common/EF/Migrations/20190312164710_Remove_Field_Status_From_Table_ItemLists.cs
+++ Common/EF/Migrations/20190312164710_Remove_Field_Status_From_Table_ItemLists.cs
@@ -4,21 +4,35 @@ namespace Ferretto.Common.EF.Migrations
 {
     public partial class Remove_Field_Status_From_Table_ItemLists : Migration
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "Status",
-                table: "ItemLists");
-        }
+        #region Methods
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            if (migrationBuilder == null)
+            {
+                throw new System.ArgumentNullException(nameof(migrationBuilder));
+            }
+
             migrationBuilder.AddColumn<string>(
                 name: "Status",
                 table: "ItemLists",
                 type: "char(1)",
                 nullable: false,
-                defaultValue: "");
+                defaultValue: string.Empty);
         }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            if (migrationBuilder == null)
+            {
+                throw new System.ArgumentNullException(nameof(migrationBuilder));
+            }
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "ItemLists");
+        }
+
+        #endregion
     }
 }
