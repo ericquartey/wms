280d344b20a4d5648c8d7177ed4a93ba9fd0a855
Marco Morbelli <marco.morbelli@altran.it>
2019-03-18 15:57:16 +0100

    feat(): inserted the Log provider and added the NLog using

---
+++ VerticalWarehouses/Machine Automation Service/MAS_AutomationService/LogTargets/SQLiteTarget.cs
@@ -0,0 +1,37 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Ferretto.VW.MAS_AutomationService.LogTargets
+{
+    namespace Ferretto.VW.MAS_AutomationService.LogTargets
+    {
+        [Target("MyFirst")]
+        public sealed class SQliteTarget : TargetWithLayout
+        {
+            #region Constructors
+
+            public SQliteTarget()
+            {
+            }
+
+            #endregion
+
+            #region Properties
+
+            [RequiredParameter]
+            public string Database { get; set; }
+
+            #endregion
+
+            #region Methods
+
+            protected override void Write(LogEventInfo logEvent)
+            {
+                var logMessage = this.Layout.Render(logEvent);
+            }
+
+            #endregion
+        }
+    }
+}
