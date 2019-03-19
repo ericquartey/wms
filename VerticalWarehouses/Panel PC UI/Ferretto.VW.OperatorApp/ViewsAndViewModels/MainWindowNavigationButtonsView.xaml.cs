da9d3092e2288fe3b7dc13297a8726b3884b929c
Ennio Di Bernardo <edibernardo@ferrettogroup.com>
2019-03-19 11:52:57 +0100

    Inserted a navigation button in the main menu. Inserted the Help Windows folder and the page base.

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.OperatorApp/ViewsAndViewModels/MainWindowNavigationButtonsView.xaml.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.OperatorApp/ViewsAndViewModels/MainWindowNavigationButtonsView.xaml.cs
@@ -25,48 +25,48 @@ namespace Ferretto.VW.OperatorApp.ViewsAndViewModels
         public MainWindowNavigationButtonsView()
         {
             this.InitializeComponent();
-            this.NavigationButtonScrollViewer.AddHandler(ScrollViewer.ScrollChangedEvent, new ScrollChangedEventHandler(this.CheckVerticalOffset), true);
+            /// this.NavigationButtonScrollViewer.AddHandler(ScrollViewer.ScrollChangedEvent, new ScrollChangedEventHandler(this.CheckVerticalOffset), true);
         }
 
         #endregion
 
-        #region Methods
+       #region Methods
 
-        private void CheckVerticalOffset(object sender, ScrollChangedEventArgs e)
-        {
-            if (e.VerticalOffset == 0)
-            {
-                this.UpScroll.IsEnabled = false;
-            }
-            else
-            {
-                this.UpScroll.IsEnabled = true;
-            }
-            if (e.VerticalOffset == 322)
-            {
-                this.DownScroll.IsEnabled = false;
-            }
-            else
-            {
-                this.DownScroll.IsEnabled = true;
-            }
-        }
+        //private void CheckVerticalOffset(object sender, ScrollChangedEventArgs e)
+        //{
+        //    if (e.VerticalOffset == 0)
+        //    {
+        //        this.UpScroll.IsEnabled = false;
+        //    }
+        //    else
+        //    {
+        //        this.UpScroll.IsEnabled = true;
+        //    }
+        //    if (e.VerticalOffset == 322)
+        //    {
+        //        this.DownScroll.IsEnabled = false;
+        //    }
+        //    else
+        //    {
+        //        this.DownScroll.IsEnabled = true;
+        //    }
+        //}
 
-        private void DownScroll_Click(object sender, RoutedEventArgs e)
-        {
-            this.NavigationButtonScrollViewer.LineDown();
-        }
+        //private void DownScroll_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.NavigationButtonScrollViewer.LineDown();
+        //}
 
-        private void ScrollViewer_ManipulationBoundaryFeedback(object sender, ManipulationBoundaryFeedbackEventArgs e)
-        {
-            e.Handled = true;
-        }
+        //private void ScrollViewer_ManipulationBoundaryFeedback(object sender, ManipulationBoundaryFeedbackEventArgs e)
+        //{
+        //    e.Handled = true;
+        //}
 
-        private void UpScroll_Click(object sender, RoutedEventArgs e)
-        {
-            this.NavigationButtonScrollViewer.LineUp();
-            //TODO: scroll up and down nav button height
-        }
+        //private void UpScroll_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.NavigationButtonScrollViewer.LineUp();
+        //    //TODO: scroll up and down nav button height
+        //}
 
         #endregion
     }
