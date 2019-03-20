c90b9d85a32f4825c5a34f3a70ae1aaeb6523e1e
Alessandro Gennaro <agennaro@ferrettogroup.com>
2019-03-20 16:12:18 +0100

    feat: Implement and refactor the IO Switch axis operation.

--- VerticalWarehouses/Machine Automation Service/MAS_IODriver/StateMachines/SwitchAxis/EndState.cs
+++ VerticalWarehouses/Machine Automation Service/MAS_IODriver/StateMachines/SwitchAxis/EndState.cs
@@ -3,19 +3,28 @@ using System.Threading;
 using Ferretto.VW.Common_Utils.Enumerations;
 using Ferretto.VW.Common_Utils.Messages;
 using Ferretto.VW.Common_Utils.Messages.Data;
+using Microsoft.Extensions.Logging;
 
 namespace Ferretto.VW.MAS_IODriver.StateMachines.SwitchAxis
 {
     public class EndState : IoStateBase
     {
+        #region Fields
+
+        private readonly ILogger logger;
+
+        #endregion
+
         #region Constructors
 
-        public EndState(Axis axisToSwitchOn, IIoStateMachine parentStateMachine)
+        public EndState(Axis axisToSwitchOn, ILogger logger, IIoStateMachine parentStateMachine)
         {
-            Console.WriteLine($"{DateTime.Now}: Thread:{Thread.CurrentThread.ManagedThreadId} - MAS_IODriver:EndState:Ctor");
             this.parentStateMachine = parentStateMachine;
+            this.logger = logger;
 
-            SwitchAxisMessageData messageData = new SwitchAxisMessageData(axisToSwitchOn);
+            this.logger?.LogTrace($"{DateTime.Now}: Thread:{Thread.CurrentThread.ManagedThreadId} - MAS_IODriver:EndState:Ctor");
+
+            var messageData = new SwitchAxisMessageData(axisToSwitchOn);
             var endNotification = new NotificationMessage(messageData, "Motor Switch complete", MessageActor.Any,
                 MessageActor.IODriver, MessageType.SwitchAxis, MessageStatus.OperationEnd);
             this.parentStateMachine.PublishNotificationEvent(endNotification);
