2e4448a00637edc91632d2ec3a4f81c38319a9ee
Nicola Moro <nmoro@ferrettogroup.com>
2019-03-21 17:24:15 +0100

    feat: update Data Model length fields from integer to double

--- Common/DataModels/LoadingUnitSizeClass.cs
+++ Common/DataModels/LoadingUnitSizeClass.cs
@@ -7,23 +7,23 @@ namespace Ferretto.Common.DataModels
     {
         #region Properties
 
-        public int? BayForksUnthread { get; set; }
+        public double? BayForksUnthread { get; set; }
 
-        public int? BayOffset { get; set; }
+        public double? BayOffset { get; set; }
 
-        public int? CellForksUnthread { get; set; }
+        public double CellForksUnthread { get; set; }
 
         public string Description { get; set; }
 
         public int Id { get; set; }
 
-        public int Length { get; set; }
+        public double Length { get; set; }
 
-        public int? Lift { get; set; }
+        public double? Lift { get; set; }
 
         public IEnumerable<LoadingUnitType> LoadingUnitTypes { get; set; }
 
-        public int Width { get; set; }
+        public double Width { get; set; }
 
         #endregion
     }
