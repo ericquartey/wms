64ec676653f13b0376f3cefa0af110f0fa44a947
Nicola Moro <nmoro@ferrettogroup.com>
2019-03-22 12:27:28 +0100

    refactor: minor code cleanup

--- WMS/Data/Core/Models/Compartment/CompartmentDetails.cs
+++ WMS/Data/Core/Models/Compartment/CompartmentDetails.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Ferretto.Common.BLL.Interfaces.Models;
 using Ferretto.Common.Resources;
-using Ferretto.WMS.Data.Core.Interfaces;
 
 namespace Ferretto.WMS.Data.Core.Models
 {
