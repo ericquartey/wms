2518f7015e26c63272d00661ddf45705655da11c
Nicola Moro <nmoro@ferrettogroup.com>
2019-03-28 16:05:21 +0100

    chore: remove code analyzer suppression

--- WMS/Data/WebAPI/Hubs/SchedulerHub.cs
+++ WMS/Data/WebAPI/Hubs/SchedulerHub.cs
@@ -1,13 +1,8 @@
-﻿using System.Threading.Tasks;
-using Ferretto.WMS.Data.Hubs;
+﻿using Ferretto.WMS.Data.Hubs;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Ferretto.WMS.Data.WebAPI.Hubs
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessage(
-        "Major Code Smell",
-        "S4261: Add the 'Async' suffix to the name of this method.",
-        Justification = "The methods names here will be exposed by SignalR, so we want that names are clean")]
     public class SchedulerHub : Hub<ISchedulerHub>
     {
     }
