9cc67c07dfb2f7bce66178cbf3bf9a2092658fb6
Alice Orsato <alice.orsato@autoware.it>
2019-04-02 14:54:25 +0200

    [add] dp reason in Action bar + tooltip in bar button item

--- Common/Controls/Controls/ActionBar/ActionBarItem.xaml.cs
+++ Common/Controls/Controls/ActionBar/ActionBarItem.xaml.cs
@@ -1,14 +1,36 @@
-﻿using DevExpress.Xpf.Bars;
+﻿using System.Windows;
+using DevExpress.Xpf.Bars;
+using Ferretto.Common.BLL.Interfaces.Models;
 
 namespace Ferretto.Common.Controls
 {
     public partial class ActionBarItem : BarButtonItem
     {
+        #region Fields
+
+        public static readonly DependencyProperty ReasonProperty = DependencyProperty.Register(
+            nameof(Reason),
+            typeof(string),
+            typeof(ActionBarItem),
+            new UIPropertyMetadata(null));
+
+        #endregion
+
         #region Constructors
 
         public ActionBarItem()
         {
             this.InitializeComponent();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Reason
+        {
+            get => this.Policy?.Reason;
+            set => this.SetValue(ReasonProperty, value);
         }
 
         #endregion
