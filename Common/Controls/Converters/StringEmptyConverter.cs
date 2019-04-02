cc9adee87912348ed1cf26c91062859b5904378c
Alice Orsato <alice.orsato@autoware.it>
2019-04-02 14:57:09 +0200

    [add] new converter StringEmpty

---
+++ Common/Controls/Converters/StringEmptyConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Ferretto.Common.Controls
+{
+    public class StringEmptyConverter : IValueConverter
+    {
+        #region Methods
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((string)value) ? Common.Resources.MasterData.AllowedOperation : value;
+        }
+
+        public object ConvertBack(
+              object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
