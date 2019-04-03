cf0726039bdbd8f977deae4a9ead62f4e7dc19a4
Nicola Moro <nmoro@ferrettogroup.com>
2019-04-03 15:59:42 +0200

    refactor: move IFileProvider and IStreamFile (previously IImageFile) to BLL.Interfaces

---
+++ Common/BLL.Interfaces/Models/IStreamFile.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace Ferretto.Common.BLL.Interfaces.Models
+{
+    public interface IStreamFile : IModel<string>
+    {
+        #region Properties
+
+        Stream Stream { get; set; }
+
+        #endregion
+    }
+}
