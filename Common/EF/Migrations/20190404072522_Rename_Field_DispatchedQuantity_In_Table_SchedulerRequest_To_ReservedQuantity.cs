5d301512f4aefbc691857c64fab765e59393027c
Alessandro Salomone <alessandro.salomone@altran.it>
2019-04-04 09:27:41 +0200

    feat(scheduler): create migration for scheduler request update

---
+++ Common/EF/Migrations/20190404072522_Rename_Field_DispatchedQuantity_In_Table_SchedulerRequest_To_ReservedQuantity.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ferretto.Common.EF.Migrations
+{
+    public partial class Rename_Field_DispatchedQuantity_In_Table_SchedulerRequest_To_ReservedQuantity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DispatchedQuantity",
+                table: "SchedulerRequests",
+                newName: "ReservedQuantity");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReservedQuantity",
+                table: "SchedulerRequests",
+                newName: "DispatchedQuantity");
+        }
+    }
+}
