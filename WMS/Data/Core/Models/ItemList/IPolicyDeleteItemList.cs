7dfe1546c021370f95caaa2a043e1bad91c04dca
Alessandro Salomone <alessandro.salomone@altran.it>
2019-04-05 18:17:32 +0200

    feat(policies): rename policy interfaces

--- WMS/Data/Core/Models/ItemList/IPolicyDeleteItemList.cs
+++ WMS/Data/Core/Models/ItemList/IPolicyDeleteItemList.cs
@@ -2,11 +2,13 @@ using Ferretto.Common.BLL.Interfaces.Models;
 
 namespace Ferretto.WMS.Data.Core.Models
 {
-    public interface IAvailabilityItem : IModel<int>
+    public interface IPolicyDeleteItemList : IModel<int>
     {
         #region Properties
 
-        int TotalAvailable { get; }
+        bool HasActiveRows { get; }
+
+        ItemListStatus Status { get; }
 
         #endregion
     }
