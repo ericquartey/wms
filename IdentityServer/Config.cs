1259de5058a346752edce4d7ebaf9f18003190f2
Alessandro Salomone <alessandro.salomone@altran.it>
2019-04-07 20:40:38 +0200

    feat(identity-server): add project

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        #region Methods
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("wms-data", "WMS Data Service API"),
+                new ApiResource("wms-scheduler", "WMS Scheduler Service API"),
+                new ApiResource("vw-automation", "VW Automation Service API"),
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "wms-desktop-client",
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "wms-data", "wms-scheduler" }
+                },
+                new Client
+                {
+                    ClientId = "vw-panel-pc-client",
+
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "vw-automation", "wms-data" }
+                },
+                new Client
+                {
+                    ClientId = "vw-automation-client",
+                        // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "wms-data", "wms-scheduler" }
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "nicola",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "3",
+                    Username = "giovanni",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "4",
+                    Username = "alessandro",
+                    Password = "password"
+                }
+            };
+        }
+
+        #endregion
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        #region Constructors
+
+        public Startup(IHostingEnvironment environment)
+        {
+            this.Environment = environment;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public IHostingEnvironment Environment { get; }
+
+        #endregion
+
+        #region Methods
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (this.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to support static files
+            //app.UseStaticFiles();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you wan to add an MVC-based UI
+            //app.UseMvcWithDefaultRoute();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you wan to add an MVC-based UI
+            //services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApis())
+                .AddTestUsers(Config.GetUsers())
+                .AddInMemoryClients(Config.GetClients());
+
+            if (this.Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+        }
+
+        #endregion
+    }
+}
