3497040a87f72dd7362e4d865cf19d3d7afe5dc1
Nicola Moro <nmoro@ferrettogroup.com>
2019-04-29 12:20:29 +0200

    feat: derive LoadingUnitWithdrawDialogViewModel from BaseDialogViewModel

--- WMS/App/Modules/MasterData/ViewModels/LoadingUnit/LoadingUnitWithdrawDialogViewModel.cs
+++ WMS/App/Modules/MasterData/ViewModels/LoadingUnit/LoadingUnitWithdrawDialogViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows.Input;
 using CommonServiceLocator;
 using Ferretto.WMS.App.Controls;
@@ -14,7 +9,7 @@ using Prism.Commands;
 
 namespace Ferretto.WMS.Modules.MasterData
 {
-    public class LoadingUnitWithdrawDialogViewModel : BaseServiceNavigationViewModel
+    public class LoadingUnitWithdrawDialogViewModel : BaseDialogViewModel<LoadingUnitWithdraw>
     {
         #region Fields
 
@@ -22,108 +17,34 @@ namespace Ferretto.WMS.Modules.MasterData
 
         private readonly ILoadingUnitProvider loadingUnitProvider = ServiceLocator.Current.GetInstance<ILoadingUnitProvider>();
 
-        private bool canShowError;
-
-        private ICommand closeDialogCommand;
-
-        private string error;
-
-        private bool isBusy;
-
-        private bool isEnableError;
-
-        private bool isModelValid;
-
-        private LoadingUnitWithdraw loadingUnitWithdraw;
-
         private ICommand runWithdrawCommand;
+
+        #endregion
+
+        #region Constructors
+
+        public LoadingUnitWithdrawDialogViewModel()
+        {
+            this.Initialize();
+        }
 
         #endregion
 
         #region Properties
 
-        public bool CanShowError
-        {
-            get => this.canShowError;
-            set
-            {
-                this.SetProperty(ref this.canShowError, value);
-                this.UpdateIsEnableError();
-            }
-        }
-
-        public ICommand CloseDialogCommand => this.closeDialogCommand ??
-                     (this.closeDialogCommand = new DelegateCommand(
-                 this.ExecuteCloseDialogCommand));
-
-        public string Error { get => this.error; set => this.SetProperty(ref this.error, value); }
-
-        public bool IsBusy
-        {
-            get => this.isBusy;
-            set => this.SetProperty(ref this.isBusy, value);
-        }
-
-        public bool IsEnableError
-        {
-            get => this.isEnableError;
-            set => this.SetProperty(ref this.isEnableError, value);
-        }
-
-        public bool IsModelValid
-        {
-            get
-            {
-                var temp = false;
-                if (this.LoadingUnitWithdraw == null)
-                {
-                    temp = true;
-                }
-                else
-                {
-                    temp = string.IsNullOrWhiteSpace(this.LoadingUnitWithdraw.Error);
-                }
-
-                this.SetProperty(ref this.isModelValid, temp);
-                this.UpdateIsEnableError();
-                return temp;
-            }
-        }
-
-        public LoadingUnitWithdraw LoadingUnitWithdraw
-        {
-            get => this.loadingUnitWithdraw;
-            set
-            {
-                if (this.LoadingUnitWithdraw != null && value != this.LoadingUnitWithdraw)
-                {
-                    this.LoadingUnitWithdraw.PropertyChanged -= this.OnLoadingUnitWithdrawPropertyChanged;
-                }
-
-                if (this.SetProperty(ref this.loadingUnitWithdraw, value))
-                {
-                    this.LoadingUnitWithdraw.PropertyChanged += this.OnLoadingUnitWithdrawPropertyChanged;
-                }
-            }
-        }
-
         public ICommand RunWithdrawCommand => this.runWithdrawCommand ??
-                    (this.runWithdrawCommand = new DelegateCommand(
+            (this.runWithdrawCommand = new DelegateCommand(
                     async () => await this.RunWithdrawAsync(),
-                    this.CanRunWithdraw));
+                    this.CanRunWithdraw)
+                .ObservesProperty(() => this.Model));
 
         #endregion
 
         #region Methods
 
-        protected virtual void EvaluateCanExecuteCommands()
+        protected override void EvaluateCanExecuteCommands()
         {
             ((DelegateCommand)this.RunWithdrawCommand)?.RaiseCanExecuteChanged();
-        }
-
-        protected void ExecuteCloseDialogCommand()
-        {
-            this.Disappear();
         }
 
         protected override async Task OnAppearAsync()
@@ -132,84 +53,79 @@ namespace Ferretto.WMS.Modules.MasterData
 
             await base.OnAppearAsync().ConfigureAwait(true);
             await this.LoadDataAsync();
+
             this.IsBusy = false;
         }
 
         private bool CanRunWithdraw()
         {
-            var canExecute = this.LoadingUnitWithdraw != null
-               && this.IsModelValid
-               && !this.IsBusy;
-
-            if (canExecute)
-            {
-                this.CanShowError = true;
-            }
-
-            return canExecute;
+            return !this.IsBusy;
         }
 
         private async Task LoadDataAsync()
         {
-            try
+            var modelId = (int?)this.Data.GetType().GetProperty("LoadingUnitId")?.GetValue(this.Data);
+            if (!modelId.HasValue)
             {
-                var modelId = (int)this.Data.GetType().GetProperty("LoadingUnitId")?.GetValue(this.Data);
+                return;
+            }
 
-                var luDetails = await this.loadingUnitProvider.GetByIdAsync(modelId);
-                var bayChoices = luDetails.AreaId.HasValue ?
-                                                      await this.bayProvider.GetByAreaIdAsync(luDetails.AreaId.Value) :
-                                                      null;
-                this.LoadingUnitWithdraw = new LoadingUnitWithdraw
-                {
-                    Id = luDetails.Id,
-                    Code = luDetails.Code,
-                    LoadingUnitTypeDescription = luDetails.LoadingUnitTypeDescription,
-                    LoadingUnitStatusDescription = luDetails.LoadingUnitStatusDescription,
-                    AreaId = luDetails.AreaId,
-                    AreaName = luDetails.AreaName,
-                    BayChoices = bayChoices,
-                };
-            }
-            catch (Exception ex)
+            var luDetails = await this.loadingUnitProvider.GetByIdAsync((int)modelId);
+            var bayChoices = luDetails.AreaId.HasValue
+                ? await this.bayProvider.GetByAreaIdAsync(luDetails.AreaId.Value)
+                : null;
+            this.Model = new LoadingUnitWithdraw
             {
-                this.Error = ex.Message;
-            }
+                Id = luDetails.Id,
+                Code = luDetails.Code,
+                LoadingUnitTypeDescription = luDetails.LoadingUnitTypeDescription,
+                LoadingUnitStatusDescription = luDetails.LoadingUnitStatusDescription,
+                AreaId = luDetails.AreaId,
+                AreaName = luDetails.AreaName,
+                BayChoices = bayChoices,
+            };
         }
 
-        private void OnLoadingUnitWithdrawPropertyChanged(object sender, PropertyChangedEventArgs e)
+        private bool CheckWithdrawConditions()
         {
-            ((DelegateCommand)this.RunWithdrawCommand)?.RaiseCanExecuteChanged();
+            this.CanShowError = true;
+
+            return this.IsModelValid
+                && this.ChangeDetector.IsRequiredValid;
+        }
+
+        private void Initialize()
+        {
+            this.Model = new LoadingUnitWithdraw();
         }
 
         private async Task RunWithdrawAsync()
         {
-            if (this.LoadingUnitWithdraw?.Id == 0 || !this.LoadingUnitWithdraw.BayId.HasValue)
+            if (!this.CheckWithdrawConditions())
             {
                 return;
             }
 
             this.IsBusy = true;
 
-            var result = await this.loadingUnitProvider.WithdrawAsync(this.LoadingUnitWithdraw.Id, this.LoadingUnitWithdraw.BayId.Value);
+            var result = await this.loadingUnitProvider.WithdrawAsync(this.Model.Id, this.Model.BayId.Value);
+
+            this.IsBusy = false;
 
             if (result.Success)
             {
-                this.EventService.Invoke(new StatusPubSubEvent(Common.Resources.MasterData.LoadingUnitWithdrawCommenced, StatusType.Success));
+                this.EventService.Invoke(new StatusPubSubEvent(
+                    Common.Resources.MasterData.LoadingUnitWithdrawCommenced,
+                    StatusType.Success));
 
-                this.Disappear();
+                this.CloseDialogCommand.Execute(null);
             }
             else
             {
-                this.Error = result.Description;
-                this.EventService.Invoke(new StatusPubSubEvent(result.Description, StatusType.Error));
+                this.EventService.Invoke(new StatusPubSubEvent(
+                    result.Description,
+                    StatusType.Error));
             }
-
-            this.IsBusy = false;
-        }
-
-        private void UpdateIsEnableError()
-        {
-            this.IsEnableError = this.isModelValid && this.CanShowError;
         }
 
         #endregion
