3d01b5030b04e31290d80ac74933c5e39b851c29
Alice Orsato <alice.orsato@autoware.it>
2019-05-14 16:28:41 +0200

    [move] options file to Option folder

---
+++ WMS/Data/Core/Models/Item/Option/ItemPutOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ferretto.WMS.Data.Core.Models
+{
+    public class ItemPutOptions
+    {
+        #region Properties
+
+        public IEnumerable<Compartment> Compartments { get; set; }
+
+        public IEnumerable<CompartmentPut> CompartmentsPut { get; set; }
+
+        public ItemManagementType? ManagementType { get; set; }
+
+        #endregion
+    }
+}
