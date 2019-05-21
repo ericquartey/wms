392c4c6368ad6c29bcbb07ab1f3b8afd67cd7381
Alice Orsato <alice.orsato@autoware.it>
2019-05-21 09:26:47 +0200

    [remove] derivation from CompartmentSet, for CompartmentSetForPut

--- WMS/Data/Core/Models/Compartment/CompartmentSetForPut.cs
+++ WMS/Data/Core/Models/Compartment/CompartmentSetForPut.cs
@@ -1,10 +1,26 @@
-﻿namespace Ferretto.WMS.Data.Core.Models
+﻿using System;
+
+namespace Ferretto.WMS.Data.Core.Models
 {
-    public class CompartmentSetForPut : CompartmentSet
+    public class CompartmentSetForPut
     {
         #region Properties
 
+        public DateTime? FifoStartDate { get; set; }
+
+        public string Lot { get; set; }
+
+        public int? MaterialStatusId { get; set; }
+
+        public int? PackageTypeId { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
         public double RemainingCapacity { get; set; }
+
+        public string Sub1 { get; set; }
+
+        public string Sub2 { get; set; }
 
         #endregion
     }
