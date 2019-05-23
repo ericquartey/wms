0edb0a1db9dd2ab0386208c31388b5a71b1c2839
Alice Orsato <alice.orsato@autoware.it>
2019-05-23 10:57:29 +0200

    [fix] region area, context

--- WMS/Data/Tests/Scheduler/RequestForPutItemTest.cs
+++ WMS/Data/Tests/Scheduler/RequestForPutItemTest.cs
@@ -17,9 +17,14 @@ namespace Ferretto.WMS.Data.Tests.Scheduler
         [TestMethod]
         [TestProperty(
             "Description",
-            @"GIVEN a item with type Volume to put in loading unit \
-               WHEN a new request for that item is made, without specific selection parameters (eg. Sub1, Lot, etc.) \
-               THEN the new request should be applied the criteria by Volume to find right compartments")]
+            @"GIVEN an item with type FIFO \
+            AND three compartments with different FIFO start time \
+            WHEN a new put request for that item is made, without specific selection parameters (eg. Sub1, Lot, etc.) \
+            AND there are not enough compartments with FIFO start time that is newer than the item's FIFO time \
+            THEN the request is rejected \
+            WHEN a new put request for that item is made, without specific selection parameters (eg. Sub1, Lot, etc.) \
+            AND there are enough compartments with FIFO start time that is newer than the item's FIFO time \
+            THEN the request is accepted")]
         [DataRow(5)]
         [DataRow(2)]
         [DataRow(1)]
@@ -45,7 +50,7 @@ namespace Ferretto.WMS.Data.Tests.Scheduler
                 ItemId = item1.Id,
                 LoadingUnitId = this.LoadingUnit1.Id,
                 CompartmentTypeId = itemComparmentType1.CompartmentTypeId,
-                Stock = 5,
+                Stock = 3,
                 FifoStartDate = DateTime.Today.AddDays(-3),
             };
             var compartment2 = new Common.DataModels.Compartment
@@ -67,55 +72,61 @@ namespace Ferretto.WMS.Data.Tests.Scheduler
                 FifoStartDate = DateTime.Today.AddDays(-1),
             };
 
-            #endregion
-
             using (var context = this.CreateContext())
             {
-                #region Act
-
                 context.Items.Add(item1);
                 context.ItemsCompartmentTypes.Add(itemComparmentType1);
                 context.Compartments.Add(compartment1);
                 context.Compartments.Add(compartment2);
                 context.Compartments.Add(compartment3);
                 context.SaveChanges();
-
-                var schedulerRequestPutProvider = this.GetService<ISchedulerRequestPutProvider>();
-
-                var itemPutOptions = new ItemOptions
-                {
-                    AreaId = 1,
-                    RequestedQuantity = 1,
-                };
-                var itemSchedulerRequest = await schedulerRequestPutProvider.FullyQualifyPutRequestAsync(item1.Id, itemPutOptions, null);
-
-                #endregion
-
-                #region Assert
-
-                switch (fifoTime)
-                {
-                    case 1:
-
-                        Assert.IsNull(itemSchedulerRequest);
-                        break;
-
-                    case 2:
-                    case 5:
-                        Assert.IsNotNull(itemSchedulerRequest);
-                        break;
-                }
-
-                #endregion
             }
+
+            var schedulerRequestPutProvider = this.GetService<ISchedulerRequestPutProvider>();
+
+            var itemPutOptions = new ItemOptions
+            {
+                AreaId = 1,
+                RequestedQuantity = 5,
+            };
+
+            #endregion
+
+            #region Act
+
+            var itemSchedulerRequest = await schedulerRequestPutProvider.FullyQualifyPutRequestAsync(item1.Id, itemPutOptions, null);
+
+            #endregion
+
+            #region Assert
+
+            switch (fifoTime)
+            {
+                case 1:
+
+                    Assert.IsNull(itemSchedulerRequest);
+                    break;
+
+                case 2:
+                case 5:
+                    Assert.IsNotNull(itemSchedulerRequest);
+                    break;
+            }
+
+            #endregion
         }
 
         [TestMethod]
         [TestProperty(
             "Description",
-            @"GIVEN a item with type FIFO to put in loading unit \
-               WHEN a new request for that item is made, without specific selection parameters (eg. Sub1, Lot, etc.) \
-               THEN the new request should be applied the criteria by FIFO and then by Volume to find right compartments")]
+            @"GIVEN an item with type Volume \
+            AND three compartments (1 full, 1 empty and 1 half full) \
+            WHEN a new put request for that item is made \
+            AND the requested quantity is higher than the available space \
+            THEN the request is rejected \
+            WHEN a new put request for that item is made \
+            AND the requested quantity is lower than the available space \
+            THEN the request is accepted")]
         [DataRow(5)]
         [DataRow(15)]
         [DataRow(25)]
@@ -154,61 +165,65 @@ namespace Ferretto.WMS.Data.Tests.Scheduler
                 MaxCapacity = 10
             };
 
-            #endregion
-
             using (var context = this.CreateContext())
             {
-                #region Act
-
                 context.Compartments.Add(compartment1);
                 context.Compartments.Add(compartment2);
                 context.Compartments.Add(compartment3);
                 context.ItemsCompartmentTypes.Add(itemComparmentType1);
 
                 context.SaveChanges();
-
-                var schedulerRequestPutProvider = this.GetService<ISchedulerRequestPutProvider>();
-
-                var itemPutOptions1 = new ItemOptions
-                {
-                    AreaId = 1,
-                    RequestedQuantity = requestedQuantity,
-                };
-
-                // Test Put One Item -> One Shot
-                var itemSchedulerRequest = await schedulerRequestPutProvider.FullyQualifyPutRequestAsync(
-                    this.ItemVolume.Id, itemPutOptions1, null);
-
-                // test RequestedQuantity is minor or major maxcapacity of associated compartments
-                var stockTotal = compartment1.Stock + compartment2.Stock + compartment3.Stock;
-                var maxCapacityTotal = itemComparmentType1.MaxCapacity * 3;
-                var availableSpace = maxCapacityTotal - stockTotal;
-
-                #endregion
-
-                #region Assert
-
-                if (requestedQuantity <= availableSpace)
-                {
-                    Assert.IsNotNull(itemSchedulerRequest);
-                    Assert.AreEqual(itemPutOptions1.RequestedQuantity, itemSchedulerRequest.RequestedQuantity);
-                }
-                else
-                {
-                    // if full, it return failed
-                    Assert.IsNull(itemSchedulerRequest);
-                }
-
-                #endregion
             }
+
+            var schedulerRequestPutProvider = this.GetService<ISchedulerRequestPutProvider>();
+
+            var itemPutOptions1 = new ItemOptions
+            {
+                AreaId = 1,
+                RequestedQuantity = requestedQuantity,
+            };
+
+            #endregion
+
+            #region Act
+
+            // Test Put One Item -> One Shot
+            var itemSchedulerRequest = await schedulerRequestPutProvider.FullyQualifyPutRequestAsync(
+                this.ItemVolume.Id, itemPutOptions1, null);
+
+            // test RequestedQuantity is minor or major maxcapacity of associated compartments
+            var stockTotal = compartment1.Stock + compartment2.Stock + compartment3.Stock;
+            var maxCapacityTotal = itemComparmentType1.MaxCapacity * 3;
+            var availableSpace = maxCapacityTotal - stockTotal;
+
+            #endregion
+
+            #region Assert
+
+            if (requestedQuantity <= availableSpace)
+            {
+                Assert.IsNotNull(itemSchedulerRequest);
+                Assert.AreEqual(itemPutOptions1.RequestedQuantity, itemSchedulerRequest.RequestedQuantity);
+            }
+            else
+            {
+                // if full, it return failed
+                Assert.IsNull(itemSchedulerRequest);
+            }
+
+            #endregion
         }
 
         [TestMethod]
         [TestProperty(
             "Description",
-            @"GIVEN a item to put in loading unit \
-               WHEN a new request for that item is made, with specific selection parameters (eg. Sub1, Lot, etc.) \
-               THEN the new request should be elaborated and the filters are be applied.")]
+            @"GIVEN an item with FIFO management type \
+            WHEN a new put request for that item is made, with specific selection parameters (i.e. Sub1 and/or Sub2), \
+            AND the sum of the available space in the selected compartment set is greater than the requested quantity, \
+            THEN the request is accepted \
+            WHEN a new put request for that item is made, with specific selection parameters (i.e. Sub1 and/or Sub2), \
+            AND the sum of the available space in the selected compartment set is lower than the requested quantity, \
+            THEN the request is rejected")]
         [DataRow(5, "S1", "S2")]
         [DataRow(5, null, "S2")]
         [DataRow(7, "S1", "S2")]
@@ -264,45 +279,47 @@ namespace Ferretto.WMS.Data.Tests.Scheduler
                 FifoStartDate = DateTime.Today.AddDays(-1),
             };
 
-            #endregion
-
             using (var context = this.CreateContext())
             {
-                #region Act
-
                 context.Items.Add(item1);
                 context.ItemsCompartmentTypes.Add(itemComparmentType1);
                 context.Compartments.Add(compartment1);
                 context.Compartments.Add(compartment2);
                 context.Compartments.Add(compartment3);
                 context.SaveChanges();
-
-                var schedulerRequestPutProvider = this.GetService<ISchedulerRequestPutProvider>();
-                var itemPutOptions1 = new ItemOptions
-                {
-                    AreaId = 1,
-                    RequestedQuantity = requestedQuantity,
-                    Sub1 = sub1,
-                    Sub2 = sub2,
-                };
-                var itemSchedulerRequest = await schedulerRequestPutProvider.FullyQualifyPutRequestAsync(
-                    item1.Id, itemPutOptions1, null);
-
-                #endregion
-
-                #region Assert
-
-                if (requestedQuantity == 5)
-                {
-                    Assert.IsNotNull(itemSchedulerRequest);
-                }
-                else if (requestedQuantity == 7)
-                {
-                    Assert.IsNull(itemSchedulerRequest);
-                }
-
-                #endregion
             }
+
+            var schedulerRequestPutProvider = this.GetService<ISchedulerRequestPutProvider>();
+
+            var itemPutOptions1 = new ItemOptions
+            {
+                AreaId = 1,
+                RequestedQuantity = requestedQuantity,
+                Sub1 = sub1,
+                Sub2 = sub2,
+            };
+
+            #endregion
+
+            #region Act
+
+            var itemSchedulerRequest = await schedulerRequestPutProvider.FullyQualifyPutRequestAsync(
+                item1.Id, itemPutOptions1, null);
+
+            #endregion
+
+            #region Assert
+
+            if (requestedQuantity == 5)
+            {
+                Assert.IsNotNull(itemSchedulerRequest);
+            }
+            else if (requestedQuantity == 7)
+            {
+                Assert.IsNull(itemSchedulerRequest);
+            }
+
+            #endregion
         }
 
         [TestInitialize]
