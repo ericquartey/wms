9b1fd583f6497813468a405b35ad5339f1f2f4e3
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-06-05 17:10:41 +0200

    merge with develop

--- VerticalWarehouses/Machine Automation Service/MAS_AutomationService/Controllers/InstallationController_Methods.cs
+++ VerticalWarehouses/Machine Automation Service/MAS_AutomationService/Controllers/InstallationController_Methods.cs
@@ -18,13 +18,13 @@ namespace Ferretto.VW.MAS_AutomationService.Controllers
 
         private async Task ExecuteBeltBurnishingMethod(decimal upperBound, decimal lowerBound, int requiredCycles)
         {
-            var speed = await this.dataLayerConfigurationValueManagement.GetDecimalConfigurationValueAsync((long)VerticalAxis.MaxSpeed, (long)ConfigurationCategory.VerticalAxis);
+            var maxSpeed = await this.dataLayerConfigurationValueManagement.GetDecimalConfigurationValueAsync((long)VerticalAxis.MaxSpeed, (long)ConfigurationCategory.VerticalAxis);
             var acceleration = await this.dataLayerConfigurationValueManagement.GetDecimalConfigurationValueAsync((long)VerticalAxis.MaxAcceleration, (long)ConfigurationCategory.VerticalAxis);
             var deceleration = await this.dataLayerConfigurationValueManagement.GetDecimalConfigurationValueAsync((long)VerticalAxis.MaxDeceleration, (long)ConfigurationCategory.VerticalAxis);
             var resolution = await this.dataLayerConfigurationValueManagement.GetDecimalConfigurationValueAsync((long)VerticalAxis.Resolution, (long)ConfigurationCategory.VerticalAxis);
 
             IPositioningMessageData verticalPositioningMessageData = new PositioningMessageData(Axis.Vertical, MovementType.Relative, upperBound,
-                speed, acceleration, deceleration, requiredCycles, lowerBound, upperBound, resolution);
+                maxSpeed, acceleration, deceleration, requiredCycles, lowerBound, upperBound, resolution);
 
             this.eventAggregator.GetEvent<CommandEvent>().Publish(new CommandMessage(verticalPositioningMessageData, "Execute Belt Burninshing Command",
                 MessageActor.FiniteStateMachines, MessageActor.WebApi, MessageType.Positioning));
