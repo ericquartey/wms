7fb32ae75fcb35aade813e7593017239e86eac2c
Nicola Moro <nmoro@ferrettogroup.com>
2019-06-06 14:19:24 +0200

    chore: fix wrong field label

--- Common/Resources/ItemLists.Designer.cs
+++ Common/Resources/ItemLists.Designer.cs
@@ -19,7 +19,7 @@ namespace Ferretto.Common.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ItemLists {
@@ -120,6 +120,15 @@ namespace Ferretto.Common.Resources {
         public static string ItemListRequestAccepted {
             get {
                 return ResourceManager.GetString("ItemListRequestAccepted", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to List Row.
+        /// </summary>
+        public static string ItemListRow {
+            get {
+                return ResourceManager.GetString("ItemListRow", resourceCulture);
             }
         }
         
