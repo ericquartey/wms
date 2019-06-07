b5934c7b7033625de4880e1e6f36a60816b2d92f
Alice Orsato <alice.orsato@autoware.it>
2019-06-07 10:00:36 +0200

    add new model for associate item to compartment type

---
+++ WMS/App/App.Core/Models/Item/AssociateItemWithCompartmentType.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Ferretto.Common.Resources;
+
+namespace Ferretto.WMS.App.Core.Models
+{
+    public class AssociateItemWithCompartmentType : BusinessObject
+    {
+        #region Properties
+
+        [Display(Name = nameof(BusinessObjects.AbcClass), ResourceType = typeof(BusinessObjects))]
+        public string AbcClassDescription { get; set; }
+
+        public string AbcClassId { get; set; }
+
+        [Display(Name = nameof(BusinessObjects.ItemCode), ResourceType = typeof(BusinessObjects))]
+        public string Code { get; set; }
+
+        [Display(Name = nameof(BusinessObjects.ItemDescription), ResourceType = typeof(BusinessObjects))]
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        [Display(Name = nameof(BusinessObjects.ItemCategory), ResourceType = typeof(BusinessObjects))]
+        public string ItemCategoryDescription { get; set; }
+
+        public int? ItemCategoryId { get; set; }
+
+        [Display(Name = nameof(BusinessObjects.ItemMaxCapacity), ResourceType = typeof(BusinessObjects))]
+        public double? MaxCapacity { get; set; }
+
+        public string MeasureUnitDescription { get; set; }
+
+        [Display(Name = nameof(BusinessObjects.ItemAvailable), ResourceType = typeof(BusinessObjects))]
+        public double TotalAvailable { get; set; }
+
+        [Display(Name = nameof(BusinessObjects.ItemReservedForPick), ResourceType = typeof(BusinessObjects))]
+        public double TotalReservedForPick { get; set; }
+
+        [Display(Name = nameof(BusinessObjects.ItemReservedToPut), ResourceType = typeof(BusinessObjects))]
+        public double TotalReservedToPut { get; set; }
+
+        [Display(Name = nameof(BusinessObjects.ItemStock), ResourceType = typeof(BusinessObjects))]
+        public double TotalStock { get; set; }
+
+        #endregion
+    }
+}
