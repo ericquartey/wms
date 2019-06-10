ca29056140939f3f39baff97334cb37899212435
Marco Morbelli <marco.morbelli@altran.it>
2019-06-10 14:42:06 +0200

    feat(): Changed the IoDriverExceptionMessageData in a IoExceptionFieldMessageData

---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS_Utils/Messages/FieldData/IoExceptionFieldMessageData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Ferretto.VW.Common_Utils.Messages.Enumerations;
+using Ferretto.VW.MAS_Utils.Messages.FieldInterfaces;
+
+namespace Ferretto.VW.MAS_Utils.Messages.FieldData
+{
+    public class IoExceptionFieldMessageData : IIoExceptionFieldMessageData
+    {
+        #region Constructors
+
+        public IoExceptionFieldMessageData(Exception innerException, string exceptionDescription, int exceptionCode, MessageVerbosity verbosity = MessageVerbosity.Debug)
+        {
+            this.InnerException = innerException;
+            this.ExceptionDescription = exceptionDescription;
+            this.ExceptionCode = exceptionCode;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int ExceptionCode { get; }
+
+        public string ExceptionDescription { get; }
+
+        public Exception InnerException { get; }
+
+        public MessageVerbosity Verbosity { get; private set; }
+
+        #endregion
+    }
+}
