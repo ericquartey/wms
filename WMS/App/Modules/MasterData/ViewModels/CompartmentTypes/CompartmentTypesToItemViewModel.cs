581539c653447a48c00df2dca15d2df7bad63833
Giovanni Basso <giovanni.basso@altran.it>
2019-06-11 11:27:10 +0200

    chore add CompartmentTypesToItemView

---
+++ WMS/App/Modules/MasterData/ViewModels/CompartmentTypes/CompartmentTypesToItemViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ferretto.WMS.App.Controls;
+using Ferretto.WMS.App.Core.Interfaces;
+using Ferretto.WMS.App.Core.Models;
+
+namespace Ferretto.WMS.App.Modules.MasterData
+{
+    public class CompartmentTypesToItemViewModel : BaseServiceNavigationViewModel
+    {
+        #region Fields
+
+        private readonly ICompartmentTypeProvider compartmentTypeProvider;
+
+        private ItemCompartmentType selectedCompartmentType;
+
+        private string title;
+
+        private IEnumerable<ItemCompartmentType> unassociateCompartmentTypesDataSource;
+
+        #endregion
+
+        #region Constructors
+
+        public CompartmentTypesToItemViewModel(ICompartmentTypeProvider compartmentTypeProvider)
+        {
+            this.compartmentTypeProvider = compartmentTypeProvider;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ItemCompartmentType SelectedCompartmentType
+        {
+            get => this.selectedCompartmentType;
+            set => this.SetProperty(ref this.selectedCompartmentType, value);
+        }
+
+        public string Title => this.title;
+
+        public IEnumerable<ItemCompartmentType> UnassociateCompartmentTypesDataSource
+        {
+            get => this.unassociateCompartmentTypesDataSource;
+            set => this.SetProperty(ref this.unassociateCompartmentTypesDataSource, value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override async Task OnAppearAsync()
+        {
+            await this.LoadItemAreasAsync();
+        }
+
+        private async Task LoadItemAreasAsync()
+        {
+            if (!(this.Data is ItemDetails itemDetails))
+            {
+                return;
+            }
+
+            this.title = string.Format("Associate Compartment Type to This Item {0}", itemDetails.Code);
+            this.RaisePropertyChanged(nameof(this.Title));
+
+            var allowedItemAreasResult = await this.compartmentTypeProvider.GetAllUnassociatedByItemIdAsync(itemDetails.Id);
+            if (allowedItemAreasResult.Success)
+            {
+                this.UnassociateCompartmentTypesDataSource = allowedItemAreasResult.Entity;
+            }
+            else
+            {
+                this.UnassociateCompartmentTypesDataSource = null;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ WMS/App/Modules/MasterData/Views/CompartmentTypes/CompartmentTypesToItemView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Ferretto.WMS.App.Modules.MasterData
+{
+    public partial class CompartmentTypesToItemView : UserControl
+    {
+        #region Constructors
+
+        public CompartmentTypesToItemView()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
