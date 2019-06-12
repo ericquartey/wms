4faa8e7ba10a71ecd9dd9bfce167b4f2492e5cad
Nicola Moro <nmoro@ferrettogroup.com>
2019-06-12 16:24:09 +0200

    fix: add missing string resources for machine details view

--- Common/Resources/BusinessObjects.Designer.cs
+++ Common/Resources/BusinessObjects.Designer.cs
@@ -19,7 +19,7 @@ namespace Ferretto.Common.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class BusinessObjects {
@@ -169,7 +169,7 @@ namespace Ferretto.Common.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Loading Unit in bay.
+        ///   Looks up a localized string similar to Loading Unit in Bay.
         /// </summary>
         public static string BayLoadingUnitInBay {
             get {
