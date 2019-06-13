1ace70435e0d40777201288d137864ea9a440de5
Alice Orsato <alice.orsato@autoware.it>
2019-06-13 11:46:25 +0200

    add required and manage error to compartment type

--- WMS/App/App.Core/Models/CompartmentType/CompartmentType.cs
+++ WMS/App/App.Core/Models/CompartmentType/CompartmentType.cs
@@ -14,17 +14,53 @@ namespace Ferretto.WMS.App.Core.Models
 
         public int EmptyCompartmentsCount { get; set; }
 
+        [Required]
         [Display(Name = nameof(BusinessObjects.CompartmentTypeHeightDescription), ResourceType = typeof(BusinessObjects))]
         public double? Height { get; set; }
 
         [Display(Name = nameof(BusinessObjects.CompartmentTypeHeightDescription), ResourceType = typeof(BusinessObjects))]
         public string HeightDescription { get; set; }
 
+        [Required]
         [Display(Name = nameof(BusinessObjects.CompartmentTypeWidthDescription), ResourceType = typeof(BusinessObjects))]
         public double? Width { get; set; }
 
         [Display(Name = nameof(BusinessObjects.CompartmentTypeWidthDescription), ResourceType = typeof(BusinessObjects))]
         public string WidthDescription { get; set; }
+
+        #endregion
+
+        #region Indexers
+
+        public override string this[string columnName]
+        {
+            get
+            {
+                if (!this.IsValidationEnabled)
+                {
+                    return null;
+                }
+
+                var baseError = base[columnName];
+                if (!string.IsNullOrEmpty(baseError))
+                {
+                    return baseError;
+                }
+
+                switch (columnName)
+                {
+                    case nameof(this.Width):
+
+                        return this.GetErrorMessageIfNegativeOrZero(this.Width, columnName);
+
+                    case nameof(this.Height):
+
+                        return this.GetErrorMessageIfNegativeOrZero(this.Height, columnName);
+                }
+
+                return null;
+            }
+        }
 
         #endregion
     }
