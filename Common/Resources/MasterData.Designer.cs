d51178fbe1bf7189994a83a6e23552d1a59b60f6
Nicola Moro <nmoro@ferrettogroup.com>
2019-06-14 10:41:43 +0200

    fix: wrong string when compartment type has no associated items

--- Common/Resources/MasterData.Designer.cs
+++ Common/Resources/MasterData.Designer.cs
@@ -19,7 +19,7 @@ namespace Ferretto.Common.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class MasterData {
@@ -507,6 +507,15 @@ namespace Ferretto.Common.Resources {
         public static string CompartmentTypeDeletedSuccesfully {
             get {
                 return ResourceManager.GetString("CompartmentTypeDeletedSuccesfully", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to This compartment type is not associated to any item..
+        /// </summary>
+        public static string CompartmentTypeHasNoItems {
+            get {
+                return ResourceManager.GetString("CompartmentTypeHasNoItems", resourceCulture);
             }
         }
         
