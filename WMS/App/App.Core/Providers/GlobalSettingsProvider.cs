da2bad423014e1e4ac1e8bf7ff1816eae2395200
Alice Orsato <alice.orsato@autoware.it>
2019-06-20 12:09:56 +0200

    add GlobalSettings as models, providers, controllers in App.Core

---
+++ WMS/App/App.Core/Providers/GlobalSettingsProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Ferretto.WMS.App.Core.Interfaces;
+using Ferretto.WMS.App.Core.Models;
+
+namespace Ferretto.WMS.App.Core.Providers
+{
+    public class GlobalSettingsProvider : IGlobalSettingsProvider
+    {
+        #region Fields
+
+        private readonly Data.WebAPI.Contracts.IGlobalSettingsDataService globalSettingsDataService;
+
+        private readonly IMapper mapper;
+
+        #endregion
+
+        #region Constructors
+
+        public GlobalSettingsProvider(
+            Data.WebAPI.Contracts.IGlobalSettingsDataService globalSettingsDataService,
+            IMapper mapper)
+        {
+            this.globalSettingsDataService = globalSettingsDataService;
+            this.mapper = mapper;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<GlobalSettings> GetAllAsync()
+        {
+            var gS = await this.globalSettingsDataService.GetAllAsync();
+            return this.mapper.Map<GlobalSettings>(gS);
+        }
+
+        #endregion
+    }
+}
