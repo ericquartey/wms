1c6808ca6f072c9ca3a9bdd183cca5e09dad37ef
Alice Orsato <alice.orsato@autoware.it>
2019-06-20 12:09:49 +0200

    add automapper profile for GlobalSettings

---
+++ WMS/Data/Core/Maps/GlobalSettingsProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace Ferretto.WMS.Data.Core.Maps
+{
+    public class GlobalSettingsProfile : Profile
+    {
+        #region Constructors
+
+        public GlobalSettingsProfile()
+        {
+            this.CreateMap<GlobalSettings, Common.DataModels.GlobalSettings>();
+            this.CreateMap<Common.DataModels.GlobalSettings, GlobalSettings>();
+        }
+
+        #endregion
+    }
+}
