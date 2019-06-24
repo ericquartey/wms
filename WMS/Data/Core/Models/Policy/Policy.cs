b11029ee99c8f9a4b82db4c8761d511ee8825062
Nicola Moro <nmoro@ferrettogroup.com>
2019-06-24 11:16:57 +0200

    feat: Policy class now manage adding messages

--- WMS/Data/Core/Models/Policy/Policy.cs
+++ WMS/Data/Core/Models/Policy/Policy.cs
@@ -1,16 +1,31 @@
-﻿using Ferretto.Common.BLL.Interfaces.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ferretto.Common.BLL.Interfaces.Models;
 
 namespace Ferretto.WMS.Data.Core.Models
 {
     public class Policy : IPolicy
     {
+        #region Fields
+
+        private readonly List<string> errorMessages = new List<string>();
+
+        #endregion
+
         #region Properties
 
-        public bool IsAllowed { get; set; }
+        public bool IsAllowed => !this.errorMessages.Any();
 
         public string Name { get; set; }
 
-        public string Reason { get; set; }
+        public string Reason
+        {
+            get
+            {
+                var reason = string.Join(System.Environment.NewLine, this.errorMessages);
+                return !string.IsNullOrEmpty(reason) ? reason : null;
+            }
+        }
 
         public PolicyType Type { get; set; }
 
@@ -22,6 +37,14 @@ namespace Ferretto.WMS.Data.Core.Models
         {
             var policyAction = this.IsAllowed ? "allow" : "deny";
             return $"{this.Type}: {policyAction} {this.Name}";
+        }
+
+        public void AddErrorMessage(string errorMessage)
+        {
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                this.errorMessages.Add(errorMessage);
+            }
         }
 
         #endregion
