61c9ac0e6364c168d36c1550216942a54c8bd407
Alice Orsato <alice.orsato@autoware.it>
2019-06-24 16:49:01 +0200

    [add] correction to min step compartment on data service in compartment + compartment type

--- WMS/Data/Core/Providers/Data/CompartmentTypeProvider.cs
+++ WMS/Data/Core/Providers/Data/CompartmentTypeProvider.cs
@@ -20,6 +20,8 @@ namespace Ferretto.WMS.Data.Core.Providers
     {
         #region Fields
 
+        private readonly IGlobalSettingsProvider globalSettingsProvider;
+
         private readonly IItemCompartmentTypeProvider itemCompartmentTypeProvider;
 
         #endregion
@@ -29,10 +31,12 @@ namespace Ferretto.WMS.Data.Core.Providers
         public CompartmentTypeProvider(
             DatabaseContext dataContext,
             IItemCompartmentTypeProvider itemCompartmentTypeProvider,
+            IGlobalSettingsProvider globalSettingsProvider,
             INotificationService notificationService)
             : base(dataContext, notificationService)
         {
             this.itemCompartmentTypeProvider = itemCompartmentTypeProvider;
+            this.globalSettingsProvider = globalSettingsProvider;
         }
 
         #endregion
@@ -54,6 +58,12 @@ namespace Ferretto.WMS.Data.Core.Providers
                 !model.Width.HasValue)
             {
                 throw new ArgumentNullException(nameof(model));
+            }
+
+            var globalSettings = await this.globalSettingsProvider.GetGlobalSettingsAsync();
+            if (!model.ApplyCorrection(globalSettings.MinStepCompartment))
+            {
+                return new CreationErrorOperationResult<CompartmentType>();
             }
 
             using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
