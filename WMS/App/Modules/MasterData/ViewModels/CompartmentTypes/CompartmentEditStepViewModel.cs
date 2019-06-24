b1d25ada9da5ae0178770514d213f511278d523a
Giovanni Basso <giovanni.basso@altran.it>
2019-06-24 10:38:59 +0200

    chore add CompartmentEditStepView

---
+++ WMS/App/Modules/MasterData/ViewModels/CompartmentTypes/CompartmentEditStepViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Ferretto.WMS.App.Controls;
+using Ferretto.WMS.App.Core.Models;
+
+namespace Ferretto.WMS.App.Modules.MasterData
+{
+    public class CompartmentEditStepViewModel : StepViewModel
+    {
+        #region Methods
+
+        protected override Task OnAppearAsync()
+        {
+            if (this.Data is Tuple<ItemDetails, LoadingUnit> itemLoadingUnit)
+            {
+                this.Title = string.Format(Ferretto.Common.Resources.Title.CreateCompartmentForThisItem, itemLoadingUnit.Item1.Code);
+            }
+
+            return base.OnAppearAsync();
+        }
+
+        #endregion
+    }
+}
