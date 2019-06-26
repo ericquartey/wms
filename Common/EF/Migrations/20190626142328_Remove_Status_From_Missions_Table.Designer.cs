23add03c2b2395586fd8222279d7ec72fc7a74c1
Alessandro Salomone <alessandro.salomone@altran.it>
2019-06-26 16:31:41 +0200

    feat(missions): create migration to remove Status field from missions

--- Common/EF/Migrations/20190626142328_Remove_Status_From_Missions_Table.Designer.cs
+++ Common/EF/Migrations/20190626142328_Remove_Status_From_Missions_Table.Designer.cs
@@ -4,14 +4,16 @@ using Ferretto.Common.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ferretto.Common.EF.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190626142328_Remove_Status_From_Missions_Table")]
+    partial class Remove_Status_From_Missions_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
