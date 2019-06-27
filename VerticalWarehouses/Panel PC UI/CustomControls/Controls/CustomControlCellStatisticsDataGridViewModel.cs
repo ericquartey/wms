f79024e72676c622a903863eb065767154e4adec
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-06-27 13:30:40 +0200

    feat(DataGdrids): created DataGridCell and CustomControlCellStatisticsDataGridViewModel class and interface ICustomControlCellStatisticsDataGridViewModel

---
+++ VerticalWarehouses/Panel PC UI/CustomControls/Controls/CustomControlCellStatisticsDataGridViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ferretto.VW.CustomControls.Interfaces;
+using Ferretto.VW.CustomControls.Utils;
+using Prism.Mvvm;
+
+namespace Ferretto.VW.CustomControls.Controls
+{
+    public class CustomControlCellStatisticsDataGridViewModel : BindableBase, ICustomControlCellStatisticsDataGridViewModel
+    {
+        #region Fields
+
+        private ObservableCollection<DataGridItem> articles;
+
+        private DataGridItem selectedArticle;
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<DataGridItem> Articles { get => this.articles; set => this.SetProperty(ref this.articles, value); }
+
+        public BindableBase NavigationViewModel { get; set; }
+
+        public DataGridItem SelectedArticle { get => this.selectedArticle; set => this.SetProperty(ref this.selectedArticle, value); }
+
+        #endregion
+
+        #region Methods
+
+        public void ExitFromViewMethod()
+        {
+            // HACK
+        }
+
+        public Task OnEnterViewAsync()
+        {
+            // HACK
+            return Task.CompletedTask;
+        }
+
+        public void UnSubscribeMethodFromEvent()
+        {
+            // HACK
+        }
+
+        #endregion
+    }
+}
