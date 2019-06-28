5bac8fc507fb167077044a1af8bff3e8493ad358
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-06-28 11:07:57 +0200

    feat(DataGrids): implemented cells' statistics datagrid in cells' statistics view

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.OperatorApp/ViewsAndViewModels/Other/Statistics/CellsStatisticsViewModel.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.OperatorApp/ViewsAndViewModels/Other/Statistics/CellsStatisticsViewModel.cs
@@ -7,6 +7,11 @@ using Unity;
 using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
+using Ferretto.VW.CustomControls.Interfaces;
+using Ferretto.VW.CustomControls.Controls;
+using Ferretto.VW.WmsCommunication.Interfaces;
+using System.Collections.ObjectModel;
+using Ferretto.VW.CustomControls.Utils;
 
 namespace Ferretto.VW.OperatorApp.ViewsAndViewModels.Other.Statistics
 {
@@ -14,11 +19,21 @@ namespace Ferretto.VW.OperatorApp.ViewsAndViewModels.Other.Statistics
     {
         #region Fields
 
-        private IEventAggregator eventAggregator;
+        private ObservableCollection<DataGridCell> cells;
+
+        private IUnityContainer container;
+
+        private BindableBase dataGridViewModel;
+
+        private CustomControlCellStatisticsDataGridViewModel dataGridViewModelRef;
 
         private ICommand drawerCompactingButtonCommand;
 
-        private IUnityContainer container;
+        private IEventAggregator eventAggregator;
+
+        private DataGridCell selectedCell;
+
+        private IWmsDataProvider wmsDataProvider;
 
         #endregion
 
@@ -34,14 +49,18 @@ namespace Ferretto.VW.OperatorApp.ViewsAndViewModels.Other.Statistics
 
         #region Properties
 
+        public ObservableCollection<DataGridCell> Cells { get => this.cells; set => this.SetProperty(ref this.cells, value); }
+
+        public BindableBase DataGridViewModel { get => this.dataGridViewModel; set => this.SetProperty(ref this.dataGridViewModel, value); }
+
         public ICommand DrawerCompactingButtonCommand => this.drawerCompactingButtonCommand ?? (this.drawerCompactingButtonCommand = new DelegateCommand(() =>
-        {
-            NavigationService.NavigateToView<DrawerCompactingViewModel, IDrawerCompactingViewModel>();
-        }));
-
-
+                {
+                    NavigationService.NavigateToView<DrawerCompactingViewModel, IDrawerCompactingViewModel>();
+                }));
 
         public BindableBase NavigationViewModel { get; set; }
+
+        public DataGridCell SelectedCell { get => this.selectedCell; set => this.SetProperty(ref this.selectedCell, value); }
 
         #endregion
 
@@ -52,9 +71,36 @@ namespace Ferretto.VW.OperatorApp.ViewsAndViewModels.Other.Statistics
             // TODO
         }
 
+        public void InitializeViewModel(IUnityContainer container)
+        {
+            this.container = container;
+            this.dataGridViewModelRef = this.container.Resolve<ICustomControlCellStatisticsDataGridViewModel>() as CustomControlCellStatisticsDataGridViewModel;
+
+            this.wmsDataProvider = this.container.Resolve<IWmsDataProvider>();
+        }
+
         public async Task OnEnterViewAsync()
         {
-            // TODO
+            this.Cells = new ObservableCollection<DataGridCell>();
+            var random = new Random();
+            for (int i = 0; i < random.Next(5, 20); i++)
+            {
+                var cell = new DataGridCell
+                {
+                    State = "State",
+                    TotalFront = random.Next(0, 30).ToString(),
+                    TotalFrontPercentage = random.Next(0, 100).ToString(),
+                    TotalBack = random.Next(0, 30).ToString(),
+                    TotalBackPercentage = random.Next(0, 100).ToString()
+                };
+                this.Cells.Add(cell);
+            }
+            this.SelectedCell = this.Cells[0];
+
+            this.dataGridViewModelRef.Cells = this.Cells;
+            this.dataGridViewModelRef.SelectedCell = this.SelectedCell;
+
+            this.DataGridViewModel = this.dataGridViewModelRef;
         }
 
         public void SubscribeMethodToEvent()
