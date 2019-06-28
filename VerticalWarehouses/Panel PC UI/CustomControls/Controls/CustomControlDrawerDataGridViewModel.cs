638eb125e9a88b301bd35cab51b88c809ee9cd4b
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-06-28 16:55:33 +0200

    feat(DataGrids): created data grids' view models and interfaces

---
+++ VerticalWarehouses/Panel PC UI/CustomControls/Controls/CustomControlDrawerDataGridViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Ferretto.VW.CustomControls.Interfaces;
+using Prism.Mvvm;
+
+namespace Ferretto.VW.CustomControls.Controls
+{
+    public class CustomControlDrawerDataGridViewModel : BindableBase, ICustomControlDrawerDataGridViewModel
+    {
+        #region Properties
+
+        public BindableBase NavigationViewModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        #endregion
+
+        #region Methods
+
+        public void ExitFromViewMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnEnterViewAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnSubscribeMethodFromEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ VerticalWarehouses/Panel PC UI/CustomControls/Controls/CustomControlListDataGridViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ferretto.VW.CustomControls.Interfaces;
+using Prism.Mvvm;
+
+namespace Ferretto.VW.CustomControls.Controls
+{
+    public class CustomControlListDataGridViewModel : BindableBase, ICustomControlListDataGridViewModel
+    {
+        #region Properties
+
+        public BindableBase NavigationViewModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        #endregion
+
+        #region Methods
+
+        public void ExitFromViewMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnEnterViewAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnSubscribeMethodFromEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
