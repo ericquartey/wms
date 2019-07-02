0b38f286fdc270cc45cfc646116d52e70a341e3e
Nicolò Padovani <nicolo.padovani@autoware.it>
2019-07-02 10:55:22 +0200

    feat(DataGrid): implemented maintenance data grid

--- VerticalWarehouses/Panel PC UI/CustomControls/Controls/CustomControlMaintenanceDataGridViewModel.cs
+++ VerticalWarehouses/Panel PC UI/CustomControls/Controls/CustomControlMaintenanceDataGridViewModel.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Ferretto.VW.CustomControls.Interfaces;
+using Ferretto.VW.CustomControls.Utils;
 using Prism.Mvvm;
 
 namespace Ferretto.VW.CustomControls.Controls
 {
     public class CustomControlMaintenanceDataGridViewModel : BindableBase, ICustomControlMaintenanceDataGridViewModel
     {
+        #region Fields
+
+        private ObservableCollection<DataGridKit> kits;
+
+        private DataGridKit selectedKit;
+
+        #endregion
+
         #region Properties
 
-        public BindableBase NavigationViewModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ObservableCollection<DataGridKit> Kits { get => this.kits; set => this.SetProperty(ref this.kits, value); }
+
+        public BindableBase NavigationViewModel { get; set; }
+
+        public DataGridKit SelectedKit { get => this.selectedKit; set => this.SetProperty(ref this.selectedKit, value); }
 
         #endregion
 
@@ -17,17 +31,18 @@ namespace Ferretto.VW.CustomControls.Controls
 
         public void ExitFromViewMethod()
         {
-            throw new NotImplementedException();
+            // HACK
         }
 
         public Task OnEnterViewAsync()
         {
-            throw new NotImplementedException();
+            // HACK
+            return Task.CompletedTask;
         }
 
         public void UnSubscribeMethodFromEvent()
         {
-            throw new NotImplementedException();
+            // HACK
         }
 
         #endregion
