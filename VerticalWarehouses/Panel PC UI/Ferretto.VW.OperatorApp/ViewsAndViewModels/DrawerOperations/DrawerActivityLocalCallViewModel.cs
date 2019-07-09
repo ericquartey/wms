f7c0b621ad97b5f4bc62ffeb481a945757188fb9
Ennio Di Bernardo <edibernardo@ferrettogroup.com>
2019-07-09 13:49:38 +0200

    Updated grid dimension and some button position.

---
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.OperatorApp/ViewsAndViewModels/DrawerOperations/DrawerActivityLocalCallViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Ferretto.Common.Controls.WPF;
+using Ferretto.VW.MAS_AutomationService.Contracts;
+using Ferretto.VW.OperatorApp.Interfaces;
+using Ferretto.VW.OperatorApp.ServiceUtilities;
+using Ferretto.VW.OperatorApp.ServiceUtilities.Interfaces;
+using Ferretto.VW.OperatorApp.ViewsAndViewModels.DrawerOperations;
+using Ferretto.VW.OperatorApp.ViewsAndViewModels.DrawerOperations.Details;
+using Ferretto.VW.Utils.Source;
+using Ferretto.VW.Utils.Source.Filters;
+using Ferretto.VW.WmsCommunication.Interfaces;
+using Ferretto.VW.WmsCommunication.Source;
+using Ferretto.WMS.Data.WebAPI.Contracts;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using Unity;
+
+namespace Ferretto.VW.OperatorApp.ViewsAndViewModels.DrawerOperations
+{
+    class DrawerActivityLocalCallViewModel
+    {
+    }
+}
