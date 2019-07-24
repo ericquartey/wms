a267682e18f2f6371f2e83b47a9bb98be305a585
Nicola Moro <nmoro@ferrettogroup.com>
2019-07-24 16:55:37 +0200

    refactor: specify generic key type for data models interface IDataModel

---
+++ Common/DataModels/Models/LoadingUnitStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Ferretto.Common.DataModels
+{
+    // Stato Udc
+    public sealed class LoadingUnitStatus : IDataModel<string>
+    {
+        #region Properties
+
+        public string Description { get; set; }
+
+        public string Id { get; set; }
+
+        public IEnumerable<LoadingUnit> LoadingUnits { get; set; }
+
+        #endregion
+    }
+}
