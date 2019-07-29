f5382f044ef9c107ceb97872a66d9c334782062d
Danilo <dcorallo@ferrettogroup.com>
2019-07-29 16:11:45 +0200

    Socket architecture

--- VerticalWarehouses/Simulator/Ferretto.VW.Simulator.Services/Interfaces/IMachineService.cs
+++ VerticalWarehouses/Simulator/Ferretto.VW.Simulator.Services/Interfaces/IMachineService.cs
@@ -19,6 +19,8 @@ namespace Ferretto.VW.Simulator.Services.Interfaces
 
         Task ProcessStartSimulatorAsync();
 
+        Task ProcessStopSimulatorAsync();
+
         #endregion
     }
 }
--- VerticalWarehouses/Simulator/Ferretto.VW.Simulator.Services/MachineService.cs
+++ VerticalWarehouses/Simulator/Ferretto.VW.Simulator.Services/MachineService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,13 +19,23 @@ namespace Ferretto.VW.Simulator.Services
 
         private readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
+        private CancellationTokenSource cts = new CancellationTokenSource();
+
+        private readonly TcpListener listenerInverter = new TcpListener(IPAddress.Any, 17221);
+
+        private readonly TcpListener listenerIoDriver1 = new TcpListener(IPAddress.Any, 19550);
+
+        private readonly TcpListener listenerIoDriver2 = new TcpListener(IPAddress.Any, 19551);
+
+        private readonly TcpListener listenerIoDriver3 = new TcpListener(IPAddress.Any, 19552);
+
         #endregion
 
         #region Properties
 
         public bool IsStartedSimulator { get; private set; }
 
-        public bool NotIsStartedSimulator { get => !this.IsStartedSimulator; }
+        public bool NotIsStartedSimulator => !this.IsStartedSimulator;
 
         #endregion
 
@@ -37,12 +49,83 @@ namespace Ferretto.VW.Simulator.Services
         {
             this.Logger.Trace("1:ProcessStartSimulator");
 
-            await Task.Delay(100);
+            this.cts = new CancellationTokenSource();
+            this.listenerInverter.Start();
+            this.listenerIoDriver1.Start();
+            this.listenerIoDriver2.Start();
+            this.listenerIoDriver3.Start();
 
+            Task.Run(() => this.AcceptClient(this.listenerInverter, this.cts.Token));
+            Task.Run(() => this.AcceptClient(this.listenerIoDriver1, this.cts.Token));
+            Task.Run(() => this.AcceptClient(this.listenerIoDriver2, this.cts.Token));
+            Task.Run(() => this.AcceptClient(this.listenerIoDriver3, this.cts.Token));
+
+            await Task.Delay(100);
             this.IsStartedSimulator = true;
 
             this.RaisePropertyChanged(nameof(this.IsStartedSimulator));
             this.RaisePropertyChanged(nameof(this.NotIsStartedSimulator));
+        }
+
+        public async Task ProcessStopSimulatorAsync()
+        {
+            this.Logger.Trace("1:ProcessStopSimulator");
+
+            this.cts.Cancel();
+            this.listenerInverter.Stop();
+            this.listenerIoDriver1.Stop();
+            this.listenerIoDriver2.Stop();
+            this.listenerIoDriver3.Stop();
+
+            await Task.Delay(100);
+            this.IsStartedSimulator = false;
+
+            this.RaisePropertyChanged(nameof(this.IsStartedSimulator));
+            this.RaisePropertyChanged(nameof(this.NotIsStartedSimulator));
+
+        }
+
+        private void AcceptClient(TcpListener listener, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var client = listener.AcceptTcpClient();
+                Task.Run(() => this.ManageClient(client, this.cts.Token));
+            }
+        }
+
+        private void ManageClient(TcpClient client, CancellationToken token)
+        {
+            using (client)
+            {
+                var buffer = new byte[1024];
+                Socket socket = client.Client;
+                while (!token.IsCancellationRequested)
+                {
+                    if (socket != null && socket.Connected)
+                    {
+                        if (socket.Poll(0, SelectMode.SelectRead))
+                        {
+                            var bytes = socket.Receive(buffer);
+                            if (bytes > 0)
+                            {
+                                // TODO: Parse
+
+                                // TODO: Reply
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
         }
 
         #endregion
