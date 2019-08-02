7a94aa798850a73fd648e9239e71811c63e95202
Antonello Giuseppe Bianchi <antonello@nementis.eu>
2019-08-02 11:24:07 +0200

    Fix:FSM template state machine update

---
+++ VerticalWarehouses/Machine Automation Service/MAS_FiniteStateMachines/Template/Models/TemplateData.cs
@@ -0,0 +1,8 @@
+﻿using Ferretto.VW.MAS.FiniteStateMachines.Template.Interfaces;
+
+namespace Ferretto.VW.MAS.FiniteStateMachines.Template.Models
+{
+    public class TemplateData : ITemplateData
+    {
+    }
+}
