3b6da20aae50a24427f4933480462c65b1549e77
Mirco Solato <msolato@ferrettogroup.com>
2019-08-05 12:06:14 +0200

    interface imachineservice

--- VerticalWarehouses/Simulator/Ferretto.VW.Simulator.Services/Interfaces/IMachineService.cs
+++ VerticalWarehouses/Simulator/Ferretto.VW.Simulator.Services/Interfaces/IMachineService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
+using Ferretto.VW.Simulator.Services.Models;
 
 namespace Ferretto.VW.Simulator.Services.Interfaces
 {
@@ -9,7 +11,31 @@ namespace Ferretto.VW.Simulator.Services.Interfaces
     {
         #region Properties
 
+        ObservableCollection<InverterModel> Inverters { get; set; }
+
+        InverterModel Inverters00 { get; set; }
+
+        InverterModel Inverters01 { get; set; }
+
+        InverterModel Inverters02 { get; set; }
+
+        InverterModel Inverters03 { get; set; }
+
+        InverterModel Inverters04 { get; set; }
+
+        InverterModel Inverters05 { get; set; }
+
+        InverterModel Inverters06 { get; set; }
+
+        InverterModel Inverters07 { get; set; }
+
         bool IsStartedSimulator { get; }
+
+        IODeviceModel RemoteIOs01 { get; set; }
+
+        IODeviceModel RemoteIOs02 { get; set; }
+
+        IODeviceModel RemoteIOs03 { get; set; }
 
         #endregion
 
