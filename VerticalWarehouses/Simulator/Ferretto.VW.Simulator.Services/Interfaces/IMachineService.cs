1d8103a8b9da64990d860f5e30d0ea119bdff1f8
Mirco Solato <msolato@ferrettogroup.com>
2019-08-05 11:47:56 +0200

    feat: first release

---
+++ VerticalWarehouses/Simulator/Ferretto.VW.Simulator.Services/Interfaces/IMachineService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferretto.VW.Simulator.Services.Interfaces
+{
+    public interface IMachineService
+    {
+        #region Properties
+
+        bool IsStartedSimulator { get; }
+
+        #endregion
+
+        #region Methods
+
+        Task ProcessStartSimulatorAsync();
+
+        Task ProcessStopSimulatorAsync();
+
+        #endregion
+    }
+}
---
+++ VerticalWarehouses/Simulator/Ferretto.VW.Simulator.Services/Models/IODeviceModel.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ferretto.VW.Simulator.Services.Helpers;
+using Prism.Mvvm;
+
+namespace Ferretto.VW.Simulator.Services.Models
+{
+    public enum IoPorts
+    {
+        #region Outputs
+
+        ResetSecurity = 0,
+
+        ElevatorMotor = 1,
+
+        CradleMotor = 2,
+
+        MeasureBarrier = 3,
+
+        BayLight = 4,
+
+        FreeSensor1 = 5,
+
+        EndMissionRobot = 6,
+
+        ReadyWarehouseRobot = 7,
+
+        #endregion
+
+        #region Inputs
+
+        NormalState = 0,
+
+        MushroomEmergency = 1,
+
+        MicroCarterLeftSideBay = 2,
+
+        MicroCarterRightSideBay = 3,
+
+        AntiIntrusionBarrierBay = 4,
+
+        LoadingUnitInBay = 5,
+
+        LoadingUnitInLowerBay = 6,
+
+        FreeSensor = 7,
+
+        ElevatorMotorFeedback = 8,
+
+        CradleMotorFeedback = 9,
+
+        DrawerInMachineSide = 10,
+
+        DrawerInOperatorSide = 11,
+
+        CalibrationBarrierLight = 12,
+
+        HookTrolley = 14,
+
+        FinePickingRobot = 15
+
+        #endregion
+    }
+
+    public class IODeviceModel : BindableBase
+    {
+        #region Fields
+
+        public byte[] Buffer;
+
+        private bool enabled = true;
+
+        private ObservableCollectionWithItemNotify<BitModel> inputs = new ObservableCollectionWithItemNotify<BitModel>();
+
+        #endregion
+
+        #region Constructors
+
+        public IODeviceModel()
+        {
+            this.inputs.Add(new BitModel("Id:00", false));
+            this.inputs.Add(new BitModel("Id:01", false));
+            this.inputs.Add(new BitModel("Id:02", false));
+            this.inputs.Add(new BitModel("Id:03", false));
+            this.inputs.Add(new BitModel("Id:04", false));
+            this.inputs.Add(new BitModel("Id:05", false));
+            this.inputs.Add(new BitModel("Id:06", false));
+            this.inputs.Add(new BitModel("Id:07", false));
+            this.inputs.Add(new BitModel("Id:08", false));
+            this.inputs.Add(new BitModel("Id:09", false));
+            this.inputs.Add(new BitModel("Id:10", false));
+            this.inputs.Add(new BitModel("Id:11", false));
+            this.inputs.Add(new BitModel("Id:12", false));
+            this.inputs.Add(new BitModel("Id:13", false));
+            this.inputs.Add(new BitModel("Id:14", false));
+            this.inputs.Add(new BitModel("Id:15", false));
+
+            this.inputs.PropertyChanged += (s, e) =>
+            {
+                this.RaisePropertyChanged(nameof(this.InputsValue));
+            };
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool Enabled { get => this.enabled; set => this.SetProperty(ref this.enabled, value, () => this.RaisePropertyChanged(nameof(this.Enabled))); }
+
+        public byte FirmwareVersion { get; set; } = 0x10;
+
+        public int Id { get; set; }
+
+        public ObservableCollectionWithItemNotify<BitModel> Inputs
+        {
+            get => this.inputs;
+            set => this.SetProperty(ref this.inputs, value);
+        }
+
+        public ushort InputsValue
+        {
+            get
+            {
+                ushort result = 0;
+                for (int i = 0; i < this.Inputs.Count; i++)
+                {
+                    if (this.Inputs[i].Value)
+                    {
+                        result += (ushort)Math.Pow(2, i);
+                    }
+                }
+                return result;
+            }
+        }
+
+        #endregion
+    }
+}
