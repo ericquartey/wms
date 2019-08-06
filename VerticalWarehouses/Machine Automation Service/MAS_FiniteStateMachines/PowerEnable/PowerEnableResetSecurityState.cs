40e8226f1fa7e9626b6a6163035bcc6ea1d49843
Danilo <dcorallo@ferrettogroup.com>
2019-08-06 12:18:16 +0200

    Fix message

--- VerticalWarehouses/Machine Automation Service/MAS_FiniteStateMachines/PowerEnable/PowerEnableResetSecurityState.cs
+++ VerticalWarehouses/Machine Automation Service/MAS_FiniteStateMachines/PowerEnable/PowerEnableResetSecurityState.cs
@@ -82,6 +82,7 @@ namespace Ferretto.VW.MAS.FiniteStateMachines.PowerEnable
                 FieldMessageActor.IoDriver,
                 FieldMessageActor.FiniteStateMachines,
                 FieldMessageType.ResetSecurity);
+            this.ParentStateMachine.PublishFieldCommandMessage(commandMessage);
 
             this.Logger.LogTrace($"1:Publishing Field Command Message {commandMessage.Type} Destination {commandMessage.Destination}");
         }
