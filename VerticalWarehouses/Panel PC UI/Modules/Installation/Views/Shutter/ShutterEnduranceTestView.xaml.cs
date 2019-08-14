d9c11bc778b55320a404f8ef04bb83eaf50ea68c
Alessandro Salomone <alessandro.salomone@altran.it>
2019-08-14 10:10:21 +0200

    fix shutter control views

---
+++ VerticalWarehouses/Panel PC UI/Modules/Installation/Views/Shutter/ShutterEnduranceTestView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Ferretto.VW.App.Controls;
+
+namespace Ferretto.VW.App.Installation.Views
+{
+    public partial class ShutterEnduranceTestView : View
+    {
+        #region Constructors
+
+        public ShutterEnduranceTestView()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
