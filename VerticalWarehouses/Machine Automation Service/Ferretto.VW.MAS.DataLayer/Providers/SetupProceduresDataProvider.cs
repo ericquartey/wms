0a045d9d20d26238a0879b2b221df51bc9bf7f1a
Alessandro Salomone <alessandro.salomone@altran.it>
2019-10-08 17:15:40 +0200

    add setup parameters

---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Providers/SetupProceduresDataProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Ferretto.VW.MAS.DataLayer.DatabaseContext;
+using Ferretto.VW.MAS.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ferretto.VW.MAS.DataLayer
+{
+    internal class SetupProceduresDataProvider : ISetupProceduresDataProvider
+    {
+        #region Fields
+
+        private readonly DataLayerContext dataContext;
+
+        #endregion
+
+        #region Constructors
+
+        public SetupProceduresDataProvider(DataLayerContext dataContext)
+        {
+            this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+        }
+
+        #endregion
+
+        #region Methods
+
+        public SetupProceduresSet GetAll()
+        {
+            return this.dataContext.SetupProceduresSets
+                .Include(s => s.BayHeightCheck)
+                .Include(s => s.BeltBurnishingTest)
+                .Include(s => s.CellPanelsCheck)
+                .Include(s => s.CellsHeightCheck)
+                .Include(s => s.ShutterTest)
+                .Include(s => s.VerticalResolutionCalibration)
+                .Single();
+        }
+
+        #endregion
+    }
+}
