ece6a67525930a5c9585552e79c7b33504403ed7
Stefano Prandini <sprandini@ferrettogroup.com>
2019-10-09 14:17:46 +0200

    fix: semiauto carousel positioning

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/Homing/HomingSwitchAxisDoneState.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/Homing/HomingSwitchAxisDoneState.cs
@@ -5,6 +5,7 @@ using Ferretto.VW.MAS.DeviceManager.Homing.Interfaces;
 using Ferretto.VW.MAS.Utils.Enumerations;
 using Ferretto.VW.MAS.Utils.Messages;
 using Ferretto.VW.MAS.Utils.Messages.FieldData;
+using Ferretto.VW.MAS.Utils.Messages.FieldInterfaces;
 using Microsoft.Extensions.Logging;
 
 // ReSharper disable ArrangeThisQualifier
@@ -65,6 +66,33 @@ namespace Ferretto.VW.MAS.DeviceManager.Homing
                         this.stateData.FieldMessage = message;
                         this.ParentStateMachine.ChangeState(new HomingErrorState(this.stateData));
                         break;
+                }
+            }
+            else if (message.Type == FieldMessageType.InverterStatusUpdate)
+            {
+                if (message.Data is IInverterStatusUpdateFieldMessageData dataInverters)
+                {
+                    if (dataInverters.CurrentPosition != null)
+                    {
+                        var notificationData = new PositioningMessageData();
+                        notificationData.CurrentPosition = dataInverters.CurrentPosition.Value;
+                        notificationData.AxisMovement = dataInverters.CurrentAxis;
+                        if (this.machineData.AxisToCalibrate == Axis.BayChain)
+                        {
+                            notificationData.MovementMode = MovementMode.BayChain;
+                        }
+                        var notificationMessage = new NotificationMessage(
+                            notificationData,
+                            $"Current Encoder position: {notificationData.CurrentPosition}",
+                            MessageActor.AutomationService,
+                            MessageActor.FiniteStateMachines,
+                            MessageType.Positioning,
+                            this.machineData.RequestingBay,
+                            this.machineData.TargetBay,
+                            MessageStatus.OperationExecuting);
+
+                        this.ParentStateMachine.PublishNotificationMessage(notificationMessage);
+                    }
                 }
             }
         }
