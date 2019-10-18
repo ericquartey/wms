9e3e27721785b4e2d52eb75b2a854c800e8e1822
Giovanni Basso <giovanni.basso@altran.it>
2019-10-18 14:21:58 +0200

    chore add zero chain sensors on BaySensorsView

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Resources/InstallationApp.Designer.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Resources/InstallationApp.Designer.cs
@@ -19,7 +19,7 @@ namespace Ferretto.VW.App.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class InstallationApp {
@@ -2946,6 +2946,15 @@ namespace Ferretto.VW.App.Resources {
         public static string WeightControl {
             get {
                 return ResourceManager.GetString("WeightControl", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Zero chain.
+        /// </summary>
+        public static string ZeroChain {
+            get {
+                return ResourceManager.GetString("ZeroChain", resourceCulture);
             }
         }
         
