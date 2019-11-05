e8a22a8ff32673a0846a1791f2ac64f3779b8d0e
Mirco Solato <msolato@ferrettogroup.com>
2019-11-05 10:41:56 +0100

    fix bug 4022
    Related Work Items: #4022

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Providers/BaysProvider.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Providers/BaysProvider.cs
@@ -11,6 +11,7 @@ using Ferretto.VW.MAS.InverterDriver.Contracts;
 using Ferretto.VW.MAS.Utils.Enumerations;
 using Ferretto.VW.MAS.Utils.Events;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Prism.Events;
 
 // ReSharper disable ArrangeThisQualifier
@@ -19,6 +20,11 @@ namespace Ferretto.VW.MAS.DataLayer
     internal sealed class BaysProvider : BaseProvider, IBaysProvider
     {
         #region Fields
+
+        private static readonly MemoryCacheEntryOptions CacheOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromMinutes(1));
+
+        private readonly IMemoryCache cache;
 
         private readonly DataLayerContext dataContext;
 
@@ -40,12 +46,14 @@ namespace Ferretto.VW.MAS.DataLayer
             DataLayerContext dataContext,
             IEventAggregator eventAggregator,
             IMachineProvider machineProvider,
-            IElevatorDataProvider elevatorDataProvider)
+            IElevatorDataProvider elevatorDataProvider,
+            IMemoryCache memoryCache)
             : base(eventAggregator)
         {
             this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
             this.machineProvider = machineProvider ?? throw new ArgumentNullException(nameof(machineProvider));
             this.elevatorDataProvider = elevatorDataProvider ?? throw new ArgumentNullException(nameof(elevatorDataProvider));
+            this.cache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
 
             this.notificationEvent = eventAggregator.GetEvent<NotificationEvent>();
         }
@@ -210,7 +218,7 @@ namespace Ferretto.VW.MAS.DataLayer
 
                 if (bay is null)
                 {
-                    if (this.dataContext.ElevatorAxes.Any(a => a.Inverter.Index == inverterIndex))
+                    if (this.GetElevatorAxes().Any(a => a.Inverter.Index == inverterIndex))
                     {
                         return BayNumber.ElevatorBay;
                     }
@@ -345,6 +353,28 @@ namespace Ferretto.VW.MAS.DataLayer
                 }
 
                 return bay.ChainOffset;
+            }
+        }
+
+        public IEnumerable<ElevatorAxis> GetElevatorAxes()
+        {
+            lock (this.dataContext)
+            {
+                var cacheKey = GetElevatorAxesCacheKey();
+                if (!this.cache.TryGetValue(cacheKey, out IEnumerable<ElevatorAxis> cacheEntry))
+                {
+                    cacheEntry = this.dataContext.ElevatorAxes
+                        .Include(i => i.Inverter)
+                        .ToList();
+                    if (cacheEntry is null)
+                    {
+                        throw new EntityNotFoundException(string.Empty);
+                    }
+
+                    this.cache.Set(cacheKey, cacheEntry, CacheOptions);
+                }
+
+                return cacheEntry;
             }
         }
 
@@ -597,6 +627,8 @@ namespace Ferretto.VW.MAS.DataLayer
                 return this.GetByNumber(bayNumber);
             }
         }
+
+        internal static string GetElevatorAxesCacheKey() => $"{nameof(GetElevatorAxes)}";
 
         private void Update(Bay bay)
         {
