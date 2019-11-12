6c93e3f8a18aeba87343aada0744b68c3e36d0f9
Alessandro Gennaro <agennaro@ferrettogroup.com>
2019-11-12 18:33:19 +0100

    feat: Change log error message body inside the ComputePositioningValues method.

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.InverterDriver/Providers/InvertersProvider.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.InverterDriver/Providers/InvertersProvider.cs
@@ -119,7 +119,7 @@ namespace Ferretto.VW.MAS.InverterDriver
 
                 if (position < 0)
                 {
-                    throw new Exception($"The requested target position ({positioningData.TargetPosition}) is below the axis lower bound ({axis.LowerBound}).");
+                    throw new Exception($"The requested position ({position}) is below the axis lower bound ({axis.LowerBound}).");
                 }
 
                 if (axis.Orientation == Orientation.Vertical && positioningData.ComputeElongation)
