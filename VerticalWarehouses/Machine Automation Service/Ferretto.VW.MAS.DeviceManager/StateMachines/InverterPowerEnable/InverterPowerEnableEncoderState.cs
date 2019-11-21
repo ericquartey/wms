4677c6c01f4207f631551ff8e60a87855ae437e0
Stefano Prandini <sprandini@ferrettogroup.com>
2019-11-21 17:40:00 +0100

    clean

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/StateMachines/InverterPowerEnable/InverterPowerEnableEncoderState.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/StateMachines/InverterPowerEnable/InverterPowerEnableEncoderState.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Ferretto.VW.CommonUtils.Messages;
 using Ferretto.VW.CommonUtils.Messages.Enumerations;
 using Ferretto.VW.MAS.DataLayer;
