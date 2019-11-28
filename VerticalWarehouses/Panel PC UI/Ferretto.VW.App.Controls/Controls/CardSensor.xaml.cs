7b5f38bcdcff002e693289fd720e62b3e932a885
Mirco Solato <msolato@ferrettogroup.com>
2019-11-28 13:24:54 +0100

    restyle home menu buttom

---
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Controls/Controls/CardSensor.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ferretto.VW.App.Controls.Controls
+{
+    /// <summary>
+    /// Interaction logic for CardSensor.xaml
+    /// </summary>
+    public partial class CardSensor : UserControl
+    {
+        #region Fields
+
+        public static readonly DependencyProperty CardTextProperty =
+            DependencyProperty.Register(
+                nameof(CardText),
+                typeof(string),
+                typeof(CardSensor),
+                new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty CardValueProperty =
+            DependencyProperty.Register(
+                nameof(CardValue),
+                typeof(string),
+                typeof(CardSensor),
+                new PropertyMetadata(string.Empty));
+
+        #endregion
+
+        #region Constructors
+
+        public CardSensor()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string CardText
+        {
+            get => (string)this.GetValue(CardTextProperty);
+            set => this.SetValue(CardTextProperty, value);
+        }
+
+        public string CardValue
+        {
+            get => (string)this.GetValue(CardValueProperty);
+            set => this.SetValue(CardValueProperty, value);
+        }
+
+        #endregion
+    }
+}
--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Installation/ViewModels/Movements/MovementsViewModel.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Installation/ViewModels/Movements/MovementsViewModel.cs
@@ -7,17 +7,68 @@ using Ferretto.VW.App.Controls;
 using Ferretto.VW.App.Resources;
 using Ferretto.VW.App.Services;
 using Ferretto.VW.MAS.AutomationService.Contracts;
+using Ferretto.VW.MAS.AutomationService.Contracts.Hubs;
 using Prism.Regions;
 
 namespace Ferretto.VW.App.Installation.ViewModels
 {
     internal sealed class MovementsViewModel : BaseMainViewModel
     {
+        #region Fields
+
+        private DelegateCommand goToMovementsGuidedCommand;
+
+        private DelegateCommand goToMovementsManualCommand;
+
+        private bool isMovementsGuided = true;
+
+        private bool isWaitingForResponse;
+
+        private string title;
+
+        #endregion
+
         #region Constructors
 
         public MovementsViewModel()
             : base(PresentationMode.Installer)
         {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public override EnableMask EnableMask => EnableMask.MachinePoweredOn;
+
+        public ICommand GoToMovementsGuidedCommand =>
+            this.goToMovementsGuidedCommand
+            ??
+            (this.goToMovementsGuidedCommand = new DelegateCommand(
+                () => this.GoToMovementsExecuteCommand(true),
+                this.CanGoToMovementsExecuteCommand));
+
+        public ICommand GoToMovementsManualCommand =>
+            this.goToMovementsManualCommand
+            ??
+            (this.goToMovementsManualCommand = new DelegateCommand(
+                () => this.GoToMovementsExecuteCommand(false),
+                this.CanGoToMovementsExecuteCommand));
+
+        public bool IsMovementsGuided => this.isMovementsGuided;
+
+        public bool IsMovementsManual => !this.isMovementsGuided;
+
+        public bool IsWaitingForResponse
+        {
+            get => this.isWaitingForResponse;
+            set => this.SetProperty(ref this.isWaitingForResponse, value, this.RaiseCanExecuteChanged);
+        }
+
+        public string Title
+        {
+            get => this.title;
+            set => this.SetProperty(ref this.title, value, this.RaiseCanExecuteChanged);
         }
 
         #endregion
@@ -31,15 +82,55 @@ namespace Ferretto.VW.App.Installation.ViewModels
 
         public override async Task OnAppearedAsync()
         {
+            this.IsWaitingForResponse = true;
+
             await base.OnAppearedAsync();
 
             try
             {
+                this.GoToMovementsExecuteCommand(this.isMovementsGuided);
             }
             catch (System.Exception ex)
             {
                 this.ShowNotification(ex);
             }
+            finally
+            {
+                this.IsWaitingForResponse = false;
+            }
+        }
+
+        protected override async Task OnMachineModeChangedAsync(MachineModeChangedEventArgs e)
+        {
+            await base.OnMachineModeChangedAsync(e);
+            this.RaiseCanExecuteChanged();
+        }
+
+        private bool CanGoToMovementsExecuteCommand()
+        {
+            return !this.IsWaitingForResponse;
+        }
+
+        private void GoToMovementsExecuteCommand(bool isGuided)
+        {
+            this.isMovementsGuided = isGuided;
+            if (isGuided)
+            {
+                this.Title = "Movimenti Guidati";
+            }
+            else
+            {
+                this.Title = "Movimenti Manuali";
+            }
+        }
+
+        private void RaiseCanExecuteChanged()
+        {
+            this.goToMovementsGuidedCommand?.RaiseCanExecuteChanged();
+            this.goToMovementsManualCommand?.RaiseCanExecuteChanged();
+
+            this.RaisePropertyChanged(nameof(this.IsMovementsGuided));
+            this.RaisePropertyChanged(nameof(this.IsMovementsManual));
         }
 
         #endregion
