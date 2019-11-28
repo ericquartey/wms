b994dc1829b4c5af0f11b25c55b762021279b4c4
Mirco Solato <msolato@ferrettogroup.com>
2019-11-28 12:17:53 +0100

    fix: per designalre elevatore pieno o vuoto controllo solo i sensori

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/Providers/ElevatorProvider.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/Providers/ElevatorProvider.cs
@@ -241,8 +241,8 @@ namespace Ferretto.VW.MAS.DeviceManager.Providers
             //           the elevator must be full with pawl in non-zero position
             var loadingUnit = this.elevatorDataProvider.GetLoadingUnitOnBoard();
             var isChainInZeroPosition = this.machineResourcesProvider.IsSensorZeroOnCradle;
-            var isElevatorFull = this.machineResourcesProvider.IsDrawerCompletelyOnCradle && loadingUnit != null;
-            var isElevatorEmpty = this.machineResourcesProvider.IsDrawerCompletelyOffCradle && loadingUnit is null;
+            var isElevatorFull = this.machineResourcesProvider.IsDrawerCompletelyOnCradle; // && loadingUnit != null;
+            var isElevatorEmpty = this.machineResourcesProvider.IsDrawerCompletelyOffCradle; // && loadingUnit is null;
 
             if (!(isElevatorFull && !isChainInZeroPosition) && !(isElevatorEmpty && isChainInZeroPosition))
             {
