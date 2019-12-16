6ec88dfe1e09e868d15685784295153b4a5b0cd0
Prandini Stefano <sprandini@ferrettogroup.com>
2019-12-16 22:32:01 +0100

    fix: ScaleMovementsByWeight

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataModels/Elevator/StepMovementParameters.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataModels/Elevator/StepMovementParameters.cs
@@ -25,17 +25,17 @@ namespace Ferretto.VW.MAS.DataModels
 
             if (this.AdjustByWeight)
             {
-                var min = Math.Min(axis.FullLoadMovement.Speed, this.Speed);
-                var max = Math.Max(axis.EmptyLoadMovement.Speed, this.Speed);
+                if (this.Speed >= axis.FullLoadMovement.Speed && this.Speed <= axis.EmptyLoadMovement.Speed)
+                {
+                    var deltaSpeed = (axis.EmptyLoadMovement.Speed - axis.FullLoadMovement.Speed) * scalingFactor;
+                    this.Speed = Math.Max(this.Speed - deltaSpeed, axis.FullLoadMovement.Speed);
+                }
 
-                var deltaSpeed = (max - min) * scalingFactor;
-                this.Speed = Math.Max(this.Speed - deltaSpeed, axis.FullLoadMovement.Speed);
-
-                min = Math.Min(axis.FullLoadMovement.Acceleration, this.Acceleration);
-                max = Math.Max(axis.EmptyLoadMovement.Acceleration, this.Acceleration);
-
-                var deltaAcceleration = (max - min) * scalingFactor;
-                this.Acceleration = Math.Max(this.Acceleration - deltaAcceleration, axis.FullLoadMovement.Acceleration);
+                if (this.Acceleration >= axis.FullLoadMovement.Acceleration && this.Acceleration <= axis.EmptyLoadMovement.Acceleration)
+                {
+                    var deltaAcceleration = (axis.EmptyLoadMovement.Acceleration - axis.FullLoadMovement.Acceleration) * scalingFactor;
+                    this.Acceleration = Math.Max(this.Acceleration - deltaAcceleration, axis.FullLoadMovement.Acceleration);
+                }
             }
         }
 
