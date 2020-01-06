c65928ce009c04f823b97003a6b24bbaebb7e6a6
Danilo <dcorallo@ferrettogroup.com>
2020-01-06 23:47:52 +0100

    feat: DateTime in login view (useful when debugging OTP issues)

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Login/Views/LoginView.xaml.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Login/Views/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 using Ferretto.VW.App.Controls;
 using Ferretto.VW.App.Controls.Controls.Keyboards;
 
@@ -6,16 +7,33 @@ namespace Ferretto.VW.App.Modules.Login.Views
 {
     public partial class LoginView
     {
+        #region Fields
+
+        private readonly System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+        #endregion
+
         #region Constructors
 
         public LoginView()
         {
             this.InitializeComponent();
+
+            this.dispatcherTimer.Tick += this.DispatcherTimer_Tick;
+            this.dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            this.dispatcherTimer.Start();
+
+            this.DispatcherTimer_Tick(null, null);
         }
 
         #endregion
 
         #region Methods
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            this.txtTime.Text = string.Concat(DateTime.Now.ToLongDateString(), ", ", DateTime.Now.ToShortTimeString());
+        }
 
         private void OnKeyboardOpenHandler(object sender, InputEventArgs e)
         {
