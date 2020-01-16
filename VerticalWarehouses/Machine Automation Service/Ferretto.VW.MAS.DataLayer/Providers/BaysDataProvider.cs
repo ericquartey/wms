c2fd44d93b7e851e4803a4d6ff01d4bc162c3ff4
Stefano Prandini <sprandini@ferrettogroup.com>
2020-01-16 15:42:23 +0100

    fix: crash

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Providers/BaysDataProvider.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Providers/BaysDataProvider.cs
@@ -468,6 +468,7 @@ namespace Ferretto.VW.MAS.DataLayer
                 return this.dataContext.Bays
                     .AsNoTracking()
                      .Include(b => b.Shutter)
+                        .ThenInclude(i => i.Inverter)
                      .Include(b => b.Carousel)
                      .Include(b => b.Positions)
                         .ThenInclude(t => t.LoadingUnit)
