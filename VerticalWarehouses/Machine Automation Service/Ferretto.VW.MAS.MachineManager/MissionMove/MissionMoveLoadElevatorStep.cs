e45393edee68426558846fa58423d040c10aa265
Prandini Stefano <sprandini@ferrettogroup.com>
2020-01-19 11:17:15 +0100

    fix

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.MachineManager/MissionMove/MissionMoveLoadElevatorStep.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.MachineManager/MissionMove/MissionMoveLoadElevatorStep.cs
@@ -121,15 +121,17 @@ namespace Ferretto.VW.MAS.MachineManager.MissionMove
                     {
                         if (this.Mission.NeedHomingAxis == Axis.Horizontal)
                         {
+                            this.Mission.NeedHomingAxis = Axis.None;
+                            this.MissionsDataProvider.Update(this.Mission);
                             this.Logger.LogDebug($"MoveLoadingUnit start: direction {this.Mission.Direction}, openShutter {this.Mission.OpenShutterPosition}, measure {measure}");
                             this.LoadingUnitMovementProvider.MoveLoadingUnit(this.Mission.Direction, true, this.Mission.OpenShutterPosition, measure, MessageActor.MachineManager, this.Mission.TargetBay, this.Mission.LoadUnitId);
                         }
                         else if (this.Mission.NeedHomingAxis == Axis.BayChain)
                         {
+                            this.Mission.NeedHomingAxis = Axis.None;
+                            this.MissionsDataProvider.Update(this.Mission);
                             this.LoadUnitEnd();
                         }
-                        this.Mission.NeedHomingAxis = Axis.None;
-                        this.MissionsDataProvider.Update(this.Mission);
                     }
                     else
                     {
