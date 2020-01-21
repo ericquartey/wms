8ecc2bb7c4a5cb567a92ab8f4e86ac082745c339
Giovanni Basso <giovanni.basso@altran.it>
2020-01-21 17:33:37 +0100

    chore add loadingUnitControlView

---
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Operator/Views/ItemOperations/LoadingUnitCheckView/CompartmentsDataGridView.xaml.cs
@@ -0,0 +1,19 @@
+using System.Windows.Controls;
+
+namespace Ferretto.VW.App.Operator.Views
+{
+    /// <summary>
+    /// Interaction logic for CustomControlArticleDataGrid.xaml
+    /// </summary>
+    public partial class CompartmentsDataGridView : UserControl
+    {
+        #region Constructors
+
+        public CompartmentsDataGridView()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
