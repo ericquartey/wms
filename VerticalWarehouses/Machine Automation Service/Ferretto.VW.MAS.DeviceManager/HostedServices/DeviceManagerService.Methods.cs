19e293f9d545d9a7cdf088b84f245de8ebb1a324
Danilo <dcorallo@ferrettogroup.com>
2020-01-28 17:59:12 +0100

    Optimization (IsOneTonMachine)

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/HostedServices/DeviceManagerService.Methods.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/HostedServices/DeviceManagerService.Methods.cs
@@ -237,7 +237,7 @@ namespace Ferretto.VW.MAS.DeviceManager
         private void ProcessPositioningMessage(CommandMessage message, IServiceProvider serviceProvider)
         {
             var baysDataProvider = serviceProvider.GetRequiredService<IBaysDataProvider>();
-            var machineProvider = serviceProvider.GetRequiredService<IMachineProvider>();
+            var machineVolatileDataProvider = serviceProvider.GetRequiredService<IMachineVolatileDataProvider>();
             var machineResourcesProvider = serviceProvider.GetRequiredService<IMachineResourcesProvider>();
 
             System.Diagnostics.Debug.Assert(
@@ -258,7 +258,7 @@ namespace Ferretto.VW.MAS.DeviceManager
             }
             else
             {
-                data.IsOneTonMachine = machineProvider.IsOneTonMachine();
+                data.IsOneTonMachine = machineVolatileDataProvider.IsOneTonMachine.Value;
                 data.IsStartedOnBoard = machineResourcesProvider.IsDrawerCompletelyOnCradle;
 
                 currentStateMachine = new PositioningStateMachine(
