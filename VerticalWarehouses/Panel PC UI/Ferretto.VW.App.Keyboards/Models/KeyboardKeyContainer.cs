c737d28fa03eb4f618146bbf10a8f9185f6294df
Cristian Merighi <cristian@pacem.it>
2020-01-31 16:36:34 +0100

    Keyboard wip (before merging projects).

---
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Keyboards/Models/KeyboardKeyContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Ferretto.VW.App.Keyboards
+{
+    public abstract class KeyboardKeyContainer
+    {
+        #region Properties
+
+        public Thickness? KeyMargin { get; set; }
+
+        public Thickness? KeyPadding { get; set; }
+
+        public string KeyStyleResource { get; set; }
+
+        #endregion
+    }
+}
