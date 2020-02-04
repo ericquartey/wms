7db3c93fb8db10cdd6744e08b3aed71543f23410
Giovanni Basso <giovanni.basso@altran.it>
2020-02-04 11:53:17 +0100

    chore add DataGrid style

---
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Controls/Controls/DataGrids/PpcDatagrid.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+
+namespace Ferretto.VW.App.Controls
+{
+    public class PpcDataGrid : DataGrid
+    {
+        #region Methods
+
+        protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+        {
+            base.OnSelectionChanged(e);
+            if (!(this.SelectedItem is null))
+            {
+                this.UpdateLayout();
+                if (this.SelectedItem != null)
+                {
+                    this.ScrollIntoView(this.SelectedItem);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
