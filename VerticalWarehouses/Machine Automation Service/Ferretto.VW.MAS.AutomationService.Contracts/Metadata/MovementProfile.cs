edc3241da174d59c99200c1eefe2bc506c905ebb
Mirco Solato <msolato@ferrettogroup.com>
2020-02-04 12:31:14 +0100

    fix: view parameter

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.AutomationService.Contracts/Metadata/MovementProfile.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.AutomationService.Contracts/Metadata/MovementProfile.cs
@@ -10,19 +10,26 @@ namespace Ferretto.VW.MAS.AutomationService.Contracts
     [Ferretto.VW.MAS.Scaffolding.DataAnnotations.MetadataType(typeof(MovementProfile.Metadata))]
     public partial class MovementProfile
     {
+        #region Classes
+
         private class Metadata
         {
+            #region Properties
+
             [Editable(false)]
             public MovementProfileType Name { get; set; }
 
             [Category("Steps {0}")]
-            [CategoryParameter(nameof(StepMovementParameters.Id))]
-            public System.Collections.Generic.IEnumerable<StepMovementParameters> Steps { get; set; }
+            [CategoryParameter(nameof(StepMovementParameters.Number))]
+            public IEnumerable<StepMovementParameters> Steps { get; set; }
 
             [Display(ResourceType = typeof(Vertimag), Name = nameof(Vertimag.TotalDistance))]
             [Unit("mm/s")]
             public double TotalDistance { get; set; }
 
+            #endregion
         }
+
+        #endregion
     }
 }
