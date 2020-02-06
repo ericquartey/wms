0e2d30c13f0b816d9c9a53456a6f5ebacb074a14
Cristian Merighi <cristian@pacem.it>
2020-02-06 08:54:14 +0100

    Tries on AssemblyInfo/Project.

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Keyboards/Properties/AssemblyInfo.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Keyboards/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
-﻿using System.Windows.Markup;
+﻿using System.Windows;
+using System.Windows.Markup;
 
-[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("http://ferrettogroup.com/vw/keyboards", "Ferretto.VW.App.Keyboards.Converters")]
 [assembly: XmlnsDefinition("http://ferrettogroup.com/vw/keyboards", "Ferretto.VW.App.Keyboards.Controls")]
