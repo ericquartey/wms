fb023e140716948df932e8627c760b7cdfcb69a5
Giovanni Basso <giovanni.basso@altran.it>
2020-02-10 13:51:08 +0100

    chore fix update start installer  check

--- VerticalWarehouses/Installer/Ferretto.VW.Installer/Views/MainWindow.xaml.cs
+++ VerticalWarehouses/Installer/Ferretto.VW.Installer/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using NLog.Time;
 
 namespace Ferretto.VW.Installer
@@ -17,12 +18,20 @@ namespace Ferretto.VW.Installer
             this.Height = SystemParameters.PrimaryScreenHeight;
 #else
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.MouseDown += this.Shell_MouseDown;
 #endif
         }
 
         #endregion
 
         #region Methods
+        private void Shell_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
 
         protected override void OnInitialized(System.EventArgs e)
         {
