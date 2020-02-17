d3950c9a5b6500ce56927e6efa3cdb09e590e1be
Prandini Stefano <sprandini@ferrettogroup.com>
2020-02-17 15:39:28 +0100

    fix: remove unnecessary shutter manual parameters

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataModels/Bays/ShutterManualParameters.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataModels/Bays/ShutterManualParameters.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Ferretto.VW.MAS.DataModels
+﻿namespace Ferretto.VW.MAS.DataModels
 {
     public class ShutterManualParameters : DataModel
     {
