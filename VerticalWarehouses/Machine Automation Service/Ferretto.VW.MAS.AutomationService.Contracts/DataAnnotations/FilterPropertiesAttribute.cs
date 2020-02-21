7fde455867e0547c718726bee4d74cb6d0d16e29
Danilo <dcorallo@ferrettogroup.com>
2020-02-21 15:19:18 +0100

    feat: FilterPropertiesAttribute in parameters

---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.AutomationService.Contracts/DataAnnotations/FilterPropertiesAttribute.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace Ferretto.VW.MAS.Scaffolding.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+    public sealed class FilterPropertiesAttribute : Attribute
+    {
+        #region Constructors
+
+        public FilterPropertiesAttribute(Type type, string propertyName, object value, params string[] propertyList)
+        {
+            this.Type = type ?? throw new ArgumentNullException(nameof(type));
+            this.PropertyName = propertyName ?? throw new ArgumentNullException(nameof(propertyName));
+            this.Value = value ?? throw new ArgumentNullException(nameof(value));
+            this.PropertyList = new List<string>(propertyList ?? throw new ArgumentNullException(nameof(propertyList)));
+
+            this.PropertyInfo = type.GetProperty(propertyName);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public List<string> PropertyList { get; }
+
+        public string PropertyName { get; }
+
+        public Type Type { get; }
+
+        public object Value { get; }
+
+        private PropertyInfo PropertyInfo { get; }
+
+        #endregion
+
+        #region Methods
+
+        public List<string> GetExclusionList(object item)
+        {
+            var currentValue = this.PropertyInfo.GetValue(item);
+            var result = new List<string>();
+
+            if (currentValue.Equals(this.Value))
+            {
+                return this.PropertyList;
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
