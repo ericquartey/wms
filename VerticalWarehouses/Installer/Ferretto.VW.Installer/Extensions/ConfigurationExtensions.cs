99326bd376913ee439cb3243e070ff8f2ccf4dd8
Giovanni Basso <giovanni.basso@altran.it>
2020-02-25 14:22:32 +0100

    chore save parameters,bay configuration on installer config

--- VerticalWarehouses/Installer/Ferretto.VW.Installer/Extensions/ConfigurationExtensions.cs
+++ VerticalWarehouses/Installer/Ferretto.VW.Installer/Extensions/ConfigurationExtensions.cs
@@ -6,16 +6,24 @@ namespace Ferretto.VW.Installer
     public static class ConfigurationExtensions
     {
         #region Fields
-        private const string InstallMasPathName = "Install:MAS:Path";
+
+        private const string InstallBay1Ipaddress = "Install:Bay1:Ipaddress";
+
+        private const string InstallBay2Ipaddress = "Install:Bay2:Ipaddress";
+
+        private const string InstallBay3Ipaddress = "Install:Bay3:Ipaddress";
 
         private const string InstallDefaultMasIpaddressName = "Install:Default:MasIpaddress";
 
         private const string InstallDefaultMasIpportName = "Install:Default:MasIpport";
 
+        private const string InstallMasPathName = "Install:MAS:Path";
+
         #endregion
 
         #region Methods
-        public static string GetInstallMasPathName(this NameValueCollection appSettings)
+
+        public static string GetInstallBay1Ipaddress(this NameValueCollection appSettings)
         {
             if (appSettings is null)
             {
@@ -24,11 +32,45 @@ namespace Ferretto.VW.Installer
 
             try
             {
-                return appSettings.Get(InstallMasPathName);
+                return appSettings.Get(InstallBay1Ipaddress);
             }
             catch (Exception ex)
             {
-                throw new Exception($"The configuration key '{InstallDefaultMasIpaddressName}' is not specified or invalid.", ex);
+                throw new Exception($"The configuration key '{InstallBay1Ipaddress}' is not specified or invalid.", ex);
+            }
+        }
+
+        public static string GetInstallBay2Ipaddress(this NameValueCollection appSettings)
+        {
+            if (appSettings is null)
+            {
+                throw new ArgumentNullException(nameof(appSettings));
+            }
+
+            try
+            {
+                return appSettings.Get(InstallBay2Ipaddress);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"The configuration key '{InstallBay2Ipaddress}' is not specified or invalid.", ex);
+            }
+        }
+
+        public static string GetInstallBay3Ipaddress(this NameValueCollection appSettings)
+        {
+            if (appSettings is null)
+            {
+                throw new ArgumentNullException(nameof(appSettings));
+            }
+
+            try
+            {
+                return appSettings.Get(InstallBay3Ipaddress);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"The configuration key '{InstallBay3Ipaddress}' is not specified or invalid.", ex);
             }
         }
 
@@ -63,6 +105,23 @@ namespace Ferretto.VW.Installer
             catch (Exception ex)
             {
                 throw new Exception($"The configuration key '{InstallDefaultMasIpportName}' is not specified or invalid.", ex);
+            }
+        }
+
+        public static string GetInstallMasPathName(this NameValueCollection appSettings)
+        {
+            if (appSettings is null)
+            {
+                throw new ArgumentNullException(nameof(appSettings));
+            }
+
+            try
+            {
+                return appSettings.Get(InstallMasPathName);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"The configuration key '{InstallMasPathName}' is not specified or invalid.", ex);
             }
         }
 
