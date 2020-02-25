0713cb4020d24498f8967a3179b0c62cb17f40fc
Prandini Stefano <sprandini@ferrettogroup.com>
2020-02-25 12:47:13 +0100

    fix: test stop do not change requiredCycles

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/StateMachines/Positioning/PositioningEndState.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/StateMachines/Positioning/PositioningEndState.cs
@@ -91,7 +91,7 @@ namespace Ferretto.VW.MAS.DeviceManager.Positioning
 
                             var notificationMessage = new NotificationMessage(
                                 this.machineData.MessageData,
-                                this.machineData.MessageData.ExecutedCycles == 0 ? "Positioning Stopped" : "Test Stopped",
+                                this.machineData.MessageData.RequiredCycles == 0 ? "Positioning Stopped" : "Test Stopped",
                                 MessageActor.DeviceManager,
                                 MessageActor.DeviceManager,
                                 MessageType.Positioning,
@@ -210,7 +210,7 @@ namespace Ferretto.VW.MAS.DeviceManager.Positioning
 
                 var notificationMessage = new NotificationMessage(
                     this.machineData.MessageData,
-                    this.machineData.MessageData.ExecutedCycles == 0 ? "Positioning Completed" : "Test Completed",
+                    this.machineData.MessageData.RequiredCycles == 0 ? "Positioning Completed" : "Test Completed",
                     MessageActor.DeviceManager,
                     MessageActor.DeviceManager,
                     MessageType.Positioning,
