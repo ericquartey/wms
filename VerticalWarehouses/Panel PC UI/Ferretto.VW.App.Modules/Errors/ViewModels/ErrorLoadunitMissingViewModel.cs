cde5b94034b378ac7e6d1a24fc7c274df99c1bd0
Francesco Righi <francescorighi91@gmail.com>
2020-03-16 11:39:00 +0100

    Fix Error Fault empty view

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Errors/ViewModels/ErrorLoadunitMissingViewModel.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Errors/ViewModels/ErrorLoadunitMissingViewModel.cs
@@ -54,8 +54,6 @@ namespace Ferretto.VW.App.Modules.Errors.ViewModels
 
         private readonly IMachineModeWebService machineModeWebService;
 
-        private readonly INavigationService navigationService;
-
         private DelegateCommand automaticCommand;
 
         private string automaticStepText;
@@ -135,8 +133,7 @@ namespace Ferretto.VW.App.Modules.Errors.ViewModels
             IMachineLoadingUnitsWebService machineLoadingUnitsWebService,
             IMachineElevatorWebService machineElevatorWebService,
             IMachineBaysWebService machineBaysWebService,
-            IMachineErrorsWebService machineErrorsWebService,
-            INavigationService navigationService)
+            IMachineErrorsWebService machineErrorsWebService)
             : base(Services.PresentationMode.Menu | Services.PresentationMode.Installer | Services.PresentationMode.Operator)
         {
             this.machineLoadingUnitsWebService = machineLoadingUnitsWebService ?? throw new ArgumentNullException(nameof(machineLoadingUnitsWebService));
@@ -144,8 +141,7 @@ namespace Ferretto.VW.App.Modules.Errors.ViewModels
             this.machineModeWebService = machineModeWebService ?? throw new ArgumentNullException(nameof(machineModeWebService));
             this.machineElevatorWebService = machineElevatorWebService ?? throw new ArgumentNullException(nameof(machineElevatorWebService));
             this.machineBaysWebService = machineBaysWebService ?? throw new ArgumentNullException(nameof(machineBaysWebService));
-            this.navigationService = navigationService ?? throw new ArgumentNullException(nameof(navigationService));
-
+          
             this.CurrentStep = default(ErrorLoadunitMissingStepStart);
         }
 
@@ -1075,19 +1071,7 @@ namespace Ferretto.VW.App.Modules.Errors.ViewModels
                 await this.machineErrorsWebService.ResolveAllAsync();
 
                 this.MachineError = await this.machineErrorsWebService.GetCurrentAsync();
-
-                if (this.Error == null)
-                {
-                    await Application.Current.Dispatcher.BeginInvoke(
-                    System.Windows.Threading.DispatcherPriority.ApplicationIdle,
-                    new Action(() =>
-                    {
-                        if (this.navigationService.IsActiveView(nameof(Utils.Modules.Errors), Utils.Modules.Errors.ERRORLOADUNITMISSING))
-                        {
-                            this.navigationService.GoBack();
-                        }
-                    }));
-                }
+             
             }
             catch (Exception ex) when (ex is MasWebApiException || ex is System.Net.Http.HttpRequestException)
             {
