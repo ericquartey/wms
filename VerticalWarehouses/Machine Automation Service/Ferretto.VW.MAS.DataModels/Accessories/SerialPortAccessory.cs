1d874c222a69c5e8c5f8ad83a85479982df8e6b8
Alessandro Salomone <alessandro.salomone@altran.it>
2020-04-15 11:05:27 +0200

    feat: add accessories data models

---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataModels/Accessories/SerialPortAccessory.cs
@@ -0,0 +1,11 @@
+namespace Ferretto.VW.MAS.DataModels
+{
+    public class SerialPortAccessory : Accessory
+    {
+        #region Properties
+
+        public string PortName { get; set; }
+
+        #endregion
+    }
+}
