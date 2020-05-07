e518d054a301623a6e1797a153bdfa86bc8dda01
Prandini Stefano <sprandini@ferrettogroup.com>
2020-05-07 13:26:43 +0200

    fix: add AdjustAccelerationByWeight and AdjustSpeedByWeight in StepMovementParameters

---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Migrations/20200507074039_adjustByWeight.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ferretto.VW.MAS.DataLayer.Migrations
+{
+    public partial class adjustByWeight : Migration
+    {
+        #region Methods
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdjustAccelerationByWeight",
+                table: "MovementParameters");
+
+            migrationBuilder.RenameColumn(
+                name: "AdjustSpeedByWeight",
+                table: "MovementParameters",
+                newName: "AdjustByWeight");
+        }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AdjustByWeight",
+                table: "MovementParameters",
+                newName: "AdjustSpeedByWeight");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AdjustAccelerationByWeight",
+                table: "MovementParameters",
+                nullable: true);
+
+            migrationBuilder.Sql("UPDATE MovementParameters SET AdjustAccelerationByWeight = AdjustSpeedByWeight");
+        }
+
+        #endregion
+    }
+}
