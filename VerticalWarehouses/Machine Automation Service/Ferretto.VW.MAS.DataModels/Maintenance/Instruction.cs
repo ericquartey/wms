fa5ae2c40b07495e0fe0407d0bf15bef43bf3416
Prandini Stefano <sprandini@ferrettogroup.com>
2020-05-21 10:37:28 +0200

    feat: add instructions in Maintenance table

---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataModels/Maintenance/Instruction.cs
@@ -0,0 +1,15 @@
+﻿namespace Ferretto.VW.MAS.DataModels
+{
+    public sealed class Instruction : DataModel
+    {
+        #region Properties
+
+        public InstructionDefinition Definition { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public bool IsToDo { get; set; }
+
+        #endregion
+    }
+}
---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataModels/Maintenance/InstructionDefinition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Ferretto.VW.CommonUtils.Messages.Enumerations;
+
+namespace Ferretto.VW.MAS.DataModels
+{
+    public sealed class InstructionDefinition : DataModel
+    {
+        #region Properties
+
+        public InstructionType InstructionType { get; set; }
+
+        public string Description { get; set; }
+
+        public string CounterName { get; set; }
+        public int? MaxDays { get; set; }
+
+        public int? MaxRelativeCount { get; set; }
+
+        public int? MaxTotalCount { get; set; }
+
+        public bool IsElevator { get; set; }
+        public BayNumber BayNumber { get; set; }
+
+        public bool IsShutter { get; set; }
+        public bool IsCarousel { get; set; }
+        public bool IsSystem { get; set; }
+        #endregion
+    }
+}
