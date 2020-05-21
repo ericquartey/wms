cc1a1f5c52511743f89e25254ae20426cfc4c648
Francesco Righi <francescorighi91@gmail.com>
2020-05-21 16:35:45 +0200

    a

---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.AutomationService/Controllers/ServicingController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Ferretto.VW.CommonUtils.Messages.Enumerations;
+using Ferretto.VW.MAS.DataLayer;
+using Ferretto.VW.MAS.DataModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ferretto.VW.MAS.AutomationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicingController : ControllerBase
+    {
+        #region Fields
+
+        private readonly IServicingProvider servicingProvider;
+
+        #endregion
+
+        #region Constructors
+
+        public ServicingController(
+            IServicingProvider servicingProvider)
+        {
+            this.servicingProvider = servicingProvider ?? throw new System.ArgumentNullException(nameof(servicingProvider));
+        }
+
+        #endregion
+
+        #region Methods
+
+        [HttpPost("confirm-service")]
+        public IActionResult ConfirmService()
+        {
+            this.servicingProvider.ConfirmService();
+            return this.Ok();
+        }
+
+        [HttpPost("confirm-setup")]
+        public IActionResult ConfirmSetup()
+        {
+            this.servicingProvider.ConfirmSetup();
+            return this.Ok();
+        }
+
+        [HttpGet("actual-servicing-info")]
+        public ActionResult<ServicingInfo> GetActual()
+        {
+            return this.Ok(this.servicingProvider.GetActual());
+        }
+
+        [HttpGet("all-servicing-info")]
+        public ActionResult<IEnumerable<ServicingInfo>> GetAll()
+        {
+            return this.Ok(this.servicingProvider.GetAll());
+        }
+
+        [HttpGet("getById-servicing-info")]
+        public ActionResult<ServicingInfo> GetById(int id)
+        {
+            return this.Ok(this.servicingProvider.GetById(id));
+        }
+
+        [HttpGet("installation-info")]
+        public ActionResult<ServicingInfo> GetInstallationInfo()
+        {
+            return this.Ok(this.servicingProvider.GetInstallationInfo());
+        }
+
+        [HttpGet("actual-servicing-info")]
+        public ActionResult<ServicingInfo> GetLastConfirmed()
+        {
+            return this.Ok(this.servicingProvider.GetLastConfirmed());
+        }
+
+        #endregion
+    }
+}
---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.AutomationService/Controllers/StatisticsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Ferretto.VW.CommonUtils.Messages.Enumerations;
+using Ferretto.VW.MAS.DataLayer;
+using Ferretto.VW.MAS.DataModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Ferretto.VW.MAS.AutomationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        #region Fields
+
+        private readonly IStatisticsDataProvider statisticsProvider;
+
+        #endregion
+
+        #region Constructors
+
+        public StatisticsController(
+            IStatisticsDataProvider statisticsProvider)
+        {
+            this.statisticsProvider = statisticsProvider ?? throw new System.ArgumentNullException(nameof(statisticsProvider));
+        }
+
+        #endregion
+
+        #region Methods
+
+        [HttpPost("confirm-statistics")]
+        public ActionResult<int> ConfirmStatistics()
+        {
+            return this.Ok(this.statisticsProvider.ConfirmAndCreateNew());
+        }
+
+        [HttpGet("actual-statistics")]
+        public ActionResult<MachineStatistics> GetActual()
+        {
+            return this.Ok(this.statisticsProvider.GetActual());
+        }
+
+        [HttpGet("all-statistics")]
+        public ActionResult<IEnumerable<MachineStatistics>> GetAll()
+        {
+            return this.Ok(this.statisticsProvider.GetAll());
+        }
+
+        [HttpGet("actual-statistics")]
+        public ActionResult<MachineStatistics> GetById(int id)
+        {
+            return this.Ok(this.statisticsProvider.GetById(id));
+        }
+
+        [HttpGet("last-confirmed-statistics")]
+        public ActionResult<MachineStatistics> GetLastConfirmed()
+        {
+            return this.Ok(this.statisticsProvider.GetLastConfirmed());
+        }
+
+        #endregion
+    }
+}
