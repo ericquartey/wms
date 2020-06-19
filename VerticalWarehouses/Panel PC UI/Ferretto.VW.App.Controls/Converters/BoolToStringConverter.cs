98b577471b4ea33e401a9614ea8163b6325e06e4
Alessandro Gennaro <agennaro@ferrettogroup.com>
2020-06-19 15:39:42 +0200

    chore: Display information about cells in the Installation application according to the current language.

---
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Controls/Converters/BoolToStringConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Ferretto.VW.App.Controls.Converters
+{
+    public class BoolToStringConverter : IValueConverter
+    {
+        #region Methods
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool bValue)
+            {
+                return (bValue == true) ? Resources.Localized.Get("InstallationApp.True") : Resources.Localized.Get("InstallationApp.False");
+            }
+
+            return Resources.Localized.Get("InstallationApp.False");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var retValue = false;
+            if (!string.IsNullOrEmpty(value.ToString()))
+            {
+                switch (value.ToString().ToLower(culture))
+                {
+                    case "true":
+                    case "vero":
+                        retValue = true;
+                        break;
+
+                    case "false":
+                    case "falso":
+                        retValue = false;
+                        break;
+
+                    default:
+                        retValue = false;
+                        break;
+                }
+            }
+
+            return retValue;
+        }
+
+        #endregion
+    }
+}
