79e854e9130e4153278c02e1c959e9b4cee3dca4
Francesco Righi <francescorighi91@gmail.com>
2020-07-02 09:19:50 +0200

    Fix: Accessory

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Installation/ViewModels/Accessories/BarcodeReaderSettingsViewModel.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Installation/ViewModels/Accessories/BarcodeReaderSettingsViewModel.cs
@@ -116,6 +116,9 @@ namespace Ferretto.VW.App.Installation.ViewModels
 
             try
             {
+                this.IsEnabled = this.CanEnable();
+                this.RaisePropertyChanged(nameof(this.IsEnabled));
+
                 if (this.Data is BayAccessories bayAccessories)
                 {
                     this.IsAccessoryEnabled = bayAccessories.BarcodeReader.IsEnabledNew;
