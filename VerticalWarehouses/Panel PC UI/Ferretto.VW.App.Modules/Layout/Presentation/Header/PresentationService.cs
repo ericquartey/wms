9e3bfe4ca55dee6df475c40cd3b3af5c4598b666
Stefano Prandini <sprandini@ferrettogroup.com>
2020-07-16 09:40:49 +0200

    fix: disable screenshot

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Layout/Presentation/Header/PresentationService.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Layout/Presentation/Header/PresentationService.cs
@@ -166,7 +166,9 @@ namespace Ferretto.VW.App.Modules.Layout
 
         private bool CanSendScreenSnapshotAsync()
         {
-            return !this.isScreenCast;
+            return false;
+            // TODO Telemetry
+            //return !this.isScreenCast;
         }
 
         private async Task ToggleScreenCastAsync()
--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Services/Machine/MachineErrorsService.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Services/Machine/MachineErrorsService.cs
@@ -166,16 +166,18 @@ namespace Ferretto.VW.App.Services
                     &&
                     this.ActiveError != null)
                 {
-                    byte[] screenshot = null;
-                    Application.Current.Dispatcher.Invoke(() =>
-                    {
-                        screenshot = this.navigationService.TakeScreenshot();
-                    });
+                    // TODO Telemetry
 
-                    await this.telemetryHubClient.SendScreenShotAsync(
-                        (int)this.activeError.BayNumber,
-                        this.activeError.OccurrenceDate,
-                        screenshot);
+                    //byte[] screenshot = null;
+                    //Application.Current.Dispatcher.Invoke(() =>
+                    //{
+                    //    screenshot = this.navigationService.TakeScreenshot();
+                    //});
+
+                    //await this.telemetryHubClient.SendScreenShotAsync(
+                    //    (int)this.activeError.BayNumber,
+                    //    this.activeError.OccurrenceDate,
+                    //    screenshot);
 
                     await this.NavigateToErrorPageAsync();
                 }
