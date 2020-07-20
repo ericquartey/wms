4e0b4464b9cfbd9f13c2a4fce9d9ad0165dcf53a
Giovanni Basso <giovanni.basso@altran.it>
2020-07-20 13:47:23 +0200

    chore  fix error recovery

--- VerticalWarehouses/Utility/Ferretto.VW.Telemetry.Contracts.Hub/TelemetryHubClient.cs
+++ VerticalWarehouses/Utility/Ferretto.VW.Telemetry.Contracts.Hub/TelemetryHubClient.cs
@@ -9,6 +9,12 @@ namespace Ferretto.VW.Telemetry.Contracts.Hub
 {
     public class TelemetryHubClient : AutoReconnectHubClient, ITelemetryHubClient
     {
+        #region Fields
+
+        private readonly NLog.ILogger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        #endregion
+
         #region Constructors
 
         public TelemetryHubClient(Uri uri)
@@ -28,47 +34,91 @@ namespace Ferretto.VW.Telemetry.Contracts.Hub
 
         public async Task SendErrorLogAsync(IErrorLog errorlog)
         {
-            if (this.IsConnected)
+            if (!this.IsConnected)
+            {
+                return;
+            }
+
+            try
             {
                 await this.SendAsync("SendErrorLog", errorlog);
+            }
+            catch (Exception ex)
+            {
+                this.logger.Error($"Error sending error log to telemetry service {ex.Message}");
             }
         }
 
         public async Task SendMachineAsync(IMachine machine)
         {
-            if (this.IsConnected)
+            if (!this.IsConnected)
+            {
+                return;
+            }
+
+            try
             {
                 await this.SendAsync("SendMachine", machine);
+            }
+            catch (Exception ex)
+            {
+                this.logger.Error($"Error sending machine to telemetry service {ex.Message}");
             }
         }
 
         public async Task SendMissionLogAsync(IMissionLog missionLog)
         {
-            if (this.IsConnected)
+            if (!this.IsConnected)
+            {
+                return;
+            }
+
+            try
             {
                 await this.SendAsync("SendMissionLog", missionLog);
+            }
+            catch (Exception ex)
+            {
+                this.logger.Error($"Error sending mission log to telemetry service {ex.Message}");
             }
         }
 
         public async Task SendScreenCastAsync(int bayNumber, byte[] screenshot, DateTimeOffset timeStamp)
         {
-            if (this.IsConnected)
+            if (!this.IsConnected)
+            {
+                return;
+            }
+            try
             {
                 await this.SendAsync("SendScreenCast", bayNumber, timeStamp, screenshot);
+            }
+            catch (Exception ex)
+            {
+                this.logger.Error($"Error sending SendScreenCast to telemetry service {ex.Message}");
             }
         }
 
         public async Task SendScreenShotAsync(int bayNumber, DateTimeOffset timeStamp, byte[] screenShot)
         {
-            if (this.IsConnected)
+            if (!this.IsConnected)
+            {
+                return;
+            }
+
+            try
             {
                 await this.SendAsync("SendScreenShot", bayNumber, timeStamp, screenShot);
+            }
+            catch (Exception ex)
+            {
+                this.logger.Error($"Error sending screenshot to telemetry service {ex.Message}");
             }
         }
 
         protected override void RegisterEvents(HubConnection connection)
         {
-             connection.On(nameof(ITelemetryHub.RequestMachine), this.OnRequestMachine);
+            connection.On(nameof(ITelemetryHub.RequestMachine), this.OnRequestMachine);
         }
 
         private void OnRequestMachine()
