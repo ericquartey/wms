38b228ed0d7a7e638fe8ef6849a03f59fd89d271
Giovanni Basso <giovanni.basso@altran.it>
2020-07-30 11:53:52 +0200

    chore fix uwf check

--- VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/check_uwf_off.ps1
+++ VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/check_uwf_off.ps1
@@ -1,7 +1,7 @@
 ﻿$outUwf = uwfmgr.exe filter get-config
 $outUwf2 = ($outUwf -replace "\0", '') | Select-String -Pattern "(FILTER STATE|STATO FILTRO)(.*)"
 
-$uwfCurrentEnabled = $outUwf2[0] -match "(ON|ATTIVATA)"
+$uwfCurrentEnabled = $outUwf2[0] -match "(ON| ATTIVATA)"
 
 if($uwfCurrentEnabled)
 {
