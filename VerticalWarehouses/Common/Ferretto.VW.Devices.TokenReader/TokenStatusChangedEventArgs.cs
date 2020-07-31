dc50cf2bdef88e681d6ee87fb7a6e39e4d540796
Alessandro Salomone <alessandro.salomone@altran.it>
2020-07-31 09:35:51 +0200

    feat(token-reader): add driver implementation

---
+++ VerticalWarehouses/Common/Ferretto.VW.Devices.TokenReader/TokenStatusChangedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿namespace Ferretto.VW.Devices.TokenReader
+{
+    public class TokenStatusChangedEventArgs : System.EventArgs
+    {
+        #region Constructors
+
+        public TokenStatusChangedEventArgs(bool isInserted, string serialNumber)
+        {
+            this.IsInserted = isInserted;
+            this.SerialNumber = serialNumber;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public bool IsInserted { get; }
+
+        public string SerialNumber { get; }
+
+        #endregion
+    }
+}
