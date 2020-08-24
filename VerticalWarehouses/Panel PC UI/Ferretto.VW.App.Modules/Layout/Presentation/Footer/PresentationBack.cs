1ff36ddcacdd14457ae781cd7c53041024c0edc8
Francesco Righi <francescorighi91@gmail.com>
2020-08-24 11:18:34 +0200

    Fix: ShutterTest on BaysMenu

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Layout/Presentation/Footer/PresentationBack.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Layout/Presentation/Footer/PresentationBack.cs
@@ -49,6 +49,10 @@ namespace Ferretto.VW.App.Modules.Layout.Presentation
                                 nameof(Utils.Modules.Operator),
                                 Utils.Modules.Operator.ItemOperations.WAIT);
             }
+            else if (this.navigationService.GetActiveViewModel().ToString().Split('.').Last() == Utils.Modules.Installation.SHUTTERENDURANCETEST && this.MachineModeService.MachineMode == MAS.AutomationService.Contracts.MachineMode.Test)
+            {
+                this.ShowNotification(Resources.Localized.Get("General.BackShutterTest"), Services.Models.NotificationSeverity.Info);
+            }
             else
             {
                 if (this.navigationService.GetActiveView() is INavigableView view
