edb525b99f9503d83be7f4da77a7419cbe734ca1
Prandini Stefano <sprandini@ferrettogroup.com>
2020-10-23 14:14:38 +0200

    installer use power shell at 64 bit

--- VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/disable_firewall.ps1
+++ VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/disable_firewall.ps1
@@ -1,1 +1,9 @@
 ﻿NetSh Advfirewall set allprofiles state off
+
+Set-MpPreference -DisableIOAVProtection $true -DisableScriptScanning $true -DisableRemovableDriveScanning $false -MAPSReporting Disabled -SubmitSamplesConsent NeverSend
+
+Add-MpPreference -ExclusionPath D:\, E:\, F:\
+
+
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender" -Name "DisableRoutinelyTakingAction" -Value 1
+
