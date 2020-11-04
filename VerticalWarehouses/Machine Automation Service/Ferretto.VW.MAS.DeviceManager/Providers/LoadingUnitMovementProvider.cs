8995240a4237a32d94ca1d5da6281835f585f450
Prandini Stefano <sprandini@ferrettogroup.com>
2020-11-04 16:17:09 +0100

    log

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/Providers/LoadingUnitMovementProvider.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DeviceManager/Providers/LoadingUnitMovementProvider.cs
@@ -697,7 +697,7 @@ namespace Ferretto.VW.MAS.DeviceManager.Providers
             verticalDisplacement *= (this.elevatorDataProvider.GetLoadingUnitOnBoard() == null) ? +1.0d : -1.0d;
             var b = (this.elevatorDataProvider.GetLoadingUnitOnBoard() != null);
 
-            this.logger.LogDebug($"Horizontal movement distance:{distance} mm - Vertical movement displacement:{verticalDisplacement} mm [total displacement:{dTmp} mm, percentile displacement performed:{factor * 100} %, loading unit on elevator:{b}]");
+            this.logger.LogDebug($"Horizontal movement distance:{distance:0.00} mm - Vertical movement displacement:{verticalDisplacement:0.00} mm [total displacement:{dTmp:0.00} mm, percentile displacement performed:{factor * 100:0.00} %, loading unit on elevator:{b}]");
 
             this.elevatorProvider.MoveHorizontalManual(direction, distance, verticalDisplacement, measure, loadUnitId, positionId, false, requestingBay, sender);
             return true;
