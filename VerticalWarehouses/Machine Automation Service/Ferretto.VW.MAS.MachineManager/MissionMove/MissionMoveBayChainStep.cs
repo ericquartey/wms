6a625047d23863346cf04f16ac0ce8b7cfdc63d6
Prandini Stefano <sprandini@ferrettogroup.com>
2020-11-06 16:21:17 +0100

    fix bay chain

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.MachineManager/MissionMove/MissionMoveBayChainStep.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.MachineManager/MissionMove/MissionMoveBayChainStep.cs
@@ -354,6 +354,23 @@ namespace Ferretto.VW.MAS.MachineManager.MissionMove
                     }
                     else if (this.Mission.ErrorCode == MachineErrorCode.MoveBayChainNotAllowed)
                     {
+                        var waitingMission = this.MissionsDataProvider.GetAllActiveMissions()
+                            .FirstOrDefault(m => m.LoadUnitSource == this.Mission.LoadUnitDestination
+                                && (m.Step == MissionStep.WaitDepositCell || m.Step == MissionStep.WaitChain));
+
+                        if (waitingMission != null)
+                        {
+                            // wake up the mission waiting for the bay chain movement
+                            this.Logger.LogInformation($"Resume waiting deposit Mission:Id={waitingMission.Id}");
+                            this.LoadingUnitMovementProvider.ResumeOperation(
+                                waitingMission.Id,
+                                waitingMission.LoadUnitSource,
+                                waitingMission.LoadUnitDestination,
+                                waitingMission.WmsId,
+                                waitingMission.MissionType,
+                                waitingMission.TargetBay,
+                                MessageActor.MachineManager);
+                        }
                         this.SetErrorMoveBayChain(bay, position);
                         return;
                     }
