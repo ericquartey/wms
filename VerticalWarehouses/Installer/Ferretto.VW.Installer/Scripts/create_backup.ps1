d5e94d6fe94ce62e6003cd5f28fb1b48a903c2b2
Prandini Stefano <sprandini@ferrettogroup.com>
2020-11-18 17:21:31 +0100

    installer: add backup database

--- VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/create_backup.ps1
+++ VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/create_backup.ps1
@@ -2,6 +2,7 @@ $stageAs="$(Backup:Root:Path)\Staging\$(MAS:DirName)";
 $stagePpc="$(Backup:Root:Path)\Staging\$(PPC:DirName)";
 $stageInstaller="$(Backup:Root:Path)\Staging\$(Installer:DirName)";
 $stageTelemetry="$(Backup:Root:Path)\Staging\$(TS:DirName)";
+$stageDatabase="$(Backup:Root:Path)\Staging\Database"
 
 $as="$(Install:Root:Path)\$(MAS:DirName)";
 $ppc="$(Install:Root:Path)\$(PPC:DirName)";
@@ -13,6 +14,7 @@ if(Test-Path $stageAs -PathType Container) {Remove-Item $stageAs -Recurse -Force
 if(Test-Path $stagePpc -PathType Container) {Remove-Item $stagePpc -Recurse -Force};
 if(Test-Path $stageInstaller -PathType Container) {Remove-Item $stageInstaller -Recurse -Force};
 if(Test-Path $stageTelemetry -PathType Container) {Remove-Item $stageTelemetry -Recurse -Force};
+if(Test-Path $stageDatabase -PathType Container) {Remove-Item $stageDatabase -Recurse -Force};
 
 # copy current app in staging dirs
 Copy-Item $as $stageAs -Recurse;
@@ -20,8 +22,12 @@ Copy-Item $ppc $stagePpc -Recurse;
 Copy-Item $installer $stageInstaller -Recurse;
 Copy-Item $telemetry $stageTelemetry -Recurse;
 
+Copy-Item "E:\Database" $stageDatabase -Recurse;
+
 # remove current app dirs
 Remove-Item $as -Force -Recurse;
 Remove-Item $ppc -Force -Recurse;
 Remove-Item $installer -Force -Recurse
 Remove-Item $telemetry -Force -Recurse
+
+Remove-Item "F:\Database\Telemetry\*.*" -Force -Recurse
