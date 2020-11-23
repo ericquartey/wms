7c6902fd2d80c6c3c3fe83604cd51e046e7fc94c
Francesco Righi <francescorighi91@gmail.com>
2020-11-23 17:47:05 +0100

    Fix

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Migrations/20201123110038_BoxAndItemUniqueIdLength.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Migrations/20201123110038_BoxAndItemUniqueIdLength.cs
@@ -4,6 +4,19 @@ namespace Ferretto.VW.MAS.DataLayer.Migrations
 {
     public partial class BoxAndItemUniqueIdLength : Migration
     {
+        #region Methods
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Box",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "ItemUniqueIdLength",
+                table: "Machines");
+        }
+
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.AddColumn<bool>(
@@ -16,20 +29,9 @@ namespace Ferretto.VW.MAS.DataLayer.Migrations
                 name: "ItemUniqueIdLength",
                 table: "Machines",
                 nullable: false,
-                defaultValue: 16);
+                defaultValue: 0);
         }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "Box",
-                table: "Machines");
-
-            migrationBuilder.DropColumn(
-                name: "ItemUniqueIdLength",
-                table: "Machines");
-
-            migrationBuilder.Sql("UPDATE Machines SET ItemUniqueIdLength = 16");
-        }
+        #endregion
     }
 }
