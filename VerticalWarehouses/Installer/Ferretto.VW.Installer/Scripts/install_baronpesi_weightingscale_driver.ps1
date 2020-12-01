9a9c058bb218b502e43953377a8c546c58b68729
Prandini Stefano <sprandini@ferrettogroup.com>
2020-12-01 09:36:51 +0100

    fix: installer

--- VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/install_baronpesi_weightingscale_driver.ps1
+++ VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/install_baronpesi_weightingscale_driver.ps1
@@ -1,9 +1,10 @@
 $driverFullPath = "$(Update:Temp:Path)\$(Installer:DirName)\Drivers\ScaleBaronPesi";
+Get-ChildItem -Path "$driverFullPath\*.exe.zip" | Rename-Item -NewName { $_.Name -replace '.exe.zip','.exe' };
 $contents = Get-ChildItem -Path $driverFullPath | sort | Select-Object -First 1;
 $driverFileName = $contents.Name;
 if ($driverFileName -ne $null)
 {
-    $arguments = "/I ""$driverFullPath\$driverFileName"" /quiet";
-    Start-Process msiexec.exe -Wait -ArgumentList $arguments;
+    $arguments = "/SILENT /SUPPRESSMSGBOXES /NORESTART";
+    Start-Process -FilePath "$driverFullPath\$driverFileName" -Wait -ArgumentList $arguments;
 }
 
