188fc16c6ab7f5cbd7e449f1ae98612ac199cf65
Prandini Stefano <sprandini@ferrettogroup.com>
2020-12-10 17:23:57 +0100

    config

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Migrations/20201202123338_WeightingScaleTcp.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Migrations/20201202123338_WeightingScaleTcp.cs
@@ -12,7 +12,7 @@ namespace Ferretto.VW.MAS.DataLayer.Migrations
 
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("UPDATE Accessories SET IpAddress = '192.168.0.14', TcpPort = 4001 where Discriminator = 'WeightingScale' ");
+            migrationBuilder.Sql("UPDATE Accessories SET IpAddress = '192.168.0.15', TcpPort = 4001 where Discriminator = 'WeightingScale' ");
         }
 
         #endregion
