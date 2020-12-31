0026225ca85efdde8e50999d746a3e5436a18c0b
Prandini Stefano <sprandini@ferrettogroup.com>
2020-12-31 15:07:25 +0100

    fix: installer

--- VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/autostart_installer.ps1
+++ VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/autostart_installer.ps1
@@ -1,1 +1,4 @@
 ﻿Set-ItemProperty "$(PPC:Registry:Path)" -Name "$(PPC:Registry:Key)" -Value "$(Update:Temp:Path)\$(Installer:DirName)\$(Installer:FileName)";
+
+$winLogonPath = "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $winLogonPath -Name "Shell" -Value "explorer.exe" -Force
