093092ba224e655776fc752cc18af2ef07382b06
Prandini Stefano <sprandini@ferrettogroup.com>
2020-12-31 15:49:42 +0100

    comment

--- VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/autostart_installer.ps1
+++ VerticalWarehouses/Installer/Ferretto.VW.Installer/Scripts/autostart_installer.ps1
@@ -1,5 +1,6 @@
 ﻿Set-ItemProperty "$(PPC:Registry:Path)" -Name "$(PPC:Registry:Key)" -Value "$(Update:Temp:Path)\$(Installer:DirName)\$(Installer:FileName)";
 
+#set default shell
 $winLogonPath = "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Set-ItemProperty -Path $winLogonPath -Name "Shell" -Value "explorer.exe" -Force
 
