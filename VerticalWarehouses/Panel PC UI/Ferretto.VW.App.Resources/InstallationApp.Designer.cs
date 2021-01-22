adaf328a1725a5ef954a8dd60efbd55a95830323
Prandini Stefano <sprandini@ferrettogroup.com>
2021-01-22 18:10:19 +0100

    Fix: enable tuning bay with a single load unit

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Resources/InstallationApp.Designer.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Resources/InstallationApp.Designer.cs
@@ -4156,7 +4156,7 @@ namespace Ferretto.VW.App.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Movimenti Guidati.
+        ///   Looks up a localized string similar to Guided movements.
         /// </summary>
         public static string MovementsGuided {
             get {
@@ -4165,7 +4165,7 @@ namespace Ferretto.VW.App.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Movimenti Manuali.
+        ///   Looks up a localized string similar to Manual movements.
         /// </summary>
         public static string MovementsManual {
             get {
@@ -6235,7 +6235,7 @@ namespace Ferretto.VW.App.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Use weight control.
+        ///   Looks up a localized string similar to Do weighing.
         /// </summary>
         public static string UseWeightControl {
             get {
