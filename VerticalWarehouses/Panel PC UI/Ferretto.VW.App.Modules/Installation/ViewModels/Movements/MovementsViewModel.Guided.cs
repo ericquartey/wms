f2117333d9bc3d111f5cc6165e9821609bb62aca
Prandini Stefano <sprandini@ferrettogroup.com>
2021-01-27 16:07:49 +0100

    fix: enable tuning bay

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Installation/ViewModels/Movements/MovementsViewModel.Guided.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Installation/ViewModels/Movements/MovementsViewModel.Guided.cs
@@ -716,8 +716,7 @@ namespace Ferretto.VW.App.Installation.ViewModels
         {
             return this.CanBaseExecute() &&
                    !this.IsTuningBay &&
-                   !(this.MachineStatus.LoadingUnitPositionDownInBay != null &&
-                        this.MachineStatus.LoadingUnitPositionUpInBay != null) &&
+                   this.MachineStatus.LoadingUnitPositionDownInBay is null &&
                    this.SensorsService.Sensors.ACUBay1S3IND;
         }
 
