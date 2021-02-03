baec589d366b7a4e79ec91b25006e9be43e9cd55
Prandini Stefano <sprandini@ferrettogroup.com>
2021-02-03 18:10:25 +0100

    fix: SetLoadingUnit

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataModels/Bays/ShutterManualParameters.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataModels/Bays/ShutterManualParameters.cs
@@ -72,6 +72,24 @@
             }
         }
 
+        public double MaxSpeed
+        {
+            get => this.maxSpeed;
+            set
+            {
+                this.maxSpeed = value;
+            }
+        }
+
+        public double MinSpeed
+        {
+            get => this.minSpeed;
+            set
+            {
+                this.minSpeed = value;
+            }
+        }
+
         #endregion
     }
 }
