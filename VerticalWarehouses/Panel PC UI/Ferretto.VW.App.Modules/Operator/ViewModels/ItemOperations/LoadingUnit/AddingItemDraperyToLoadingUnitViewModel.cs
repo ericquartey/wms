fc8e3597290c65b349563822d375df305ee72092
Alessandro Gennaro <agennaro@ferrettogroup.com>
2021-04-07 14:05:34 +0200

    feat: Add the view containing the information (scheleton) when add a drapery item into a given loading unit.

---
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Operator/ViewModels/ItemOperations/LoadingUnit/AddingItemDraperyToLoadingUnitViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ferretto.VW.App.Resources;
+using Ferretto.VW.App.Services;
+using Ferretto.VW.MAS.AutomationService.Contracts;
+using Ferretto.VW.MAS.AutomationService.Contracts.Hubs;
+using Prism.Commands;
+using Prism.Events;
+
+namespace Ferretto.VW.App.Modules.Operator.ViewModels
+{
+    public class AddingItemDraperyToLoadingUnitViewModel : BaseOperatorViewModel
+    {
+        #region Fields
+
+        private double draperyHeight;
+
+        private string draperyId;
+
+        private string draperyItemCode;
+
+        private string draperyItemDescription;
+
+        private double draperyQuantity;
+
+        #endregion
+
+        #region Constructors
+
+        public AddingItemDraperyToLoadingUnitViewModel()
+            : base(PresentationMode.Operator)
+        {
+            this.Logger.Debug("Ctor AddingItemDraperyToLoadingUnitViewModel!");
+        }
+
+        #endregion
+
+        #region Properties
+
+        public double DraperyHeight
+        {
+            get => this.draperyHeight;
+            set => this.SetProperty(ref this.draperyHeight, value, this.RaiseCanExecuteChanged);
+        }
+
+        public string DraperyId
+        {
+            get => this.draperyId;
+            set => this.SetProperty(ref this.draperyId, value, this.RaiseCanExecuteChanged);
+        }
+
+        public string DraperyItemCode
+        {
+            get => this.draperyItemCode;
+            set => this.SetProperty(ref this.draperyItemCode, value, this.RaiseCanExecuteChanged);
+        }
+
+        public string DraperyItemDescription
+        {
+            get => this.draperyItemDescription;
+            set => this.SetProperty(ref this.draperyItemDescription, value, this.RaiseCanExecuteChanged);
+        }
+
+        public double DraperyQuantity
+        {
+            get => this.draperyQuantity;
+            set => this.SetProperty(ref this.draperyQuantity, value, this.RaiseCanExecuteChanged);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override async Task OnAppearedAsync()
+        {
+            await base.OnAppearedAsync();
+        }
+
+        protected override void RaiseCanExecuteChanged()
+        {
+            base.RaiseCanExecuteChanged();
+        }
+
+        #endregion
+    }
+}
---
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Operator/Views/ItemOperations/LoadingUnit/AddingItemDraperyToLoadingUnitView.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Ferretto.VW.App.Modules.Operator.Views
+{
+    /// <summary>
+    /// Interaction logic for AddingItemDraperyToLoadingUnitView.xaml
+    /// </summary>
+    public partial class AddingItemDraperyToLoadingUnitView
+    {
+        #region Constructors
+
+        public AddingItemDraperyToLoadingUnitView()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
