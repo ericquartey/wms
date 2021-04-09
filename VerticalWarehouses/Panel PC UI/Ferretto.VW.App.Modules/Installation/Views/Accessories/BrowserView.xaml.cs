34f2fd660e8c0c859ed4fc82dc355b5c23cccaed
Francesco Righi <francescorighi91@gmail.com>
2021-04-09 11:16:13 +0200

    fix: add BrowserView

---
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Modules/Installation/Views/Accessories/BrowserView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ferretto.VW.App.Modules.Installation.Views
+{
+    public partial class BrowserView : UserControl
+    {
+        #region Fields
+
+        public static readonly DependencyProperty UrlProperty =
+           DependencyProperty.Register(nameof(Url), typeof(string), typeof(BrowserView), new PropertyMetadata(OnUrlChanged));
+
+        #endregion
+
+        #region Constructors
+
+        public BrowserView()
+        {
+            this.InitializeComponent();
+
+            Instance = this;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public static BrowserView Instance { get; private set; }
+
+        public string Url
+        {
+            get => (string)this.GetValue(UrlProperty);
+            set => this.SetValue(UrlProperty, value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static void OnUrlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewValue.ToString()))
+            {
+                Instance.MyWebBrowser.Navigate(("http://" + e.NewValue.ToString()));
+                //Instance.MyWebBrowser.Refresh();
+            }
+        }
+
+        private void PpcButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.MyWebBrowser.Refresh();
+        }
+
+        #endregion
+    }
+}
