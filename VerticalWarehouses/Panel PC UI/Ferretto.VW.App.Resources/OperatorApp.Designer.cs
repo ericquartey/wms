daee83f3d46c688115f94d1b759ce5877b6a2688
Stefano Prandini <sprandini@ferrettogroup.com>
2021-05-24 14:56:30 +0200

    add Polish translations

--- VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Resources/OperatorApp.Designer.cs
+++ VerticalWarehouses/Panel PC UI/Ferretto.VW.App.Resources/OperatorApp.Designer.cs
@@ -943,6 +943,15 @@ namespace Ferretto.VW.App.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        public static string ConfirmOnFull {
+            get {
+                return ResourceManager.GetString("ConfirmOnFull", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Change.
         /// </summary>
         public static string ConfirmPartial {
