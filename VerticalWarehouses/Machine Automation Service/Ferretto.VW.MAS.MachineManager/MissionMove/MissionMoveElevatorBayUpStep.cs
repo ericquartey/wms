9e8fd2dc13dc8958dd2688391a84762e60b30166
Stefano Prandini <sprandini@ferrettogroup.com>
2021-05-28 18:54:33 +0200

    fix

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.MachineManager/MissionMove/MissionMoveElevatorBayUpStep.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.MachineManager/MissionMove/MissionMoveElevatorBayUpStep.cs
@@ -95,9 +95,10 @@ namespace Ferretto.VW.MAS.MachineManager.MissionMove
                 }
                 else
                 {
-                    var destinationPosition = bay.Positions.SingleOrDefault(s => s.Location == this.Mission.LoadUnitDestination);
+                    var position = bay.Positions.SingleOrDefault(s => s.Location == this.Mission.LoadUnitDestination);
+                    var destination = bay.Positions.SingleOrDefault(s => s.IsUpper != position.IsUpper);
 
-                    this.LoadingUnitMovementProvider.PositionElevatorToPosition(destinationPosition.Height,
+                    this.LoadingUnitMovementProvider.PositionElevatorToPosition(destination.Height,
                                         BayNumber.None,
                                         this.Mission.CloseShutterPosition,
                                         measure: false,
@@ -105,7 +106,7 @@ namespace Ferretto.VW.MAS.MachineManager.MissionMove
                                         this.Mission.TargetBay,
                                         this.Mission.RestoreConditions,
                                         this.Mission.LoadUnitId,
-                                        destinationPosition.Id,
+                                        destination.Id,
                                         null);
                 }
 
@@ -115,7 +116,7 @@ namespace Ferretto.VW.MAS.MachineManager.MissionMove
             else
             {
                 var positionUp = bay.Positions.SingleOrDefault(s => s.IsUpper);
-                var destination =bay.Positions.SingleOrDefault(s => !s.IsUpper);
+                var destination = bay.Positions.SingleOrDefault(s => !s.IsUpper);
 
                 if (!this.SensorsProvider.IsLoadingUnitInLocation(destination.Location)
                     || !this.SensorsProvider.IsLoadingUnitInLocation(positionUp.Location)
