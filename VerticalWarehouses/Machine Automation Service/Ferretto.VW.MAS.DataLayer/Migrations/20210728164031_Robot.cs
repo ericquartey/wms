7fad31a94d6a8d21a2df718e9beabe86bf480ed4
Stefano Prandini <sprandini@ferrettogroup.com>
2021-07-28 18:41:30 +0200

    migration

---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Migrations/20210728164031_Robot.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ferretto.VW.MAS.DataLayer.Migrations
+{
+    public partial class Robot : Migration
+    {
+        #region Methods
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRobot",
+                table: "Bays");
+        }
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRobot",
+                table: "Bays",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        #endregion
+    }
+}
---
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/migrationsAddRobot.cmd
@@ -0,0 +1,2 @@
+dotnet ef migrations add Robot --verbose
+pause
