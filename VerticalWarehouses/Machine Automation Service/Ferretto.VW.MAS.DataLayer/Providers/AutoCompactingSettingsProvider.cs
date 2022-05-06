032fed695934d417b9b723817cc6955d24b41453
FedericoBizzarri <bizzarriwork@hotmail.com>
2022-05-06 16:57:05 +0200

    Fix : CanCompactingStart

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Providers/AutoCompactingSettingsProvider.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.DataLayer/Providers/AutoCompactingSettingsProvider.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Ferretto.VW.CommonUtils.Enumerations;
+using Ferretto.VW.CommonUtils.Messages.Enumerations;
 using Ferretto.VW.MAS.DataModels;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Prism.Events;
-
-using Ferretto.VW.MAS.Utils.FiniteStateMachines;
 
 namespace Ferretto.VW.MAS.DataLayer
 {
@@ -20,17 +17,13 @@ namespace Ferretto.VW.MAS.DataLayer
 
         private readonly IEventAggregator eventAggregator;
 
-        private readonly ILoadingUnitsDataProvider loadingUnitsDataProvider;
-
-        private readonly ILogger logger;
-
-        private readonly IMachineProvider machineProvider;
+        private readonly ILogger<DataLayerService> logger;
 
         private readonly IMachineVolatileDataProvider machineVolatileDataProvider;
 
         private readonly IMissionsDataProvider missionsDataProvider;
 
-        private readonly IServiceScope scope;
+        private readonly IBaysDataProvider baysDataProvider;
 
         #endregion
 
@@ -40,17 +33,16 @@ namespace Ferretto.VW.MAS.DataLayer
             DataLayerContext dataContext,
             IEventAggregator eventAggregator,
             IMissionsDataProvider missionsDataProvider,
-            ILoadingUnitsDataProvider loadingUnitsDataProvider,
             IMachineVolatileDataProvider machineVolatileDataProvider,
             IMachineProvider machineProvider,
+            IBaysDataProvider baysDataProvider,
             ILogger<DataLayerService> logger) : base(eventAggregator)
         {
             this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
             this.eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
             this.missionsDataProvider = missionsDataProvider ?? throw new ArgumentNullException(nameof(missionsDataProvider));
-            this.loadingUnitsDataProvider = loadingUnitsDataProvider ?? throw new ArgumentNullException(nameof(loadingUnitsDataProvider));
             this.machineVolatileDataProvider = machineVolatileDataProvider ?? throw new ArgumentNullException(nameof(machineVolatileDataProvider));
-            this.machineProvider = machineProvider ?? throw new ArgumentNullException(nameof(machineProvider));
+            this.baysDataProvider = baysDataProvider ?? throw new ArgumentNullException(nameof(baysDataProvider));
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
@@ -123,38 +115,34 @@ namespace Ferretto.VW.MAS.DataLayer
             {
                 var activeSettings = this.GetAllAutoCompactingSettings().ToList().Find(x => x.IsActive && DateTime.Now.TimeOfDay.TotalMinutes - x.BeginTime.TotalMinutes >= 0 && DateTime.Now.TimeOfDay.TotalMinutes - x.BeginTime.TotalMinutes <= 1 && x.RemainingTime == 0);
 
-                if (activeSettings != null || true)
+                if (activeSettings != null)
                 {
                     if (this.CanCompactingStart())
                     {
-                        this.machineVolatileDataProvider.Mode = CommonUtils.Messages.MachineMode.Manual;
+                        this.machineVolatileDataProvider.Mode = CommonUtils.Messages.MachineMode.Compact;
                     }
                     else
                     {
-                        this.logger.LogDebug($"AutoCompactingSettings : Condizioni non soddisfatte");
+                        this.logger.LogDebug($"AutoCompactingSettings : time {DateTime.Now.TimeOfDay.Hours}:{DateTime.Now.TimeOfDay.Minutes} DENIED");
                     }
                 }
             }
             catch (Exception ex)
             {
-                this.logger.LogDebug($"AutoCompactingSettings : {ex.Message}");
+                this.logger.LogError($"AutoCompactingSettings : {ex.Message}");
             }
         }
 
         private bool CanCompactingStart()
         {
-            var machine = this.machineProvider.Get();
-
-            var zeroSensor = this.machineVolatileDataProvider.IsOneTonMachine.Value ? IOMachineSensors.ZeroPawlSensorOneTon : IOMachineSensors.ZeroPawlSensor;
+            var bay = this.baysDataProvider.GetByNumber(BayNumber.BayOne);
 
             var activeMission = this.missionsDataProvider.GetAllActiveMissions();
 
-            var activeMission2 = this.missionsDataProvider.GetAllMissions();
-
             var res = this.machineVolatileDataProvider.Mode == CommonUtils.Messages.MachineMode.Automatic &&
                 this.machineVolatileDataProvider.MachinePowerState == CommonUtils.Messages.MachinePowerState.Powered &&
-                //(machine.Bays.Find(x => x.Number == CommonUtils.Messages.Enumerations.BayNumber.BayOne).Shutter != null ||
-                !activeMission.Any()/*)*/;
+                (bay.Shutter != null && bay.Shutter.Type != ShutterType.NotSpecified || bay.CurrentMission == null) &&
+                !activeMission.Any(x => x.Status == MissionStatus.Executing);
 
             //var result = !this.IsWaitingForResponse &&
             //       this.MachineService.MachinePower == MachinePowerState.Powered &&
