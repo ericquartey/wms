b64df66c0ca6f1d127891965e262cad4ae3cf7d1
Stefano Prandini <sprandini@ferrettogroup.com>
2022-11-04 19:13:39 +0100

    fix

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.AutomationService/HostedServices/NotificationTelemetryService.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.AutomationService/HostedServices/NotificationTelemetryService.cs
@@ -103,7 +103,7 @@ namespace Ferretto.VW.MAS.AutomationService
                 var machineWms = scope.ServiceProvider.GetRequiredService<IMachinesWmsWebService>();
                 var machineVolatile = scope.ServiceProvider.GetRequiredService<IMachineVolatileDataProvider>();
 
-                if (machineVolatile.WmsIsEnabled is true && machineVolatile.WMSIsConnected is true)
+                if (machineVolatile.WmsIsEnabled is true)
                 {
                     try
                     {
