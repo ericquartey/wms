4780e061c537306cd90254aa02020225935513b4
Michael Lanzafame <mlanzafame00@gmail.com>
2023-07-28 15:05:14 +0200

    Fix

--- VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.IODriver/ConditionEvaluators/SecurityIsClearedConditionEvaluator.cs
+++ VerticalWarehouses/Machine Automation Service/Ferretto.VW.MAS.IODriver/ConditionEvaluators/SecurityIsClearedConditionEvaluator.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Ferretto.VW.CommonUtils.Messages.Enumerations;
 using Ferretto.VW.MAS.DataLayer;
 using Ferretto.VW.MAS.DataModels;
+using Microsoft.Extensions.Logging;
 
 namespace Ferretto.VW.MAS.IODriver
 {
@@ -14,16 +15,20 @@ namespace Ferretto.VW.MAS.IODriver
 
         private readonly IMachineProvider machineProvider;
 
+        private readonly ILogger logger;
+
         #endregion
 
         #region Constructors
 
         public SecurityIsClearedConditionEvaluator(
+            ILogger logger,
             IIoDevicesProvider ioDevicesProvider,
             IMachineProvider machineProvider)
         {
             this.ioDevicesProvider = ioDevicesProvider ?? throw new ArgumentNullException(nameof(ioDevicesProvider));
             this.machineProvider = machineProvider ?? throw new ArgumentNullException(nameof(machineProvider));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         #endregion
@@ -32,36 +37,44 @@ namespace Ferretto.VW.MAS.IODriver
 
         public bool IsSatisfied(BayNumber bayNumber)
         {
-            var mainDevice = this.ioDevicesProvider.Devices.SingleOrDefault(d => d.IoIndex == IoIndex.IoDevice1);
-            if (mainDevice is null)
+            try
             {
+                var mainDevice = this.ioDevicesProvider.Devices.SingleOrDefault(d => d.IoIndex == IoIndex.IoDevice1);
+                if (mainDevice is null)
+                {
+                    return false;
+                }
+
+                var fireAlarm = this.machineProvider.IsFireAlarmActive() ? !mainDevice.PreFireAlarm && !mainDevice.FireAlarm : true;
+
+                var sensitiveEdgeAlarm = this.machineProvider.IsSpeaActive() && !this.machineProvider.IsSensitiveCarpetsBypass() ? mainDevice.SensitiveEdgeAlarm : false;
+                var sensitiveCarpetsAlarm = this.machineProvider.IsSpeaActive() && !this.machineProvider.IsSensitiveCarpetsBypass() ? mainDevice.SensitiveCarpetsAlarm : false;
+
+                var heightAlarm = this.machineProvider.IsHeightAlarmActive();
+
+                return !mainDevice.ResetSecurity
+                    &&
+                    !mainDevice.MushroomEmergency
+                    &&
+                    !mainDevice.MicroCarterLeftSideBay
+                    &&
+                    !mainDevice.AntiIntrusionShutterBay
+                    &&
+                    !mainDevice.MicroCarterRightSideBay
+                    &&
+                    fireAlarm
+                    &&
+                    !sensitiveEdgeAlarm
+                    &&
+                    !sensitiveCarpetsAlarm
+                    &&
+                    !heightAlarm;
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError("Test1 ResetSecurity " + ex.Message);
                 return false;
             }
-
-            var fireAlarm = this.machineProvider.IsFireAlarmActive() ? !mainDevice.PreFireAlarm && !mainDevice.FireAlarm : true;
-
-            var sensitiveEdgeAlarm = this.machineProvider.IsSpeaActive() && !this.machineProvider.IsSensitiveCarpetsBypass() ? mainDevice.SensitiveEdgeAlarm : false;
-            var sensitiveCarpetsAlarm = this.machineProvider.IsSpeaActive() && !this.machineProvider.IsSensitiveCarpetsBypass() ? mainDevice.SensitiveCarpetsAlarm : false;
-
-            var heightAlarm = this.machineProvider.IsHeightAlarmActive();
-
-            return !mainDevice.ResetSecurity
-                &&
-                !mainDevice.MushroomEmergency
-                &&
-                !mainDevice.MicroCarterLeftSideBay
-                &&
-                !mainDevice.AntiIntrusionShutterBay
-                &&
-                !mainDevice.MicroCarterRightSideBay
-                &&
-                fireAlarm
-                &&
-                !sensitiveEdgeAlarm
-                &&
-                !sensitiveCarpetsAlarm
-                &&
-                !heightAlarm;
         }
 
         #endregion
